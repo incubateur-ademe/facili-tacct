8d929fd38f3cd0ab4c225b4936fc07c2ecc559b5
AntoineConegero <antoine.conegero@gmail.com>
2024-04-30 13:57:04 +0200

    setup next

---
+++ dev/src/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  host: process.env.NEXT_PUBLIC_SITE_URL!,
+  name: "Facili-TACCT",
+  tagline: "",
+  env: (process.env.TACCT_ENV || "dev") as "dev" | "prod" | "staging",
+  appVersion: process.env.NEXT_PUBLIC_APP_VERSION!,
+  appVersionCommit: process.env.NEXT_PUBLIC_APP_VERSION_COMMIT!,
+  repositoryUrl: process.env.NEXT_PUBLIC_REPOSITORY_URL!,
+  formUrl: "",
+};
