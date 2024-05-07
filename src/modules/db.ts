f167167b4a170ee119f373a1f15db63c2f6fdb84
AntoineConegero <antoine.conegero@gmail.com>
2024-05-07 21:13:52 +0200

    middleware + Models + Prisma requests

--- src/modules/db.ts
+++ src/modules/db.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
-const db = new PrismaClient();
+const db = new PrismaClient({
+    datasourceUrl: process.env.SCALINGO_POSTGRESQL_URL,
+  });
 
 export default db;
