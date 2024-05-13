61d3acecd6816356d3504a051ed9182f2e09ec1b
AntoineConegero <antoine.conegero@gmail.com>
2024-05-13 19:11:25 +0200

    first form pages with design
    Signed-off-by: AntoineConegero <antoine.conegero@gmail.com>

--- src/app/form/components/page2.tsx
+++ src/app/form/components/page2.tsx
@@ -1,6 +1,8 @@
 import { Container, Grid, GridCol } from "../../../dsfr/server";
 import RadioButton from '../../../dsfr/base/client/RadioButtons';
 import { Button } from "@codegouvfr/react-dsfr/Button";
+import { Box } from "../../../dsfr/server";
+import Bubble from "./bubble";
 
 type Props = {
     answers2?: string;
@@ -9,7 +11,7 @@ type Props = {
   }
 
 const Page2 : React.FC<Props> = (props) => {
-    const { answers2, setAnswers2, setActiveTab } = props;
+    const { setActiveTab } = props;
     const handleForward = () => {
         setActiveTab("3");
     }
@@ -17,43 +19,19 @@ const Page2 : React.FC<Props> = (props) => {
         setActiveTab("1");
     }
     return (
-        <Container m="6w">
-            <h1>Page2</h1>
-            <RadioButton
-                legend="Question 1"
-                options={[
-                    {
-                        label: "P2 Q1 R1",
-                        nativeInputProps: {
-                          checked: answers2 === "P2 Q1 R1",
-                          onChange: ()=> setAnswers2("P2 Q1 R1")
-                      }
-                    },
-                    {
-                        label: "P2 Q1 R2",
-                        nativeInputProps: {
-                          checked: answers2 === "P2 Q1 R2",
-                          onChange: ()=> setAnswers2("P2 Q1 R2")
-                      }
-                    },
-                    {
-                        label: "P2 Q1 R3",
-                        nativeInputProps: {
-                          checked: answers2 === "P2 Q1 R3",
-                          onChange: ()=> setAnswers2("P2 Q1 R3")
-                      }
-                    },
-                ]}
-            />
-            <div style={{display:"flex", flexDirection:"row", gap:"2em"}}>
-                <Button onClick={handleBackward}>
-                    Page précédente
-                </Button>
-                <Button onClick={handleForward}>
-                    Page suivante
-                </Button>
-            </div>
-        </Container>  
+			<>
+        <Box as="div" className="speech-bubble2">
+        	<Bubble/>
+      	</Box>
+        <div style={{display:"flex", flexDirection:"row", gap:"2em"}}>
+          <Button onClick={handleBackward}>
+            Page précédente
+          </Button>
+          <Button onClick={handleForward}>
+            Page suivante
+          </Button>
+        </div>
+			</>
     )
 }
 
