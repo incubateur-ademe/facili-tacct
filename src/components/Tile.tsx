48bc1dc2985ec4de1a2cfab6d10e47c778cb77ba
AntoineConegero <antoine.conegero@gmail.com>
2024-05-21 18:50:40 +0200

    darkComp + input validation

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,4 +1,6 @@
 import styles from "./components.module.scss";
+import { fr } from "@codegouvfr/react-dsfr";
+import { useIsDark } from "@codegouvfr/react-dsfr/useIsDark";
 
 interface Props {
 	children: string;
@@ -7,8 +9,15 @@ interface Props {
 }
 
 export function TileComp ({children, selected, onClick}: Props) {
+	const { isDark } = useIsDark();
+ 	const darkClass = {
+  	backgroundColor: fr.colors.getHex({isDark}).decisions.background.default.grey.active,
+  	"&:hover": {
+  	  backgroundColor: fr.colors.getHex({isDark}).decisions.background.alt.grey.hover
+  	},
+	}
 	return (
-		<div className={selected ? styles.selected : styles.unselected}
+		<div className={selected ? styles.selected : styles.unselected} style={darkClass}
 			onClick={onClick}>
 			<h6>{children}</h6>
     </div>
