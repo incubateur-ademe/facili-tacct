3f4f3e015753938d1b30a0f837e5b838666839ce
AntoineConegero <antoine.conegero@gmail.com>
2024-06-06 18:21:42 +0200

    add all themes with icons

--- src/lib/utils/thematiques.tsx
+++ src/lib/utils/thematiques.tsx
@@ -1,10 +1,21 @@
-import inconfortIcon from "../../assets/icons/themes/inconfort_thermique_icon_green.svg";
-import gestionRisqueIcon from "../../assets/icons/themes/gestionRisques_icon_grey.svg";
-import amenagementIcon from "../../assets/icons/themes/amenagement_icon_grey.svg";
-import batimentIcon from "../../assets/icons/themes/batiment_icon_grey.svg";
-import gestionEauIcon from "../../assets/icons/themes/gestionEau_icon_grey.svg";
-import infrastructureIcon from "../../assets/icons/themes/infrastructures_icon_grey.svg";
-import mobiliteIcon from "../../assets/icons/themes/mobilite_icon_grey.svg";
+import inconfortIcon from "../../assets/icons/themes/inconfort_thermique_icon_teal.svg";
+import gestionRisqueIcon from "../../assets/icons/themes/gestionRisques_icon_teal.svg";
+import amenagementIcon from "../../assets/icons/themes/amenagement_icon_teal.svg";
+import batimentIcon from "../../assets/icons/themes/batiment_icon_teal.svg";
+import gestionEauIcon from "../../assets/icons/themes/gestionEau_icon_teal.svg";
+import infrastructureIcon from "../../assets/icons/themes/infrastructures_icon_teal.svg";
+import mobiliteIcon from "../../assets/icons/themes/mobilite_icon_teal.svg";
+import santeIcon from "../../assets/icons/themes/sante_icon_teal.svg";
+import paysageIcon from "../../assets/icons/themes/paysages_icon_teal.svg";
+import qualiteAirEauIcon from "../../assets/icons/themes/qualiteAirEau_icon_teal.svg";
+import securiteIcon from "../../assets/icons/themes/securite_icon_teal.svg";
+import agricultureIcon from "../../assets/icons/themes/agriculture_icon_teal.svg";
+import filiereBoisIcon from "../../assets/icons/themes/bois_icon_teal.svg";
+import tourismeIcon from "../../assets/icons/themes/tourisme_icon_teal.svg";
+import biodiversiteIcon from "../../assets/icons/themes/biodiversite_icon_teal.svg";
+import ressourcesEauIcon from "../../assets/icons/themes/ressourcesEau_icon_teal.svg";
+import foretIcon from "../../assets/icons/themes/foret_icon_teal.svg";
+import espacesNaturelsIcon from "../../assets/icons/themes/espacesNaturels_icon_teal.svg";
 
 const thematiques = {
 	"Aménagement":
@@ -42,7 +53,61 @@ const thematiques = {
 			id: 6,
     	  	icon: mobiliteIcon,
 		},
-	
+	"Paysages":
+    	{
+			id: 7,
+    	  	icon: paysageIcon,
+		},
+	"Qualité de l'air et de l'eau":
+    	{
+			id: 8,
+    	  	icon: qualiteAirEauIcon,
+		},
+	"Santé":
+    	{
+			id: 9,
+    	  	icon: santeIcon,
+		},
+	"Sécurité (sinistralité)":
+    	{
+			id: 10,
+    	  	icon: securiteIcon,
+		},
+	"Agriculture":
+    	{
+			id: 11,
+    	  	icon: agricultureIcon,
+		},
+	"Filière bois":
+    	{
+			id: 12,
+    	  	icon: filiereBoisIcon,
+		},
+	"Tourisme":
+    	{
+			id: 13,
+    	  	icon: tourismeIcon,
+		},
+	"Écosystèmes et biodiversité":
+    	{
+			id: 14,
+    	  	icon: biodiversiteIcon,
+		},
+	"Ressources en eau":
+    	{
+			id: 15,
+    	  	icon: ressourcesEauIcon,
+		},
+	"Forêt":
+    	{
+			id: 16,
+    	  	icon: foretIcon,
+		},
+	"Espaces naturels":
+    	{
+			id: 17,
+    	  	icon: espacesNaturelsIcon,
+		},
 }
 
 export default thematiques;
