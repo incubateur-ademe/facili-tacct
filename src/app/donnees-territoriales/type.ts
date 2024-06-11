7ee98c43eac25a0702e9e81f5c1681f26fae266a
AntoineConegero <antoine.conegero@gmail.com>
2024-06-11 18:10:39 +0200

    Reorder graphs and data

--- src/app/donnees-territoriales/type.ts
+++ src/app/donnees-territoriales/type.ts
@@ -21,21 +21,22 @@ type CommunesTypes = {
     EPCI_CODE: string;
     ratio_precarite: number;
   };
-} | {
+}
+
+
+export type DataEPCI = {
   type: string;
+  features: EPCITypes[]
+}
+
+type EPCITypes = {
+  type: string;  
   geometry: {
-    type: string;
-    coordinates: number[][][];
-};
+      type: string;
+      coordinates: number[][][][];
+  };
   properties: {
-    DCOE_C_COD: string;
-    DDEP_C_COD: string;
-    DCOE_L_LIB: string;
-    REGION: string;
-    REGION_COD: string;
-    DEPARTEMEN: string;
+    EPCI_CODE: number;
     EPCI: string;
-    EPCI_CODE: string;
-    ratio_precarite: number;
-  }; 
+  };
 }
