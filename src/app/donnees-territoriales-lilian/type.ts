6a2ff450b207e844525ba8099610a18c4f8cc28b
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2024-06-11 14:52:05 +0200

    chore(lilian): test example

---
+++ src/app/donnees-territoriales-lilian/type.ts
@@ -0,0 +1,41 @@
+export type DataCommune = {
+  type: string;
+  name: string;
+  features: CommunesTypes[]
+}
+
+type CommunesTypes = {
+  type: string;  
+  geometry: {
+      type: string;
+      coordinates: number[][][][];
+  };
+  properties: {
+    DCOE_C_COD: string;
+    DDEP_C_COD: string;
+    DCOE_L_LIB: string;
+    REGION: string;
+    REGION_COD: string;
+    DEPARTEMEN: string;
+    EPCI: string;
+    EPCI_CODE: string;
+    ratio_precarite: number;
+  };
+} | {
+  type: string;
+  geometry: {
+    type: string;
+    coordinates: number[][][];
+};
+  properties: {
+    DCOE_C_COD: string;
+    DDEP_C_COD: string;
+    DCOE_L_LIB: string;
+    REGION: string;
+    REGION_COD: string;
+    DEPARTEMEN: string;
+    EPCI: string;
+    EPCI_CODE: string;
+    ratio_precarite: number;
+  }; 
+}
