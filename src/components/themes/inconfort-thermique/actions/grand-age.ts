76e44a1319e979477cf7325f2497e04a769f31a6
AntoineConegero <antoine.conegero@gmail.com>
2024-06-17 14:24:11 +0200

    travail exterieur graph + algo grand age

---
+++ src/components/themes/inconfort-thermique/actions/grand-age.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import data from "@/lib/json-db/age-evolution-all-ages.json";
+
+export async function getGrandAge() {
+  return data;
+}
+
+export async function getGrandAgeFromEPCI(code: number) {
+  const value = data.filter(el => el['EPCI - Métropole'] === code);
+
+  if (!value) {
+    throw new Error("GrandAge not found");
+  }
+
+  return value;
+}
