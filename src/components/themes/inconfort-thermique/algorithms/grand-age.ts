d73326a3cd7577a55215808716a5e5a8a831862e
AntoineConegero <antoine.conegero@gmail.com>
2024-06-17 17:17:09 +0200

    algo for GrandAge Graph

--- src/components/themes/inconfort-thermique/algorithms/grand-age.ts
+++ src/components/themes/inconfort-thermique/algorithms/grand-age.ts
@@ -33,61 +33,51 @@ interface GrandAge {
     "over_75_sum_2020": number
   }
 
-function sumProperty (items: GrandAge[], prop: any) {
-  return items.reduce(function(a, b) {
-      return a + b[prop];
-  }, 0);
-};
+// const sumProperty = (items: GrandAge[], prop: any) => {
+//   return items.reduce(function(a, b) {
+//       return a + b[prop];
+//   }, 0);
+// };
 
+type Element = {
+    [key: string]: number,
+}
+type Percent = {
+    "id": string;
+    "value": number;
+}
 
-const columns_1968 = [
-    'under_4_sum_1968',
-    '4_to_75_sum_1968',
-    'over_75_sum_1968',
-];
-
-const columns_1975 = [
-    'under_4_sum_1975',
-    '4_to_75_sum_1975',
-    'over_75_sum_1975',
-];
-
-const columns_1982 = [
-    'under_4_sum_1982',
-    '4_to_75_sum_1982',
-    'over_75_sum_1982',
-];
-
-const columns_1990 = [
-    'under_4_sum_1990',
-    '4_to_75_sum_1990',
-    'over_75_sum_1990',
-];
-
-const columns_2009 = [
-    'under_4_sum_2009',
-    '4_to_75_sum_2009',
-    'over_75_sum_2009',
-];
-
-const columns_2014 = [
-    'under_4_sum_2014',
-    '4_to_75_sum_2014',
-    'over_75_sum_2014',
-];
-
-const columns_2020 = [
-    'under_4_sum_2020',
-    '4_to_75_sum_2020',
-    'over_75_sum_2020',
-];
+const average = (array: number[]) => array.reduce((a: number, b: number) => a + b) / array.length;
 
 export const GrandAgeAlgo = (data: GrandAge[]) => {
-    const values_2020 = data.map(row => [...columns_2020].reduce((acc, v) => ({ ...acc, [v]: row[v] }), {}));
-    console.log('values_2020', values_2020)
-    const sum_2020 = Object.values(values_2020[0]).reduce((partialSum: number, a: number) => partialSum + a, 0)
-    // const sum_2020 = sumProperty(Object.values(values_2020), 'NA5FZ_sum');
+    const percent_75_allRows: Percent[] = [];
+    const final_percents = [];
+    const years = ['1968', '1975', '1982', '1990', '1999', '2009', '2014', '2020'];
+    let i = 0;
 
-    console.log('sum2020', sum_2020)
-    return values_2020;
+    while (i < years.length) {
+        const year = years[i];
+        const columns = [
+            'under_4_sum_' + year.toString(),
+            '4_to_75_sum_' + year.toString(),
+            'over_75_sum_' + year.toString(),
+        ];
+        const values = data.map((row: GrandAge | any) => [...columns].reduce((acc, v) => ({ ...acc, [v]: row[v] }), {})); //REPLACE
+        values.forEach((el: Element) => {
+            let sum_1 = Object.values(el).reduce((partialSum, a) => Number(partialSum) + Number(a), 0);
+            let textColumn: string = 'over_75_sum_' + year.toString();
+            let percent_75_more = 100 * el[textColumn] / Number(sum_1);
+            percent_75_allRows.push({ "id": year.toString(), "value": percent_75_more});
+        });
+        i++;
+    }
+
+    const groupBy = Object.groupBy(percent_75_allRows, ({ id }) => id);
+    for (let step = 0; step < Object.values(groupBy).length; step++) {
+        const final = Object.values(groupBy)[step]!.map(e => e.value);
+        let mean_of_percents = average(final);
+        final_percents.push(mean_of_percents)
+      }
+
+    return final_percents;
 }
