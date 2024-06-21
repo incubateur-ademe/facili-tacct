19abf4c2e8ac72056b9889ab7a1a5cb1a4ee3a1c
AntoineConegero <antoine.conegero@gmail.com>
2024-06-21 13:55:07 +0200

    delete old pages + add components in constellation

--- src/app/explication/components/tourisme.tsx
+++ src/app/explication/components/tourisme.tsx
@@ -1,13 +1,23 @@
 const Tourisme = () => {
   return (
     <div>
-      <p>Bonjour</p>
       <h5>Tourisme</h5>
       <div>
+        <h6>QUI CONVAINCRE ET POURQUOI ?</h6>
+        <div>
+          <p>
+            Les vagues de chaleur ont d'ores et déjà des conséquences sur la fréquentation touristique de certains sites ou de certains types d’hébergements.
+          </p>
+          <p>
+            L'enjeu le plus immédiat est le calibrage des dispositifs de prévention et de gestion de crise (alerte canicule) du fait 
+          que la population exposée est bien plus nombreuse que la seule population résidente.
+          </p>
+        </div>
+
         <p>
-          <b>Atelier </b>
+          Mais si le tourisme est un enjeu prioritaire de votre territoire, il est important d'associer les 
+          acteurs de ce secteur aux plans d'actions sur l'inconfort thermique
         </p>
-        <p>lorem ipsum sed dolor</p>
       </div>
     </div>
   );
