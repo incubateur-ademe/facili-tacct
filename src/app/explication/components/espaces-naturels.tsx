d104807e997d767553857e95991b60cf2f696534
AntoineConegero <antoine.conegero@gmail.com>
2024-06-21 13:55:07 +0200

    delete old pages + add components in constellation

---
+++ src/app/explication/components/espaces-naturels.tsx
@@ -0,0 +1,24 @@
+const EspacesNaturels = () => {
+  return (
+    <div>
+      <h5>Espaces naturels</h5>
+      <div>
+        <h6>QUI CONVAINCRE ET POURQUOI ?</h6>
+        <div>
+          <p>
+            ...........................
+          </p>
+          <p>
+            ...................
+          </p>
+        </div>
+
+        <p>
+          ...............
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default EspacesNaturels;
---
+++ src/app/explication/components/gestion-eau.tsx
@@ -0,0 +1,24 @@
+const GestionEau = () => {
+  return (
+    <div>
+      <h5>Gestion de l'eau</h5>
+      <div>
+        <h6>QUI CONVAINCRE ET POURQUOI ?</h6>
+        <div>
+          <p>
+            ...........................
+          </p>
+          <p>
+            ...................
+          </p>
+        </div>
+
+        <p>
+          ...............
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default GestionEau;
