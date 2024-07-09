e3f64936fb05e5902701fd1a661a07bdf243bcc8
AntoineConegero <antoine.conegero@gmail.com>
2024-07-09 17:04:10 +0200

    prepared request raw sql with prisma + postgis

--- src/app/_test/page.tsx
+++ src/app/_test/page.tsx
@@ -1,18 +1,16 @@
 import { Get_Prisma } from "./prismafunc";
-import { sql } from '@vercel/postgres';
 
 export default async function Test() {
   // const [data, setData] = useState<any>();
   const db_filtered = await Get_Prisma();
   // console.log('db_filtered', db_filtered)
-
   return (
-    <div style={{ margin: "2rem" }}>
+    <div style={{ margin: "2rem" }} > 
       {db_filtered.map((el, i) => (
         <ul key={i}>
           <li>{el.shape_area}</li>
-        </ul>
+        </ul> 
       ))}
-    </div>
+    </div> 
   );
 }
