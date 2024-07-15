8a2d0ad05ae7869006f05ffe7a3bd10cb9d4ce3c
AntoineConegero <antoine.conegero@gmail.com>
2024-07-15 17:08:18 +0200

    query to display map based on url code

--- src/app/donnees-territoriales/queries.tsx
+++ src/app/donnees-territoriales/queries.tsx
@@ -5,31 +5,29 @@ const PrismaPostgres = new PostgresClient();
 
 //Paris lat : 48.864716 long : 2.349014
 
-export const Get_Communes = async () => {
-  console.time("Query Execution Time");
-  const value = await PrismaPostgres.$queryRaw`
-    SELECT 
-    epci, 
-    libelle_epci,
-    libelle_commune,
-    code_commune, 
-    precarite_logement,
-    densite_bati,
-    ST_AsGeoJSON(geometry) geometry 
-    FROM postgis."communes" WHERE epci=${'200069193'}`
-  console.timeEnd("Query Execution Time");
-
-  return value;
+export const Get_Communes = async (code: string) => {
+  try {
+    console.time("Query Execution Time");
+    const value = await PrismaPostgres.$queryRaw`
+      SELECT 
+      epci, 
+      libelle_epci,
+      libelle_commune,
+      code_commune, 
+      precarite_logement,
+      densite_bati,
+      ST_AsGeoJSON(geometry) geometry 
+      FROM postgis."communes" WHERE epci=${code};`
+    console.timeEnd("Query Execution Time");
+    // console.log(code)
+    // console.log(value)
+    return value;
+   } catch(error) {
+      console.error(error);
+      await PrismaPostgres.$disconnect();
+      process.exit(1);
+   }
 };
-
-Get_Communes()
-  .then(async () => {
-    await PrismaPostgres.$disconnect();
-  })
-  .catch(async e => {
-    console.error(e);
-    await PrismaPostgres.$disconnect();
-    process.exit(1);
-  });
+  
 
 
