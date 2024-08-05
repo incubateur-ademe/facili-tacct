77e735b92fcb8c8182f0aed2344e0670f35272e8
AntoineConegero <antoine.conegero@gmail.com>
2024-08-05 18:20:39 +0200

    searchbar requests communes + epci

---
+++ src/app/searchBar/type.ts
@@ -0,0 +1,10 @@
+export type DbFiltered = {
+  code_commune: string;
+  coordinates: string;
+  densite_bati: number;
+  epci: string;
+  libelle_commune: string;
+  libelle_epci: string;
+  precarite_logement: number;
+  geometry: string;
+};
