a3c6f90a07d853d5b8eb3f1266fe70ce0649b2fb
AntoineConegero <antoine.conegero@gmail.com>
2024-08-07 14:25:56 +0200

    add searchbar to landing page

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,9 +1,9 @@
 /* eslint typescript-sort-keys/interface: error */
-import type { ReactNode } from "react";
+import { type ReactNode } from "react";
 
 export type SearchResult = {
-    children: ReactNode;
-    id: string;
-    prefix?: ReactNode;
-    route: string;
+  children: ReactNode;
+  id: string;
+  prefix?: ReactNode;
+  route: string;
 };
---
+++ src/lib/queries/searchBar.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import { type DbFiltered } from "@/app/donnees-territoriales/type";
+
+import { PrismaClient as PostgresClient } from "../../generated/client";
+
+const PrismaPostgres = new PostgresClient();
+
+export const GetCollectivite = async (collectivite: string): Promise<DbFiltered[]> => {
+  try {
+    console.time("Query Execution Time");
+    const variable_commune = collectivite + "%";
+    const variable_code_commune = collectivite + "%";
+    const variable_epci = "%" + collectivite + "%";
+    if (isNaN(parseInt(collectivite))) {
+      const value = await PrismaPostgres.$queryRaw<DbFiltered[]>`
+      SELECT 
+      epci, 
+      libelle_epci,
+      libelle_commune,
+      code_commune
+      FROM postgis."communes2" WHERE libelle_commune ILIKE ${variable_commune} OR libelle_epci ILIKE ${variable_epci} LIMIT 30;`;
+      console.timeEnd("Query Execution Time");
+      // console.log(value);
+      return value;
+    } else if (typeof parseInt(collectivite) === "number") {
+      const value = await PrismaPostgres.$queryRaw<DbFiltered[]>`
+      SELECT 
+      epci, 
+      libelle_epci,
+      libelle_commune,
+      code_commune
+      FROM postgis."communes2" WHERE code_commune ILIKE ${variable_code_commune} LIMIT 20;`;
+      console.timeEnd("Query Execution Time");
+
+      return value;
+    } else {
+      return [
+        {
+          code_commune: "",
+          coordinates: "",
+          densite_bati: 0,
+          epci: "",
+          libelle_commune: "",
+          libelle_epci: "",
+          precarite_logement: 0,
+          geometry: "",
+        },
+      ];
+    }
+  } catch (error) {
+    console.error(error);
+    await PrismaPostgres.$disconnect();
+    process.exit(1);
+  }
+};
