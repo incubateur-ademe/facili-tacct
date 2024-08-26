b9eff577145a134a12aecf450f212f064ab96dab
AntoineConegero <antoine.conegero@gmail.com>
2024-08-26 16:15:26 +0200

    search by commune + page budget

--- src/app/ressources/diagnostic.tsx
+++ src/app/ressources/diagnostic.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { cards, ressourcesTabs } from "@/lib/utils/ressources";
 import { useState } from "react";
 import { CardComp } from "./CustomCard";
 import styles from "./ressources.module.scss";
@@ -17,137 +18,16 @@ const DiagnosticComp = () => {
     setSelectedThemeId(tab);
   }
 
-  const diagnosticTabs = [
-    {
-      id: 0,
-      titre: "Vous n'avez pas de diagnostic",
-    },
-    {
-      id: 1,
-      titre: "Vous découvrez le diagnostic pour la 1ère fois",
-    },
-    {
-      id: 2,
-      titre: "Vous voulez réviser un diagnostic connu",
-    }
-  ]
-
-  const themesTabs = [
-    {
-      id: 0,
-      titre: "Inconfort thermique",
-    },
-    {
-      id: 1,
-      titre: "Thématique 2",
-      disabled: true
-    },
-    {
-      id: 2,
-      titre: "Thématique 3",
-      disabled: true
-    }
-  ]
-
-  const allComps = [
-    {
-      id: 0,
-      tab : "Vous n'avez pas de diagnostic",
-      titre: 'Atelier "Mémoire"',
-      tag: "Atelier",
-      description:"Faites appel à la mémoire collective pour débuter sur l'exposition de votre territoire aux impacts du changement climatique.",
-      link: ""
-    },
-    {
-      id: 1,
-      tab : "Vous n'avez pas de diagnostic",
-      titre: 'Constituer une équipe "cœur"',
-      tag: "Retour d'expérience",
-      description: "Découvrez comment et pourquoi la Communauté de Communes du Bocage Bourbonnais (03) a mis en place une « équipe cœur » afin de piloter sa démarche TACCT.",
-      link: "https://librairie.ademe.fr/7214-piloter-sa-demarche-tacct-retour-d-experience-dans-le-bocage-bourbonnais-03.html"
-    },
-    {
-      id: 2,
-      tab : "Vous n'avez pas de diagnostic",
-      titre: "Faire son diagnostic de vulnérabilité",
-      tag: "Retour d'expérience",
-      description: "Retour d'expérience sur la réalisation d'un diagnostic de vulnérabilité avec TACCT à Epernay Agglo Champagne (51).",
-      link: "https://www.climaxion.fr/docutheque/realisation-dun-diagnostic-vulnerabilite-au-changement-climatique-epernay-agglo-champagne"
-    },
-    {
-      id: 3,
-      tab : "Vous n'avez pas de diagnostic",
-      titre: "Titre",
-      tag: "Article",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing, incididunt, ut labore et dolore magna aliqua. Vitae sapien pellentesque habitant morbi tristique senectus et",
-      link: ""
-    },
-    {
-      id: 4,
-      tab : "Vous découvrez le diagnostic pour la 1ère fois",
-      titre: "Restituer son diagnostic de vulnérabilité",
-      tag: "Retour d'expérience",
-      description: "Ils l'ont fait ! Retour d'expérience sur la restitution de son diagnostic de vulnérabilité en Haute Loire (43).",
-      link: "https://librairie.ademe.fr/7180-comment-restituer-son-diagnostic-des-impacts-du-changement-climatique-en-haute-loire-43.html"
-    },
-    {
-      id: 5,
-      tab : "Vous voulez réviser un diagnostic connu",
-      titre: "Évaluation en atelier",
-      tag: "Retour d'expérience",
-      description: 'Un atelier "sensible" pour évaluer les impacts du changement climatique au PNR des Pyrénées Ariégeoises (09).',
-      link: "https://librairie.ademe.fr/changement-climatique-et-energie/6223-evaluation-en-atelier-de-la-sensibilite-du-territoire-au-changement-climatique-au-pnr-pyrenees-ariegeoises-09.html"
-    },
-  ]
-
-  const inconfortThermique = [
-    {
-      id: 0,
-      tab: "Inconfort thermique",
-      titre: 'Plus Fraîche Ma Ville',
-      tag: "Article",
-      description:"Trouvez les bonnes solutions pour rafraîchir durablement votre collectivité.",
-      link: "https://www.adaptation-changement-climatique.gouv.fr/dossiers-thematiques/milieux/foret"
-    },
-    {
-      id: 1,
-      tab: "Inconfort thermique",
-      titre: 'CRACC',
-      tag: "Article",
-      description: "Canicule : à quoi s’attendre et comment s’adapter ?",
-      link: "https://www.adaptation-changement-climatique.gouv.fr/dossiers-thematiques/impacts/canicule"
-    },
-  ]
-
-  const cartesPermanentes = [
-    {
-      id: 0,
-      tab: "",
-      titre: 'Rejoignez la communauté',
-      tag: "",
-      description: "Donec et nisl id sapien blandit mattis. Aenean dictum odio sit amet risus. Morbi purus. Nulla a est sit amet purus venenatis iaculis. Vivamus viverra purus vel magna. Donec in justo sed odio malesuada dapibus.",
-      link: ""
-    },
-    {
-      id: 1,
-      tab: "",
-      titre: 'Découvrez la méthode TACCT',
-      tag: "",
-      description: "Découvrez la méthode TACCT pour élaborer une politique d’adaptation au changement climatique de « A à Z », du diagnostic de vulnérabilité jusqu’au suivi des mesures et à l’évaluation de la stratégie.",
-      link: "https://tacct.ademe.fr/"
-    },
-  ]
-
   return (
     <div>
       <div className={styles.ressourcesWrapper} style={{ padding: "0 0 4em 0" }}>
         <TabComp 
           defaultTab="Vous n'avez pas de diagnostic"
-          data={diagnosticTabs}
+          data={ressourcesTabs.diagnostic}
           handleTab={handleTab}
         />
         <div className={styles.cardsWrapper}>
-          {allComps.map((el, i) => (
+          {cards.diagnostic.map((el, i) => (
             el.tab === selectedTabId ? 
               <CardComp 
                 key={i}
@@ -162,11 +42,11 @@ const DiagnosticComp = () => {
       <div className={styles.ressourcesWrapper}>
         <TabComp 
           defaultTab="Inconfort thermique"
-          data={themesTabs}
+          data={ressourcesTabs.themes}
           handleTab={handleTheme}
         />
         <div className={styles.cardsWrapper}>
-          {inconfortThermique.map((el, i) => (
+          {cards.inconfortThermique.map((el, i) => (
             el.tab === selectedThemeId ? 
               <CardComp 
                 key={i}
@@ -180,7 +60,7 @@ const DiagnosticComp = () => {
       </div>
       <div className={styles.ressourcesWrapper}>
         <div className={styles.cardsWrapper}>
-          {cartesPermanentes.map((el, i) => (
+          {cards.cartesPermanentes.map((el, i) => (
             <CardComp 
               key={i}
               description={el.description}
