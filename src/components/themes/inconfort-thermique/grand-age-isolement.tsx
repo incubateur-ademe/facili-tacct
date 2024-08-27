742722245ed3d826bcb7e37317d3786ed17fa575
AntoineConegero <antoine.conegero@gmail.com>
2024-08-27 17:46:24 +0200

    data for communes + epci

--- src/components/themes/inconfort-thermique/grand-age-isolement.tsx
+++ src/components/themes/inconfort-thermique/grand-age-isolement.tsx
@@ -86,44 +86,45 @@ export const GrandAgeIsolement = (props: {
     titre: string;
   }>;
 }) => {
-  const { inconfort_thermique, data } = props;
+  const { inconfort_thermique } = props;
   const searchParams = useSearchParams();
-  const code = searchParams.get("code")!;
+  const codgeo = searchParams.get("codgeo")!;
+  const codepci = searchParams.get("codepci")!;
   const xData: string[] = ["1968", "1975", "1982", "1990", "1999", "2009", "2014", "2020"];
-  const temp_db = inconfort_thermique.map(grandAgeIsolementMapper);
+  const grandAgeIsolement = inconfort_thermique.map(grandAgeIsolementMapper);
 
   const yData = {
     over_80_1968_percent_epci: (
-      (100 * sumProperty(temp_db, "over_80_sum_1968")) /
-      (sumProperty(temp_db, "to_80_sum_1968") + sumProperty(temp_db, "under_4_sum_1968"))
+      (100 * sumProperty(grandAgeIsolement, "over_80_sum_1968")) /
+      (sumProperty(grandAgeIsolement, "to_80_sum_1968") + sumProperty(grandAgeIsolement, "under_4_sum_1968"))
     ).toFixed(2),
     over_80_1975_percent_epci: (
-      (100 * sumProperty(temp_db, "over_80_sum_1975")) /
-      (sumProperty(temp_db, "to_80_sum_1975") + sumProperty(temp_db, "under_4_sum_1975"))
+      (100 * sumProperty(grandAgeIsolement, "over_80_sum_1975")) /
+      (sumProperty(grandAgeIsolement, "to_80_sum_1975") + sumProperty(grandAgeIsolement, "under_4_sum_1975"))
     ).toFixed(2),
     over_80_1982_percent_epci: (
-      (100 * sumProperty(temp_db, "over_80_sum_1982")) /
-      (sumProperty(temp_db, "to_80_sum_1982") + sumProperty(temp_db, "under_4_sum_1982"))
+      (100 * sumProperty(grandAgeIsolement, "over_80_sum_1982")) /
+      (sumProperty(grandAgeIsolement, "to_80_sum_1982") + sumProperty(grandAgeIsolement, "under_4_sum_1982"))
     ).toFixed(2),
     over_80_1990_percent_epci: (
-      (100 * sumProperty(temp_db, "over_80_sum_1990")) /
-      (sumProperty(temp_db, "to_80_sum_1990") + sumProperty(temp_db, "under_4_sum_1990"))
+      (100 * sumProperty(grandAgeIsolement, "over_80_sum_1990")) /
+      (sumProperty(grandAgeIsolement, "to_80_sum_1990") + sumProperty(grandAgeIsolement, "under_4_sum_1990"))
     ).toFixed(2),
     over_80_1999_percent_epci: (
-      (100 * sumProperty(temp_db, "over_80_sum_1999")) /
-      (sumProperty(temp_db, "to_80_sum_1999") + sumProperty(temp_db, "under_4_sum_1999"))
+      (100 * sumProperty(grandAgeIsolement, "over_80_sum_1999")) /
+      (sumProperty(grandAgeIsolement, "to_80_sum_1999") + sumProperty(grandAgeIsolement, "under_4_sum_1999"))
     ).toFixed(2),
     over_80_2009_percent_epci: (
-      (100 * sumProperty(temp_db, "over_80_sum_2009")) /
-      (sumProperty(temp_db, "to_80_sum_2009") + sumProperty(temp_db, "under_4_sum_2009"))
+      (100 * sumProperty(grandAgeIsolement, "over_80_sum_2009")) /
+      (sumProperty(grandAgeIsolement, "to_80_sum_2009") + sumProperty(grandAgeIsolement, "under_4_sum_2009"))
     ).toFixed(2),
     over_80_2014_percent_epci: (
-      (100 * sumProperty(temp_db, "over_80_sum_2014")) /
-      (sumProperty(temp_db, "to_80_sum_2014") + sumProperty(temp_db, "under_4_sum_2014"))
+      (100 * sumProperty(grandAgeIsolement, "over_80_sum_2014")) /
+      (sumProperty(grandAgeIsolement, "to_80_sum_2014") + sumProperty(grandAgeIsolement, "under_4_sum_2014"))
     ).toFixed(2),
     over_80_2020_percent_epci: (
-      (100 * sumProperty(temp_db, "over_80_sum_2020")) /
-      (sumProperty(temp_db, "to_80_sum_2020") + sumProperty(temp_db, "under_4_sum_2020"))
+      (100 * sumProperty(grandAgeIsolement, "over_80_sum_2020")) /
+      (sumProperty(grandAgeIsolement, "to_80_sum_2020") + sumProperty(grandAgeIsolement, "under_4_sum_2020"))
     ).toFixed(2),
   };
 
@@ -136,10 +137,17 @@ export const GrandAgeIsolement = (props: {
         <div className={styles.container}>
           <div className="w-2/5">
             <div className={styles.explicationWrapper}>
-              <p style={{color: "#161616", margin:"0 0 0.5em"}}>
-                Dans l'EPCI {temp_db[0]?.libelle_epci} les personnes de plus de 80 ans représentent{" "}
-                <b>{yData.over_80_2020_percent_epci}%</b> de la population en 2020.
-              </p>
+              { codgeo ?
+                <p style={{color: "#161616", margin:"0 0 0.5em"}}>
+                  Dans la commune de {grandAgeIsolement[0]?.libelle_geographique}, les personnes de plus de 80 ans représentent{" "}
+                  <b>{yData.over_80_2020_percent_epci}%</b> de la population en 2020.
+                </p>
+                : 
+                <p style={{color: "#161616", margin:"0 0 0.5em"}}>
+                  Dans l'EPCI {grandAgeIsolement[0]?.libelle_epci}, les personnes de plus de 80 ans représentent{" "}
+                  <b>{yData.over_80_2020_percent_epci}%</b> de la population en 2020.
+                </p>
+              }
               <CustomTooltip title={title} />
             </div>
             <div className="px-4">
@@ -173,7 +181,7 @@ export const GrandAgeIsolement = (props: {
           </div>
         </div>
       ) : (
-        <GraphDataNotFound code={code} />
+        <GraphDataNotFound code={codgeo ? codgeo : codepci} />
       )}
     </>
   );
