a379bdbc5680ab10ea3126693ad797f68cde2822
AntoineConegero <antoine.conegero@gmail.com>
2024-08-29 17:19:47 +0200

    fix FTT-34

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 "use client";
 import ressourcesIcon from "@/assets/icons/ressources_icon_blue.svg";
-import ressourcesIconWhite from "@/assets/icons/ressources_icon_white.svg";
 import { config } from "@/config";
 import Badge from "@codegouvfr/react-dsfr/Badge";
 import Header from "@codegouvfr/react-dsfr/Header";
@@ -67,29 +66,27 @@ export const HeaderComp = () => {
             <Localisation libelle={epci} code={codepci}/>
           </>
          ) : "",
-        params.includes("ressources") ? (
-          <>
-            <Button
-              key="0"
-              startIcon={<Image src={ressourcesIconWhite as StaticImageData} alt=""></Image>}
-              sx={{
-                textTransform: "none",
-                color: "white",
-                borderRadius: "4px",
-                border: "1px solid #0063CB",
-                padding: "0.5em 1em",
-                fontWeight: 500,
-                fontFamily: "inherit",
-                fontSize: "1rem",
-                height: "48px",
-                top: "-0.5em",
-                margin: "0 0 0 1em",
-                backgroundColor: "#0063CB",
-              }}
-            >
-              Ressources
-            </Button>
-          </>
+        params.includes("ressources") ? ( ""
+            // <Button
+            //   key="0"
+            //   startIcon={<Image src={ressourcesIconWhite as StaticImageData} alt=""></Image>}
+            //   sx={{
+            //     textTransform: "none",
+            //     color: "white",
+            //     borderRadius: "4px",
+            //     border: "1px solid #0063CB",
+            //     padding: "0.5em 1em",
+            //     fontWeight: 500,
+            //     fontFamily: "inherit",
+            //     fontSize: "1rem",
+            //     height: "48px",
+            //     top: "-0.5em",
+            //     margin: "0 0 0 1em",
+            //     backgroundColor: "#0063CB",
+            //   }}
+            // >
+            //   Ressources
+            // </Button>
         ) : (
           <Button
             key="0"
