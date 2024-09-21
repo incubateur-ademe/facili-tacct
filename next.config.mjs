192b9ac94bd03144f3be88c0c10a1131c677e453
AntoineConegero <antoine.conegero@gmail.com>
2024-09-21 14:24:13 +0200

    activate cross origin policies

--- next.config.mjs
+++ next.config.mjs
@@ -113,14 +113,14 @@ const config = {
               key: "Cross-Origin-Embedder-Policy",
               value: "credentialless",
             },
-            // {
-            //   key: "Cross-Origin-Opener-Policy",
-            //   value: "same-origin",
-            // },
-            // {
-            //   key: "Cross-Origin-Resource-Policy",
-            //   value: "cross-origin",
-            // },
+            {
+              key: "Cross-Origin-Opener-Policy",
+              value: "same-origin",
+            },
+            {
+              key: "Cross-Origin-Resource-Policy",
+              value: "cross-origin",
+            },
             // {
             //     key: "Access-Control-Allow-Origin",
             //     value: "*"
