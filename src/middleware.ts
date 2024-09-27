268b9f0b9390261c39941181474ab4cf076ce2b4
AntoineConegero <antoine.conegero@gmail.com>
2024-09-27 18:19:56 +0200

    auth test

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,3 +14,10 @@
 // export const config = {
 //   matcher: "/",
 // };
+
+export { default } from "next-auth/middleware";
+
+export const config = {
+  // specify the route you want to protect
+  matcher: ["/ressources"],
+};
