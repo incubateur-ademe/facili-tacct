6209e245786f32bd49fecbb1a02102fbc2545093
AntoineConegero <antoine.conegero@gmail.com>
2024-09-28 14:22:20 +0200

    redirection after login and hashed password

---
+++ src/app/api/ressources/add/page.tsx
@@ -0,0 +1,22 @@
+
+const ApiRessourcesAdd = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1>Add an article</h1>
+      <p>Page to add an article</p>
+      <form className="flex flex-col items-center gap-y-6">
+        <label className="flex gap-8">
+          Title
+          <input name="title" type="text" className="bg-slate-100"/>
+        </label>
+        <label className="flex gap-8">
+          Content
+          <textarea name="content" className="bg-slate-100"/>
+        </label>
+        <button type="submit">Add article</button>
+      </form>
+    </div>
+  );
+};
+
+export default ApiRessourcesAdd;
