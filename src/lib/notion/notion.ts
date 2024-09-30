362469bfd666362b98a5597305acb39007770bf9
AntoineConegero <antoine.conegero@gmail.com>
2024-09-30 21:14:08 +0200

    fetch text blocks

---
+++ src/lib/notion/notion.ts
@@ -0,0 +1,20 @@
+import { Client } from "@notionhq/client";
+
+const notion = new Client({
+  auth: process.env.NOTION_KEY,
+
+});
+
+const pageId = process.env.NOTION_ID;
+export const getPage = async () => {
+  const response = await notion.pages.retrieve({ page_id: pageId! });
+  return response;
+};
+
+export const getBlocks = async (blockId: string) => {
+  const response = await notion.blocks.children.list({
+    block_id: blockId,
+    page_size: 50,
+  });
+  return response.results;
+};
