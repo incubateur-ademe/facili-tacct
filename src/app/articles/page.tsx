220b7ba7b71d8627500d420cc42ead7e94324508
AntoineConegero <antoine.conegero@gmail.com>
2024-10-08 00:27:00 +0200

    front for headings

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,5 +1,6 @@
 import { getBlocks, getPage } from "@/lib/notion/notion";
 import { Text } from "./Text";
+import styles from './text.module.css';
 
 const ArticlesApi = async () => {
   const id = process.env.NOTION_ID;
@@ -13,31 +14,32 @@ const ArticlesApi = async () => {
         case "paragraph":
           const paragraphtexts = value?.rich_text ? value?.rich_text : [];
           return (
-            <div key={i}>
+            <div className={styles.paragraph} key={i}>
               <p><Text text={paragraphtexts} /></p>
             </div>
           );
         case "heading_1":
           const headingh1Texts = value?.rich_text;
           return (
-            <div key={i}>
+            <div className={styles.heading1} key={i}>
               <h1><Text text={headingh1Texts} /></h1>
             </div>
           );
         case "heading_2":
           const headingh2Texts = value?.rich_text;
           return (
-            <div key={i}>
+            <div className={styles.heading2} key={i}>
               <h2><Text text={headingh2Texts} /></h2>
             </div>
           );
         case "heading_3":
           const headingh3Texts = value?.rich_text;
           return (
-            <div key={i}>
+            <div className={styles.heading3} key={i}>
               <h3><Text text={headingh3Texts} /></h3>
             </div>
           );
+
         // case "bulleted_list_item":
         // case "numbered_list_item":
         //   return (
@@ -50,8 +52,8 @@ const ArticlesApi = async () => {
         case "image":
           const src = value.type === "external" ? value?.external?.url : value?.file?.url;
           return (
-            <div key={i} className="flex justify-center">
-              <img src={src} />
+            <div key={i} className="flex justify-center" >
+              <img src={src} style={{maxWidth:"600px", margin:"3em"}}/>
             </div>
           );
         default:
@@ -64,7 +66,7 @@ const ArticlesApi = async () => {
   const pageContent = getTexts(getBlocksContent);
 
   return (
-    <div className="m-10">
+    <div style={{margin:"6em auto", maxWidth:"650px", display:"flex", flexDirection:"column", alignItems:"center"}}>
       {/* <h1>{getPageContent?.properties?.title.title.map((el: any) => el.plain_text)}</h1> */}
       {pageContent}
     </div>
