8d44d2228c8ccc65a3aa2ad13d2c9e1baae7dce3
AntoineConegero <antoine.conegero@gmail.com>
2024-10-13 21:37:15 +0200

    redirect with new url articles

--- src/app/articles/router.tsx
+++ src/app/articles/router.tsx
@@ -1,16 +1,25 @@
 "use client"
+import { cards } from "@/lib/ressources/notionCards";
 import { useRouter } from "next/navigation";
+import { CardComp } from "../ressources/CustomCard";
 
 const RouterArticles = ({ params }: { params: string }) => {
   const router = useRouter();
-  const handleClick = (e: string) => {
-    return () => {
-      router.push(`/articles/${e}`);
-    }
-  }
 
   return (
     <div>
+      {cards.diagnostic.map((el, i) => (
+        <CardComp 
+          key={i}
+          description={el.description}
+          titre={el.titre}
+          link={`${el.link}/${params}`}
+          backgroundColor="#E3E3FD"
+          textColor="#161616"
+          titleColor="#161616"
+          logoColor="#000091"
+        />))
+      }
       <button onClick={() => router.push(`/articles/${params}`)}>
         {params}
       </button>
