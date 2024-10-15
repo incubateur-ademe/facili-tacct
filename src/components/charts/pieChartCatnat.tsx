a8850ec1c7bd335fca83b5342faef70e252d9ae9
AntoineConegero <antoine.conegero@gmail.com>
2024-10-15 20:43:14 +0200

    slider comp with pie

--- src/components/charts/pieChartCatnat.tsx
+++ src/components/charts/pieChartCatnat.tsx
@@ -1,43 +1,41 @@
+"use client";
+
+import { CountOcc } from '@/lib/utils/reusableFunctions/occurencesCount';
 import { ResponsivePie } from '@nivo/pie';
 
-const data = [
-  {
-    "id": "erlang",
-    "label": "erlang",
-    "value": 469,
-    "color": "hsl(63, 70%, 50%)"
-  },
-  {
-    "id": "c",
-    "label": "c",
-    "value": 445,
-    "color": "hsl(34, 70%, 50%)"
-  },
-  {
-    "id": "elixir",
-    "label": "elixir",
-    "value": 76,
-    "color": "hsl(194, 70%, 50%)"
-  },
-  {
-    "id": "java",
-    "label": "java",
-    "value": 265,
-    "color": "hsl(52, 70%, 50%)"
-  },
-  {
-    "id": "rust",
-    "label": "rust",
-    "value": 462,
-    "color": "hsl(178, 70%, 50%)"
-  }
-]
+type ArreteCatNat = {
+  annee_arrete: number;
+  lib_risque_jo: string | null;
+  dat_pub_arrete: string | null;
+  code_geographique: string | null;
+  departement: string | null;
+  epci: string | null;
+  index: bigint | null;
+  libelle_epci: string | null;
+  libelle_geographique: string | null;
+  region: number | null;
+}
 
-const PieChartCatnat = () => (
-  <div style={{ height: "500px", minWidth: "450px", backgroundColor: "white" }}>
-    <ResponsivePie
-      data={data}
-      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+const PieChartCatnat = (props: {gestionRisques: ArreteCatNat[]}) => {
+  const { gestionRisques } = props;
+  const countTypes = CountOcc(gestionRisques, "lib_risque_jo");
+
+  const mapGraphData = gestionRisques?.map((el) => {
+    return {
+      id: el.lib_risque_jo ?? "",
+      label: el.lib_risque_jo ?? "",
+      value: countTypes[el.lib_risque_jo!],
+    }
+  })
+  const graphData = mapGraphData.filter(
+    (value, index, self) => index === self.findIndex(t => t.label === value.label),
+  );
+  return (
+    <div style={{ height: "500px", minWidth: "450px", backgroundColor: "white" }}>
+      <ResponsivePie
+        data={graphData}
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        isInteractive={false}
         innerRadius={0.5}
         padAngle={0.7}
         cornerRadius={3}
@@ -57,87 +55,9 @@ const PieChartCatnat = () => (
         arcLinkLabelsThickness={2}
         arcLinkLabelsColor={{ from: 'color' }}
         arcLabelsSkipAngle={10}
-        arcLabelsTextColor={{
-            from: 'color',
-            modifiers: [
-                [
-                    'darker',
-                    2
-                ]
-            ]
-        }}
-        defs={[
-            {
-                id: 'dots',
-                type: 'patternDots',
-                background: 'inherit',
-                color: 'rgba(255, 255, 255, 0.3)',
-                size: 4,
-                padding: 1,
-                stagger: true
-            },
-            {
-                id: 'lines',
-                type: 'patternLines',
-                background: 'inherit',
-                color: 'rgba(255, 255, 255, 0.3)',
-                rotation: -45,
-                lineWidth: 6,
-                spacing: 10
-            }
-        ]}
-        fill={[
-            {
-                match: {
-                    id: 'ruby'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'c'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'go'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'python'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'scala'
-                },
-                id: 'lines'
-            },
-            {
-                match: {
-                    id: 'lisp'
-                },
-                id: 'lines'
-            },
-            {
-                match: {
-                    id: 'elixir'
-                },
-                id: 'lines'
-            },
-            {
-                match: {
-                    id: 'javascript'
-                },
-                id: 'lines'
-            }
-        ]}
-    />
-  </div>
-)
+      />
+    </div>
+  )
+}
 
 export default PieChartCatnat;
