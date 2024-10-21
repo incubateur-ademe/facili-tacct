6b7284ad7f2ae0e4d3dec4c5078d1085dd5f9ca7
AntoineConegero <antoine.conegero@gmail.com>
2024-10-21 15:07:42 +0200

    color palette carto catnat

--- src/components/charts/catnat/BarChartCatnat.tsx
+++ src/components/charts/catnat/BarChartCatnat.tsx
@@ -5,13 +5,15 @@ import { CountOccByIndex } from "@/lib/utils/reusableFunctions/occurencesCount";
 import { Any } from "@/lib/utils/types";
 
 const colors: { [key: string]: string } = {
-  'Innondations': '#21A59B',
-  'Sécheresse': '#ADA943',
-  'Mouvements de terrain': '#DA1C5B',
-  'Retrait-gonflement des argiles': '#246EA3',
-  'Cyclones / Tempêtes': '#F2A03D',
-  'Grêle / neige': '#F2E3A3',
+  'Innondations': '#206EB4',
+  'Sécheresse': '#FF7700',
+  'Mouvements de terrain': '#BE5415',
+  'Retrait-gonflement des argiles': '#FFBC5C',
+  'Cyclones / Tempêtes': '#82815A',
+  'Grêle / neige': '#00A302',
+  'Avalanches': '#723AA0',
 };
+
 export const BarChartCatnat = (props: {gestionRisques: ArreteCatNat[]}) => {
   const { gestionRisques } = props;
   const typesRisques = [...new Set(gestionRisques.map(item => item.lib_risque_jo))].filter(item => item !== null).sort();
@@ -19,7 +21,14 @@ export const BarChartCatnat = (props: {gestionRisques: ArreteCatNat[]}) => {
   const minDate = Math.min(...gestionRisques.map(e => e.annee_arrete));
   const maxDate = Math.max(...gestionRisques.map(e => e.annee_arrete));
   return (
-      <div style={{ height: "500px", minWidth: "450px", backgroundColor: "white" }}>
+    <div style={{ height: "500px", minWidth: "450px", backgroundColor: "white" }}>
+      {
+        graphData.length === 0 ? 
+        (
+          <div style={{height:"inherit", alignContent: "center", textAlign:"center"}}>
+            Aucun arrêté catnat avec ces filtres
+          </div>
+        ) :
         <ResponsiveBar
           data={graphData as Any}
           keys={typesRisques}
@@ -28,6 +37,8 @@ export const BarChartCatnat = (props: {gestionRisques: ArreteCatNat[]}) => {
           colors={bar => colors[bar.id]}
           margin={{ top: 40, right: 200, bottom: 80, left: 80 }}
           padding={0.3}
+          innerPadding={2}
+          borderRadius={1}
           valueScale={{
             type: "linear",
           }}
@@ -42,28 +53,28 @@ export const BarChartCatnat = (props: {gestionRisques: ArreteCatNat[]}) => {
             ]
           }}
           tooltip={({
-            id,
-            value,
-            color
+              id,
+              value,
+              color
             }) => <div style={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              padding: 8,
-              background: '#FFFFFF',
-              right: '8rem',
-              fontSize: 'inherit',
-              borderRadius: '2px',
-              border: '1px solid #ccc',
-              translate: "-80px"
-            }}>
-                <div style={{height: "12px", width: "12px", backgroundColor: color, marginRight: "8px"}}/>
-                <span>
-                    {id}: <strong>{value}</strong>
-                </span>
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                padding: 8,
+                background: '#FFFFFF',
+                right: '8rem',
+                fontSize: 'inherit',
+                borderRadius: '2px',
+                border: '1px solid #ccc',
+                translate: "-80px"
+              }}>
+                  <div style={{height: "12px", width: "12px", backgroundColor: color, marginRight: "8px"}}/>
+                  <span>
+                      {id}: <strong>{value}</strong>
+                  </span>
               </div>
                       } 
-        
+                    
           axisTop={null}
           axisRight={null}
           axisBottom={{
@@ -139,7 +150,8 @@ export const BarChartCatnat = (props: {gestionRisques: ArreteCatNat[]}) => {
                 role="application"
                 ariaLabel="Nivo bar chart demo"
                 barAriaLabel={e=>e.id+": "+e.formattedValue+" in annee_arrete: "+e.indexValue}
-      />
+        />
+      }
     </div>
   )
 }
