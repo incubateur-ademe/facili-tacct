ecb94ebae80188f9459acc8d92f806396d47db8b
AntoineConegero <antoine.conegero@gmail.com>
2024-10-22 11:47:05 +0200

    fix slider catnat

--- src/components/SubTabs.tsx
+++ src/components/SubTabs.tsx
@@ -1,5 +1,12 @@
 "use client" 
 
+import camembertIconBlack from "@/assets/icons/camembert_icon_black.svg";
+import camembertIconWhite from "@/assets/icons/camembert_icon_white.svg";
+import cartographieIconBlack from "@/assets/icons/cartographie_icon_black.svg";
+import cartographieIconWhite from "@/assets/icons/cartographie_icon_white.svg";
+import evolutionIconBlack from "@/assets/icons/evolution_icon_black.svg";
+import evolutionIconWhite from "@/assets/icons/evolution_icon_white.svg";
+import Image, { StaticImageData } from "next/image";
 import { useState } from "react";
 import styles from "./components.module.scss";
 
@@ -11,8 +18,41 @@ interface Props {
   borderRight?: string;
 }
 
+type TabIcons = {
+  name: string;
+  iconNotSelected: StaticImageData;
+  iconSelected: StaticImageData;
+}
+
+const tabIcons = [
+  {
+    name: "Répartition", 
+    iconNotSelected: camembertIconBlack,
+    iconSelected: camembertIconWhite,
+  },
+  {
+    name: "Évolution",
+    iconNotSelected: evolutionIconBlack,
+    iconSelected: evolutionIconWhite,
+  },
+  {
+    name: "Cartographie",
+    iconNotSelected: cartographieIconBlack,
+    iconSelected: cartographieIconWhite,
+  }
+];
+
+
+const tabsWithIcons = (tabIcons: TabIcons[], name: string, selectedSubTab: string) => {
+  const obj = tabIcons.filter((tab) => tab.name === name).length > 0 ? tabIcons.filter((tab) => tab.name === name)[0] : null;
+  if (selectedSubTab === name) {
+    return obj?.iconSelected;
+  } else return obj?.iconNotSelected;
+}
+
 const SubTabs = ({ data, defaultTab, setValue, maxWidth="100%", borderRight="none" }: Props) => {
   const [selectedSubTab, setSelectedSubTab] = useState(defaultTab);
+
   return(
     <div className={styles.titles} style={{maxWidth: maxWidth, borderRight: borderRight}}>
       {data
@@ -25,6 +65,11 @@ const SubTabs = ({ data, defaultTab, setValue, maxWidth="100%", borderRight="non
               setValue(element ? element : "");
             }}
           >
+            { 
+              element && tabsWithIcons(tabIcons, element, selectedSubTab) ? 
+              <Image src={tabsWithIcons(tabIcons, element, selectedSubTab)!} alt=""/> 
+              : null
+            }
             {element}
           </button>
         ))}
