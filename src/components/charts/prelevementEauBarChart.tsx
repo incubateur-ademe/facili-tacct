36a0e6839836f94b755bfc8edbeb430bcb7551ea
AntoineConegero <antoine.conegero@gmail.com>
2024-10-30 18:54:51 +0100

    finition hover + legend prelevementEau

--- src/components/charts/prelevementEauBarChart.tsx
+++ src/components/charts/prelevementEauBarChart.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import styles from "@/components/themes/ressourcesEau/ressourcesEau.module.scss";
 import { RessourcesEau } from "@/lib/postgres/models";
 import { SumByKey } from "@/lib/utils/reusableFunctions/sumByKey";
 import { Any } from "@/lib/utils/types";
-import { ResponsiveBar } from "@nivo/bar";
+import { BarLegendProps, ResponsiveBar } from "@nivo/bar";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -20,22 +21,23 @@ type GraphData = {
 const ressourcesEauYears = ["A2008", "A2009", "A2010", "A2011", "A2012", "A2013", "A2014", "A2015", "A2016", "A2017", "A2018", "A2019", "A2020"];
 const columns = ["LIBELLE_SOUS_CHAMP", "SOUS_CHAMP", "code_geographique", "departement", "epci", "index", "libelle_epci", "libelle_geographique", "region"]
 const colors: { [key: string]: string } = {
-  'Agriculture': '#00C190',
-  'Eau potable': '#009ADC',
-  'Industrie et autres usages économiques': '#7A49BE',
-  'Refroidissement des centrales électriques': '#BB43BD',
-  'Alimentation des canaux': '#00C2CC',
-  'Production d\'électricité (barrages hydro-électriques)': '#FFCF5E',
+  "Agriculture": "#00C190",
+  "Eau potable": "#009ADC",
+  "Industrie et autres usages économiques": "#7A49BE",
+  "Refroidissement des centrales électriques": "#BB43BD",
+  "Alimentation des canaux": "#00C2CC",
+  "Production d'électricité (barrages hydro-électriques)": "#FFCF5E",
 };
+const legend = ["Agriculture", "Eau potable", "Industrie", "Refroidissement des centrales", "Alimentation des canaux", "Barrages hydro-électriques"];
 
 const  ressourceEauFilter = (sliderValues: number[], allYears: string[], data: RessourcesEau[]) => {
   const values = [`A${sliderValues[0]}`, `A${sliderValues[1]}`];
   const newSelectedYears = allYears.slice(allYears.indexOf(values[0]), allYears.indexOf(values[1]) + 1);
   const newKeys = columns.concat(newSelectedYears);
   const newArray = data.map(item => {
-    const newItem: any = {};
+    const newItem: Any = {};
     newKeys.forEach(key => {
-      newItem[key] = (item as any)[key];
+      newItem[key] = (item as Any)[key];
     });
     return newItem;
   });
@@ -70,13 +72,34 @@ const PrelevementEauBarChart = (
   const codepci = searchParams.get("codepci")!;
   const collectiviteData = codgeo ? filteredRessourcesEau.filter((obj) => obj.code_geographique === codgeo) : filteredRessourcesEau.filter((obj) => obj.epci === codepci);
   const [graphData, setGraphData] = useState<GraphData[]>(graphDataFunct(selectedYears, collectiviteData));
+  const values = [`A${sliderValue[0]}`, `A${sliderValue[1]}`];
+  const collectiviteName = codgeo ? filteredRessourcesEau.filter(
+    (obj) => obj.code_geographique === codgeo
+  )[0].libelle_geographique : filteredRessourcesEau.filter(
+    (obj) => obj.epci === codepci
+  )[0].libelle_epci; 
 
   useEffect(() => {
-    const values = [`A${sliderValue[0]}`, `A${sliderValue[1]}`];
     setSelectedYears(ressourcesEauYears.slice(ressourcesEauYears.indexOf(values[0]), ressourcesEauYears.indexOf(values[1]) + 1))
     setFilteredRessourcesEau(ressourceEauFilter(sliderValue, ressourcesEauYears, ressourcesEau));
     setGraphData(graphDataFunct(selectedYears, collectiviteData));
   }, [sliderValue, selectedYears]);
+
+  
+  const legendProps: BarLegendProps = {
+    dataFrom: 'keys',
+    anchor: 'bottom-right',
+    direction: 'column',
+    justify: false,
+    translateX: 120,
+    translateY: 0,
+    itemsSpacing: 2,
+    itemWidth: 100,
+    itemHeight: 25,
+    itemDirection: 'left-to-right',
+    itemOpacity: 0.85,
+    symbolSize: 20,
+  };
 
   return (
     <div style={{ height: "500px", minWidth: "450px", backgroundColor: "white" }}>
@@ -105,30 +128,30 @@ const PrelevementEauBarChart = (
         }}    
         axisTop={null}
         axisRight={null}
-        // axisBottom={{
-        //     tickValues: [2019, 2020],
-        //     tickSize: 0,
-        //     tickPadding: 15,
-        //     renderTick: (e) => {
-        //       return (
-        //         <g transform={`translate(${e.x},${e.y})`}>
-        //           <text
-        //             x={0}
-        //             y={10}
-        //             dy={16}
-        //             textAnchor="middle"
-        //             style={{
-        //               fill: 'black',
-        //               fontSize: 12,
-        //               fontWeight: 400,
-        //             }}
-        //           >
-        //             {e.value}
-        //           </text>
-        //         </g>
-        //       );
-        //     }
-        // }}
+        axisBottom={{
+          tickValues: sliderValue[0] != sliderValue[1] ? [sliderValue[0].toString(), sliderValue[1].toString()] : sliderValue[0].toString(),
+          tickSize: 0,
+          tickPadding: 15,
+          renderTick: (e) => {
+            return (
+              <g transform={`translate(${e.x},${e.y})`}>
+                <text
+                  x={0}
+                  y={10}
+                  dy={16}
+                  textAnchor="middle"
+                  style={{
+                    fill: 'black',
+                    fontSize: 12,
+                    fontWeight: 400,
+                  }}
+                >
+                  {e.value}
+                </text>
+              </g>
+            );
+          }
+        }}
         gridYValues={5}
         axisLeft={{
           tickSize: 5,
@@ -160,40 +183,49 @@ const PrelevementEauBarChart = (
         }}
         labelSkipWidth={40}
         labelSkipHeight={12}
-        labelTextColor={{
-          from: 'color',
-          modifiers: [
-            [
-              'darker',
-              1.6
-            ]
-          ]
-        }}
         legends={[
           {
-            dataFrom: 'keys',
-            anchor: 'bottom-right',
-            direction: 'column',
-            justify: false,
-            translateX: 120,
-            translateY: 0,
-            itemsSpacing: 2,
-            itemWidth: 100,
-            itemHeight: 20,
-            itemDirection: 'left-to-right',
-            itemOpacity: 0.85,
-            symbolSize: 20,
-            effects: [
-              {
-                on: 'hover',
-                style: { itemOpacity: 1 }
-              }
-            ]
-          }
+            ...legendProps,
+            data: legend
+              .map((leg, index) => ({
+                id: index,
+                label: leg,
+                color: Object.values(colors)[index],
+              })),
+          },
         ]}
+        tooltip={
+          ({ data }) => {
+            const dataArray = Object.entries(data).map(el => {
+              return {
+                titre: el[0],
+                value: el[1],
+                color: colors[el[0]]
+              }
+          });
+            return (
+              <div className={styles.tooltipEvolutionWrapper}>
+                <h3>{collectiviteName} ({dataArray.at(-1)?.value})</h3>
+                {
+                  dataArray.slice(0, -1).map((el, i) => {
+                    return (
+                      <div className={styles.itemWrapper} key={i}>
+                        <div className={styles.titre}> 
+                          <div className={styles.colorSquare} style={{background: el.color}}/>
+                          <p>{el.titre}</p>
+                        </div>
+                        <div className={styles.value}>
+                          <p>{(Number(el.value) / 1000000).toFixed(1)}Mm³</p>
+                        </div>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            );
+          }
+        }
         role="application"
-        ariaLabel="........."
-        barAriaLabel={e=>e.id+": "+e.formattedValue+" in annee_arrete: "+e.indexValue}
       />
     </div>
   )
