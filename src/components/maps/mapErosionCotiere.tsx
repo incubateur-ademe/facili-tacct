778fe89324f91cfe550ff33a9bac1f4022fb44ed
AntoineConegero <antoine.conegero@gmail.com>
2024-11-12 16:16:28 +0100

    requetes & map erosion littoral

--- src/components/maps/mapErosionCotiere.tsx
+++ src/components/maps/mapErosionCotiere.tsx
@@ -1,48 +1,84 @@
 "use client";
 
-import { ErosionCotiereDto } from "@/lib/dto";
+import { EpciContoursDto, ErosionCotiereDto } from "@/lib/dto";
 import { GeoJSON, MapContainer, TileLayer } from "@/lib/react-leaflet";
 import { Any } from "@/lib/utils/types";
 import { StyleFunction } from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { useSearchParams } from "next/navigation";
 import { useRef } from "react";
+
+const getCentroid = (arr: number[][]) => {
+  return arr?.reduce(
+    (x: number[], y: number[]) => {
+      return [x[0] + y[0] / arr.length, x[1] + y[1] / arr.length];
+    },
+    [0, 0],
+  );
+};
+const getCoordinates = (coords: number[][][]) => {
+  const coords_arr = [];
+  for (let i = 0; i < coords.length; i++) {
+    const center = getCentroid(coords[i]);
+    coords_arr.push(center);
+  }
+  return getCentroid(coords_arr);
+};
 
 export const MapErosionCotiere = (props: {
   erosionCotiere: ErosionCotiereDto[];
+  epciContours: EpciContoursDto[];
 }) => {
-  const { erosionCotiere } = props;
+  const { erosionCotiere, epciContours } = props;
+  const searchParams = useSearchParams();
+  const codepci = searchParams.get("codepci")!;  
   const mapRef = useRef(null);
+  const centerCoord: number[] = getCentroid(epciContours[0].geometry.coordinates[0][0]);
+  // console.log("erosionCotiere[0].geometry.coordinates?.[0]", epciContours[0].geometry.coordinates[0][0])
+  // console.log(centerCoord);
 
   const getColor = (d: number) => {
-    return d > 1 ? "#FF5E54" : d > 0.4 ? "#FFBD00" : d > 0 ? "#FFFA6A" : d > -2 ? "#D5F4A3" : "#5CFF54";
+    return d > 1 ? "#FFBE0B" : d > 0.4 ? "#FB5607" : d > 0 ? "#FF006E" : d > -2 ? "#8338EC" : "#3A86FF";
   }
 
   const style: StyleFunction<Any> = feature => {
     const typedFeature = feature as ErosionCotiereDto;
     return {
       fillColor: getColor(typedFeature?.properties.taux),
-      weight: 5,
+      weight: 1,
       opacity: 1,
       color: getColor(typedFeature?.properties.taux),
       // dashArray: "3",
-      fillOpacity: 0.8,
+      fillOpacity: 0.95,
+    };
+  };
+
+  const epciStyle: StyleFunction<Any> = () => {
+    return {
+      weight: 1,
+      opacity: 1,
+      color: "#161616",
+      // dashArray: "3",
+      fillOpacity: 0,
     };
   };
 
   return (
     <MapContainer
-      center={[48, 2.35]}
-      zoom={6}
+      center={[centerCoord[1], centerCoord[0]]}
+      zoom={10}
       ref={mapRef}
       style={{ height: "500px", width: "100%"}}
       attributionControl={false}
       zoomControl={false}
+      minZoom={9}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+        // attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <GeoJSON ref={mapRef} data={erosionCotiere as any} style={style} />
+      <GeoJSON ref={mapRef} data={erosionCotiere as Any} style={style} />
+      <GeoJSON ref={mapRef} data={epciContours as Any} style={epciStyle}/>
     </MapContainer>
   );
 };
--- src/components/themes/gestionRisques/erosionCotiere.tsx
+++ src/components/themes/gestionRisques/erosionCotiere.tsx
@@ -1,15 +1,16 @@
 import { MapErosionCotiere } from "@/components/maps/mapErosionCotiere";
+import { EpciContoursMapper } from "@/lib/mapper/epci";
 import { ErosionCotiereMapper } from "@/lib/mapper/erosionCotiere";
-import { ErosionCotiere } from "@/lib/postgres/models";
+import { EpciContours, ErosionCotiere } from "@/lib/postgres/models";
 
-const ErosionCotes = (props: { erosionCotiere: ErosionCotiere[] }) => {
-  const { erosionCotiere } = props;
+const ErosionCotes = (props: { erosionCotiere: ErosionCotiere[], epciContours:EpciContours[] }) => {
+  const { erosionCotiere, epciContours } = props;
   const erosionCotiereMap = erosionCotiere.map(ErosionCotiereMapper);
-
+  const epciContoursMap = epciContours.map(EpciContoursMapper);
   return (
     <div>
-      <h1>ErosionCotes</h1>
-      <MapErosionCotiere erosionCotiere={erosionCotiereMap} />
+      <h1>Erosion des côtes</h1>
+      <MapErosionCotiere erosionCotiere={erosionCotiereMap} epciContours={epciContoursMap} />
     </div>
   );
 };
