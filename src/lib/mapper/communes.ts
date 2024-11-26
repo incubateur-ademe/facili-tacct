28ccf53e8561f009551cebfcc818697968065c22
AntoineConegero <antoine.conegero@gmail.com>
2024-11-26 16:17:52 +0100

    contours communes surfaces protegees

--- src/lib/mapper/communes.ts
+++ src/lib/mapper/communes.ts
@@ -1,4 +1,4 @@
-import { CommunesIndicateursDto } from "../dto";
+import { CommunesContoursDto, CommunesIndicateursDto } from "../dto";
 import { CarteCommunes } from "../postgres/models";
 
 export const CommunesIndicateursMapper = (commune: CarteCommunes): CommunesIndicateursDto => ({
@@ -11,6 +11,18 @@ export const CommunesIndicateursMapper = (commune: CarteCommunes): CommunesIndic
     code_commune: commune.code_commune,
     precarite_logement: commune.precarite_logement ?? NaN,
     densite_bati: commune.densite_bati ?? NaN,
+    coordinates: commune.coordinates,
+  },
+  geometry: JSON.parse(commune.geometry),
+});
+
+export const CommunesContourMapper = (commune: CarteCommunes): CommunesContoursDto => ({
+  type: "Feature",
+  properties: {
+    epci: commune.epci,
+    libelle_epci: commune.libelle_epci,
+    libelle_commune: commune.libelle_commune,
+    code_commune: commune.code_commune,
     coordinates: commune.coordinates,
   },
   geometry: JSON.parse(commune.geometry),
