4affe9f413e2e66bcddac9208e60a6e389ddb5cc
AntoineConegero <antoine.conegero@gmail.com>
2024-11-27 15:19:47 +0100

    fix nan values carteCommunes

--- src/lib/mapper/communes.ts
+++ src/lib/mapper/communes.ts
@@ -1,20 +1,23 @@
 import { CommunesContoursDto, CommunesIndicateursDto } from "../dto";
 import { CarteCommunes } from "../postgres/models";
 
-export const CommunesIndicateursMapper = (commune: CarteCommunes): CommunesIndicateursDto => ({
-  type: "Feature",
-  properties: {
-    catnat: commune.catnat,
-    epci: commune.epci,
-    libelle_epci: commune.libelle_epci,
-    libelle_commune: commune.libelle_commune,
-    code_commune: commune.code_commune,
-    precarite_logement: commune.precarite_logement ?? NaN,
-    densite_bati: commune.densite_bati ?? NaN,
-    coordinates: commune.coordinates,
-  },
-  geometry: JSON.parse(commune.geometry),
-});
+export const CommunesIndicateursMapper = (commune: CarteCommunes): CommunesIndicateursDto => {
+  const data = {
+    type: "Feature",
+    properties: {
+      catnat: commune.catnat,
+      epci: commune.epci,
+      libelle_epci: commune.libelle_epci,
+      libelle_commune: commune.libelle_commune,
+      code_commune: commune.code_commune,
+      precarite_logement: commune.precarite_logement ?? NaN,
+      densite_bati: commune.densite_bati ?? NaN,
+      coordinates: commune.coordinates,
+    },
+    geometry: JSON.parse(commune.geometry),
+  }  
+  return data;
+};
 
 export const CommunesContourMapper = (commune: CarteCommunes): CommunesContoursDto => ({
   type: "Feature",
