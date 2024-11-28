6521669c397ff2315d622f18ae8d243d3822ecd4
AntoineConegero <antoine.conegero@gmail.com>
2024-11-28 12:59:44 +0100

    debug searchbar

--- src/lib/queries/searchBar.ts
+++ src/lib/queries/searchBar.ts
@@ -2,13 +2,13 @@
 
 import * as Sentry from "@sentry/nextjs";
 import { PrismaClient as PostgresClient } from "../../generated/client";
-import { CarteCommunes, CollectivitesSearchbar } from "../postgres/models";
+import { CollectivitesSearchbar } from "../postgres/models";
 
 const PrismaPostgres = new PostgresClient();
 
 export const GetCollectivite = async (collectivite: string): Promise<CollectivitesSearchbar[]> => {
   try {
-    console.time("Query Execution Time");
+    console.time("Query Execution Time COLLECTIVITE");
     const variableCollectivite = "%" + collectivite + "%";
     const variableCollectiviteNumber = collectivite + "%";
     if (isNaN(parseInt(collectivite))) {
@@ -24,7 +24,7 @@ export const GetCollectivite = async (collectivite: string): Promise<Collectivit
       departement,
       region
       FROM databases."collectivites_searchbar" WHERE unaccent('unaccent', search_libelle) ILIKE unaccent('unaccent', replace(${variableCollectivite}, ' ', '-')) LIMIT 20;`; // OR libelle_epci ILIKE ${variableEpci} 
-      console.timeEnd("Query Execution Time");
+      console.timeEnd(`Query Execution Time COLLECTIVITE ${collectivite}`);
       // console.log(value);
       if (value.length > 0) {
         return value 
@@ -52,7 +52,7 @@ export const GetCollectivite = async (collectivite: string): Promise<Collectivit
           } 
         },
       });
-      console.timeEnd("Query Execution Time");
+      console.timeEnd(`Query Execution Time COLLECTIVITE ${collectivite}`);
       return value as CollectivitesSearchbar[];
     } else {
       Sentry.captureMessage(`Collectivite ${collectivite} not found`);
@@ -77,215 +77,3 @@ export const GetCollectivite = async (collectivite: string): Promise<Collectivit
       process.exit(1);
    }
 }
-
-// Not using Prisma because unaccent unsupported
-// export const GetCollectivite2 = async (collectivite: string): Promise<CollectivitesSearchbar[]> => {
-//   try {
-//     console.time("Query Execution Time");
-//     const variableCollectivite = "%" + collectivite + "%";
-//     const variableCollectiviteReplace = "%" + collectivite.replace(' ', '-') + "%"
-//     const variableCollectiviteNumber = collectivite + "%";
-//     if (isNaN(parseInt(collectivite))) {
-//       const value = await PrismaPostgres.collectivites_searchbar.findMany({
-//         take: 20,
-//         where: {
-//           OR: [
-//             { 
-//               search_libelle: 
-//               { 
-//                 contains: variableCollectivite, 
-//                 mode: 'insensitive' 
-//               } 
-//             }, 
-//             { 
-//               search_libelle: 
-//               {
-//                 contains: variableCollectiviteReplace,
-//                 mode: 'insensitive'
-//               }
-//             }
-//           ],
-//         },
-//       });
-//       console.timeEnd("Query Execution Time");
-//       // console.log(value);
-//         return value;
-//       } else if (typeof parseInt(collectivite) === "number") {
-//         const value = await PrismaPostgres.collectivites_searchbar.findMany({
-//           take: 20,
-//           where: { search_code:
-//             { 
-//               startsWith: variableCollectiviteNumber
-//             } 
-//           },
-//         });
-//       console.timeEnd("Query Execution Time");
-//       return value;
-//     } else {
-//       return [
-//         {
-//           code_commune: "",
-//           search_code: "",
-//           search_libelle: "",
-//           code_epci: "",
-//           libelle_commune: "",
-//           libelle_epci: "",
-//           departement: "",
-//           region: "",
-//         },
-//       ];
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     await PrismaPostgres.$disconnect();
-//     process.exit(1);
-//   }
-// };
-
-export const GetStringCommune = async (collectivite: string): Promise<CarteCommunes[]> => {
-  try {
-    console.time("Query Execution Time");
-    const variableCommune = "%" + collectivite + "%";
-    if (isNaN(parseInt(collectivite))) {
-      const value = await PrismaPostgres.$queryRaw<CarteCommunes[]>`
-      SELECT 
-      epci, 
-      libelle_epci,
-      libelle_commune,
-      code_commune
-      FROM postgis."communes" WHERE unaccent('unaccent', libelle_commune) ILIKE unaccent('unaccent', replace(${variableCommune}, ' ', '-')) LIMIT 20;`; // OR libelle_epci ILIKE ${variableEpci} 
-      console.timeEnd(`Query Execution Time ${variableCommune}`);
-      // console.log(value);
-      return value;
-    } else {
-      return [
-        {
-          code_commune: "",
-          coordinates: "",
-          densite_bati: 0,
-          epci: "",
-          libelle_commune: "",
-          libelle_epci: "",
-          precarite_logement: 0,
-          geometry: "",
-        },
-      ];
-    }
-  } catch (error) {
-      console.error(error);
-      Sentry.captureException(error);
-      await PrismaPostgres.$disconnect();
-      process.exit(1);
-   }
-}
-
-export const GetStringEpci = async (collectivite: string): Promise<CarteCommunes[]> => {
-  try {
-    console.time("Query Execution Time");
-    const variableEpci = "%" + collectivite + "%";
-    if (isNaN(parseInt(collectivite))) {
-      const value = await PrismaPostgres.$queryRaw<CarteCommunes[]>`
-      SELECT 
-      epci, 
-      libelle_epci,
-      libelle_commune,
-      code_commune
-      FROM postgis."communes" WHERE unaccent('unaccent', libelle_epci) ILIKE unaccent('unaccent', replace(${variableEpci}, ' ', '-')) LIMIT 20;`; // OR libelle_epci ILIKE ${variableEpci} 
-      console.timeEnd(`Query Execution Time EPCI ${variableEpci}`);
-      // console.log(value);
-      return value;
-    } else {
-      return [
-        {
-          code_commune: "",
-          coordinates: "",
-          densite_bati: 0,
-          epci: "",
-          libelle_commune: "",
-          libelle_epci: "",
-          precarite_logement: 0,
-          geometry: "",
-        },
-      ];
-    }
-  } catch (error) {
-      console.error(error);
-      Sentry.captureException(error);
-      await PrismaPostgres.$disconnect();
-      process.exit(1);
-   }
-}
-
-export const GetCodeCommune = async (collectivite: string): Promise<CarteCommunes[]> => {
-  try {
-    console.time("Query Execution Time");
-    const variableCodeCommune = collectivite + "%";
-    if (typeof parseInt(collectivite) === "number") {
-      const value = await PrismaPostgres.$queryRaw<CarteCommunes[]>`
-      SELECT 
-      epci, 
-      libelle_epci,
-      libelle_commune,
-      code_commune
-      FROM postgis."communes" WHERE code_commune ILIKE ${variableCodeCommune} LIMIT 20;`;
-      console.timeEnd(`Query Execution Time communes ${variableCodeCommune}`);
-      return value;
-    } else {
-      return [
-        {
-          code_commune: "",
-          coordinates: "",
-          densite_bati: 0,
-          epci: "",
-          libelle_commune: "",
-          libelle_epci: "",
-          precarite_logement: 0,
-          geometry: "",
-        },
-      ];
-    }
-  } catch (error) {
-    console.error(error);
-    Sentry.captureException(error);
-    await PrismaPostgres.$disconnect();
-    process.exit(1);
-  }
-};
-
-export const GetCodeEpci = async (collectivite: string): Promise<CarteCommunes[]> => {
-  try {
-    console.time("Query Execution Time");
-    const variableCodeEpci = collectivite + "%";
-    if (typeof parseInt(collectivite) === "number") {
-      const value = await PrismaPostgres.$queryRaw<CarteCommunes[]>`
-      SELECT 
-      epci, 
-      libelle_epci,
-      libelle_commune,
-      code_commune
-      FROM postgis."communes" WHERE epci ILIKE ${variableCodeEpci} LIMIT 20;`;
-      console.timeEnd(`Query Execution Time epci ${variableCodeEpci}`);
-      return value;
-    } else {
-      return [
-        {
-          code_commune: "",
-          coordinates: "",
-          densite_bati: 0,
-          epci: "",
-          libelle_commune: "",
-          libelle_epci: "",
-          precarite_logement: 0,
-          geometry: "",
-        },
-      ];
-    }
-  } catch (error) {
-    console.error(error);
-    Sentry.captureException(error);
-    await PrismaPostgres.$disconnect();
-    process.exit(1);
-  }
-};
-
-
