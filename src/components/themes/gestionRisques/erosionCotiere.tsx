2e03bf6d391e190a4ba62d5874a5e19de3f4b50a
AntoineConegero <antoine.conegero@gmail.com>
2024-12-02 15:55:04 +0100

    legende erosion cotiere

--- src/components/themes/gestionRisques/erosionCotiere.tsx
+++ src/components/themes/gestionRisques/erosionCotiere.tsx
@@ -6,6 +6,7 @@ import { EpciContours, ErosionCotiere } from "@/lib/postgres/models";
 import { CustomTooltip } from "@/lib/utils/CalculTooltip";
 import { styled, Tooltip, tooltipClasses, TooltipProps } from "@mui/material";
 import { useSearchParams } from "next/navigation";
+import { LegendErosionCotiere } from "./erosionCotiere-legend";
 import styles from "./gestionRisques.module.scss";
 
 const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
@@ -83,18 +84,19 @@ const ErosionCotes = (props: { erosionCotiere: ErosionCotiere[], epciContours: E
               <div>
                 <MapErosionCotiere erosionCotiere={erosionCotiereMap} epciContours={epciContoursMap} />
               </div>
+              <LegendErosionCotiere />
               <p style={{ padding: "1em", margin: "0" }}>
                 Source : CEREMA
               </p>
             </div>
-            <div className={styles.ErosionCotiereLegendWrapper}>
+            {/* <div className={styles.ErosionCotiereLegendWrapper}>
               <h2>Mouvement du trait de côte</h2>
               <div className={styles.bloc}>
                 <p style={{width: "85px", minWidth: "85px", margin: 0, alignSelf: "center"}}>
                   <b>Recul</b>
                 </p>
                 <div className={styles.legendsWrappers}>
-                  <div className={styles.legendsSide}>
+                  <div className={styles.legendsLeft}>
                     <div className={styles.legendItem}>
                       <div className={styles.colorSquare} style={{ backgroundColor: "#A74E10" }}></div>
                       <p>Supérieur à 3 m/an</p>
@@ -104,7 +106,7 @@ const ErosionCotes = (props: { erosionCotiere: ErosionCotiere[], epciContours: E
                       <p>Entre 1,5 et 3 m/an</p>
                     </div>
                   </div>
-                  <div className={styles.legendsSide}>
+                  <div className={styles.legendsRight}>
                     <div className={styles.legendItem}>
                       <div className={styles.colorSquare} style={{ backgroundColor: "#F59550" }}></div>
                       <p>Entre 0,5 et 1,5 m/an</p>
@@ -121,7 +123,7 @@ const ErosionCotes = (props: { erosionCotiere: ErosionCotiere[], epciContours: E
                   <b>Avancée</b>
                 </p>
                 <div className={styles.legendsWrappers}>
-                  <div className={styles.legendsSide}>
+                  <div className={styles.legendsLeft}>
                     <div className={styles.legendItem}>
                       <div className={styles.colorSquare} style={{ backgroundColor: "#DCEE9F" }}></div>
                       <p>Entre 0,1 et 0,5 m/an</p>
@@ -131,7 +133,7 @@ const ErosionCotes = (props: { erosionCotiere: ErosionCotiere[], epciContours: E
                       <p>Entre 0,5 et 1,5 m/an</p>
                     </div>
                   </div>
-                  <div className={styles.legendsSide}>
+                  <div className={styles.legendsRight}>
                     <div className={styles.legendItem}>
                       <div className={styles.colorSquare} style={{ backgroundColor: "#1DA546" }}></div>
                       <p>Entre 1,5 et 3 m/an</p>
@@ -145,13 +147,13 @@ const ErosionCotes = (props: { erosionCotiere: ErosionCotiere[], epciContours: E
               </div>
               <div className={styles.bloc}>
                 <div className={styles.legendsWrappers} style={{marginLeft  : "85px"}}>
-                  <div className={styles.legendsSide}>
+                  <div className={styles.legendsLeft}>
                     <div className={styles.legendItem}>
                       <div className={styles.colorSquare} style={{ backgroundColor: "#AFF7F1" }}></div>
                       <p>Mouvement non perceptible</p>
                     </div>
                   </div>
-                  <div className={styles.legendsSide}>
+                  <div className={styles.legendsRight}>
                     <div className={styles.legendItem}>
                       <div className={styles.colorSquare} style={{ backgroundColor: "#9D9C9C" }}></div>
                       <p>Absence de données</p>
@@ -159,7 +161,7 @@ const ErosionCotes = (props: { erosionCotiere: ErosionCotiere[], epciContours: E
                   </div>
                 </div>
               </div>
-            </div>         
+            </div>          */}
           </div>
         </div>
       ) : (
