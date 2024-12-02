d3e77b62cf60b37442bddc9ef1f251c319b471a3
AntoineConegero <antoine.conegero@gmail.com>
2024-12-02 14:24:46 +0100

    erosion cotiere legend

--- src/lib/mapper/erosionCotiere.ts
+++ src/lib/mapper/erosionCotiere.ts
@@ -1,10 +1,12 @@
 import { ErosionCotiereDto } from "../dto";
 import { ErosionCotiere } from "../postgres/models";
 
-export const ErosionCotiereMapper = (erosionCotiere: ErosionCotiere): ErosionCotiereDto => ({
-  type: "Feature",
-  properties: {
-    taux: erosionCotiere.taux,
-  },
-  geometry: JSON.parse(erosionCotiere.geometry),
-});
+export const ErosionCotiereMapper = (erosionCotiere: ErosionCotiere): ErosionCotiereDto => {
+  return ({
+    type: "Feature",
+    properties: {
+      taux: erosionCotiere.taux,
+    },
+    geometry: JSON.parse(erosionCotiere.geometry),
+  })
+};
