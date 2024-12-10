83b4ace5be2108de508aa77fd4ee01bbade904fb
AntoineConegero <antoine.conegero@gmail.com>
2024-12-10 11:12:35 +0100

    build: rebase dev par rapport à la prod

--- src/app/donnees-territoriales/thematiques/biodiversiteComp.tsx
+++ src/app/donnees-territoriales/thematiques/biodiversiteComp.tsx
@@ -1,19 +1,26 @@
-"use client";
+'use client';
 
-import { fr } from "@codegouvfr/react-dsfr";
-import { Tabs } from "@codegouvfr/react-dsfr/Tabs";
-import { useIsDark } from "@codegouvfr/react-dsfr/useIsDark";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { fr } from '@codegouvfr/react-dsfr';
+import { Tabs } from '@codegouvfr/react-dsfr/Tabs';
+import { useIsDark } from '@codegouvfr/react-dsfr/useIsDark';
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-
-import AgricultureBiologique from "@/components/themes/biodiversite/agricultureBio";
-import { ConsommationEspacesNAF } from "@/components/themes/biodiversite/consommationEspacesNAF";
-import { StationsClassees } from "@/components/themes/biodiversite/stationsClassees";
-import SurfacesProtegees from "@/components/themes/biodiversite/surfacesProtegees";
-import { AgricultureBio, Biodiversite, CarteCommunes, ConsommationNAF, EpciContours, SurfacesProtegeesByCol } from "@/lib/postgres/models";
-import { useStyles } from "tss-react/dsfr";
-import styles from "../donnees.module.scss";
+import AgricultureBiologique from '@/components/themes/biodiversite/agricultureBio';
+import { ConsommationEspacesNAF } from '@/components/themes/biodiversite/consommationEspacesNAF';
+import { StationsClassees } from '@/components/themes/biodiversite/stationsClassees';
+import SurfacesProtegees from '@/components/themes/biodiversite/surfacesProtegees';
+import {
+  AgricultureBio,
+  Biodiversite,
+  CarteCommunes,
+  ConsommationNAF,
+  EpciContours,
+  SurfacesProtegeesByCol
+} from '@/lib/postgres/models';
+import { TabTooltip } from '@/lib/utils/TabTooltip';
+import { useStyles } from 'tss-react/dsfr';
+import styles from '../donnees.module.scss';
 
 interface Props {
   data: Array<{
@@ -33,103 +40,149 @@ interface Props {
 
 const allComps = [
   {
-    titre: "Stations classées",
-    Component: ({biodiversite, data, carteCommunes}: Props & { activeDataTab: string }) => <StationsClassees biodiversite={biodiversite} data={data} carteCommunes={carteCommunes} />,
+    titre: 'Stations classées',
+    Component: ({
+      biodiversite,
+      data,
+      carteCommunes
+    }: Props & { activeDataTab: string }) => (
+      <StationsClassees
+        biodiversite={biodiversite}
+        data={data}
+        carteCommunes={carteCommunes}
+      />
+    )
   },
   {
-    titre: "Agriculture biologique",
-    Component: ({data, agricultureBio}: Props & { activeDataTab: string }) => <AgricultureBiologique data={data} agricultureBio={agricultureBio} />,
+    titre: 'Agriculture biologique',
+    Component: ({
+      data,
+      agricultureBio
+    }: Props & { activeDataTab: string }) => (
+      <AgricultureBiologique data={data} agricultureBio={agricultureBio} />
+    )
   },
   {
-    titre: "Surfaces protégées",
-    Component: (
-      {data, surfacesProtegees, epciContours, carteCommunes}: Props & { activeDataTab: string }
-    ) => <SurfacesProtegees data={data} surfacesProtegees={surfacesProtegees} carteCommunes={carteCommunes}/>,
+    titre: 'Surfaces protégées',
+    Component: ({
+      data,
+      surfacesProtegees,
+      epciContours,
+      carteCommunes
+    }: Props & { activeDataTab: string }) => (
+      <SurfacesProtegees
+        data={data}
+        surfacesProtegees={surfacesProtegees}
+        carteCommunes={carteCommunes}
+      />
+    )
   },
   {
     titre: "Consommation d'espaces NAF",
-    Component: (
-      {data, consommationNAF, carteCommunes}: Props & { activeDataTab: string }
-    ) => <ConsommationEspacesNAF data={data} consommationNAF={consommationNAF} carteCommunes={carteCommunes}/>,
+    Component: ({
+      data,
+      consommationNAF,
+      carteCommunes
+    }: Props & { activeDataTab: string }) => (
+      <ConsommationEspacesNAF
+        data={data}
+        consommationNAF={consommationNAF}
+        carteCommunes={carteCommunes}
+      />
+    )
   }
 ];
 
-const BiodiversiteComp = (
-  { 
-    data, 
-    biodiversite,
-    carteCommunes, 
-    agricultureBio, 
-    surfacesProtegees, 
-    consommationNAF,
-    epciContours
-  }: Props) => {
-  const [selectedTabId, setSelectedTabId] = useState("Agriculture biologique");
-  const [selectedSubTab, setSelectedSubTab] = useState("Agriculture biologique");
+const BiodiversiteComp = ({
+  data,
+  biodiversite,
+  carteCommunes,
+  agricultureBio,
+  surfacesProtegees,
+  consommationNAF,
+  epciContours
+}: Props) => {
+  const [selectedTabId, setSelectedTabId] = useState('Agriculture biologique');
+  const [selectedSubTab, setSelectedSubTab] = useState(
+    'Agriculture biologique'
+  );
   const searchParams = useSearchParams();
-  const codepci = searchParams.get("codepci")!;
+  const codepci = searchParams.get('codepci')!;
   const { isDark } = useIsDark();
   const darkClass = {
-    backgroundColor: fr.colors.getHex({ isDark }).decisions.background.default.grey.active,
-    "&:hover": {
-      backgroundColor: fr.colors.getHex({ isDark }).decisions.background.alt.grey.hover,
-    },
+    backgroundColor: fr.colors.getHex({ isDark }).decisions.background.default
+      .grey.active,
+    '&:hover': {
+      backgroundColor: fr.colors.getHex({ isDark }).decisions.background.alt
+        .grey.hover
+    }
   };
   const { css } = useStyles();
 
   useEffect(() => {
-    setSelectedSubTab(data.filter(el => el.facteur_sensibilite === selectedTabId)[0].titre);
+    setSelectedSubTab(
+      data.filter((el) => el.facteur_sensibilite === selectedTabId)[0].titre
+    );
   }, [selectedTabId, codepci]);
 
   return (
     <div className={styles.container}>
-      <Tabs 
-        selectedTabId={selectedTabId} 
+      <Tabs
+        selectedTabId={selectedTabId}
         tabs={[
           {
-            tabId: "Agriculture",
-            label: "Agriculture"
+            tabId: 'Agriculture biologique',
+            label: (
+              <TabTooltip
+                selectedTab={selectedTabId}
+                tooltip="L’agriculture biologique fait partie d’un ensemble de pratiques agricoles respectueuses des équilibres écologiques qui contribue à la préservation des sols et des ressources naturelles. "
+                titre="Agriculture biologique"
+              />
+            )
           },
           {
-            tabId: "Surfaces protégées",
-            label: "Surfaces protégées"
+            tabId: 'Surfaces protégées',
+            label: 'Surfaces protégées'
           },
           {
             tabId: "Consommation d'espaces NAF",
             label: "Consommation d'espaces NAF"
           }
-        ]} 
-        onTabChange={setSelectedTabId} 
+        ]}
+        onTabChange={setSelectedTabId}
         className={css({
-          boxShadow: "none",
-          "::before": {
-            boxShadow: "inset 0 1px 0 0 var(--border-default-grey), inset 1px 0 0 0 transparent, inset -1px 0 0 0 transparent",
+          boxShadow: 'none',
+          '::before': {
+            boxShadow:
+              'inset 0 1px 0 0 var(--border-default-grey), inset 1px 0 0 0 transparent, inset -1px 0 0 0 transparent'
           },
-          ".fr-tabs__tab[aria-selected=true]:not(:disabled)": {
-            color: "#0063CB",
-            backgroundImage: "linear-gradient(0deg, #0063CB, #0063CB), linear-gradient(0deg, var(--border-default-grey), var(--border-default-grey)), linear-gradient(0deg, var(--border-default-grey), var(--border-default-grey)), linear-gradient(0deg, var(--border-default-grey), var(--border-default-grey))",
+          '.fr-tabs__tab[aria-selected=true]:not(:disabled)': {
+            color: '#0063CB',
+            backgroundImage:
+              'linear-gradient(0deg, #0063CB, #0063CB), linear-gradient(0deg, var(--border-default-grey), var(--border-default-grey)), linear-gradient(0deg, var(--border-default-grey), var(--border-default-grey)), linear-gradient(0deg, var(--border-default-grey), var(--border-default-grey))'
           },
-          ".fr-tabs__tab": {
-            margin: "0 0.25em 0 0",
+          '.fr-tabs__tab': {
+            margin: '0 0.25em 0 0'
           },
-          ".fr-tabs__tab:not([aria-selected=true])": {
-            color: "#3A3A3A",
-            backgroundColor: "#F9F9FF",
-            "&:hover": {
-              backgroundColor: "#e9e9ff",
-            },
+          '.fr-tabs__tab:not([aria-selected=true])': {
+            color: '#3A3A3A',
+            backgroundColor: '#F9F9FF',
+            '&:hover': {
+              backgroundColor: '#e9e9ff'
+            }
           },
-          ".fr-tabs__panel": {
-            padding: "1rem 0"
+          '.fr-tabs__panel': {
+            padding: '1rem 0'
           },
-          ".fr-tabs__list": {
-            padding: "0",
-            margin: "0",
+          '.fr-tabs__list': {
+            padding: '0',
+            margin: '0'
           }
-        })}>
+        })}
+      >
         <div className={styles.formContainer}>
-          {/* <div className={styles.titles}>
-            {data
+          <div className={styles.titles}>
+            {/* {data
               .filter(el => el.facteur_sensibilite === selectedTabId)
               .map((element, i) => (
                 <button
@@ -141,12 +194,14 @@ const BiodiversiteComp = (
                 >
                   {element.titre}
                 </button>
-              ))}
-          </div> */}
+              ))} */}
+          </div>
           <div className={styles.bubble}>
             <div className={styles.bubbleContent} style={darkClass}>
               {(() => {
-                const Component = allComps.find(el => el.titre === selectedSubTab)?.Component;
+                const Component = allComps.find(
+                  (el) => el.titre === selectedSubTab
+                )?.Component;
                 if (!Component) return null;
                 return (
                   <Component
--- src/app/donnees-territoriales/thematiques/inconfortThermiqueComp.tsx
+++ src/app/donnees-territoriales/thematiques/inconfortThermiqueComp.tsx
@@ -1,24 +1,29 @@
-"use client";
+'use client';
 
-import { fr } from "@codegouvfr/react-dsfr";
-import { Tabs } from "@codegouvfr/react-dsfr/Tabs";
-import { useIsDark } from "@codegouvfr/react-dsfr/useIsDark";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { fr } from '@codegouvfr/react-dsfr';
+import { Tabs } from '@codegouvfr/react-dsfr/Tabs';
+import { useIsDark } from '@codegouvfr/react-dsfr/useIsDark';
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-
-import { Loader } from "@/components/loader";
-import { AgeBati } from "@/components/themes/inconfort-thermique/age-bati";
-import { DensiteBati } from "@/components/themes/inconfort-thermique/densite-bati";
-import { FragiliteEconomique } from "@/components/themes/inconfort-thermique/fragilite-economique";
-import { GrandAgeIsolement } from "@/components/themes/inconfort-thermique/grand-age-isolement";
-import { TravailExterieur } from "@/components/themes/inconfort-thermique/travail-exterieur";
-import { CarteCommunes, CLC, CollectivitesSearchbar, InconfortThermique } from "@/lib/postgres/models";
-import { GetClcEpci } from "@/lib/queries/postgis/cartographie";
-import { TabTooltip } from "@/lib/utils/TabTooltip";
-import dynamic from "next/dynamic";
-import { useStyles } from "tss-react/dsfr";
-import styles from "../donnees.module.scss";
+import { Loader } from '@/components/loader';
+import { AgeBati } from '@/components/themes/inconfort-thermique/age-bati';
+import { DensiteBati } from '@/components/themes/inconfort-thermique/densite-bati';
+import { FragiliteEconomique } from '@/components/themes/inconfort-thermique/fragilite-economique';
+import { GrandAgeIsolement } from '@/components/themes/inconfort-thermique/grand-age-isolement';
+import LCZ from '@/components/themes/inconfort-thermique/lcz';
+import { TravailExterieur } from '@/components/themes/inconfort-thermique/travail-exterieur';
+import {
+  CarteCommunes,
+  CLC,
+  CollectivitesSearchbar,
+  InconfortThermique
+} from '@/lib/postgres/models';
+import { GetClcEpci } from '@/lib/queries/postgis/cartographie';
+import { TabTooltip } from '@/lib/utils/TabTooltip';
+import dynamic from 'next/dynamic';
+import { useStyles } from 'tss-react/dsfr';
+import styles from '../donnees.module.scss';
 
 interface Props {
   data: Array<{
@@ -38,144 +43,178 @@ interface VegetalisationProps {
   clc: CLC[];
 }
 
-const DynamicVegetalisation = dynamic(() => import("../../../components/themes/inconfort-thermique/vegetalisation"), {
-  ssr: false,
-  loading: () => <Loader />,
-});
+const DynamicVegetalisation = dynamic(
+  () => import('../../../components/themes/inconfort-thermique/vegetalisation'),
+  {
+    ssr: false,
+    loading: () => <Loader />
+  }
+);
 
 const allComps = [
   {
-    titre: "Grand âge",
-    Component: (
-      {inconfortThermique, data }: Props & { activeDataTab: string }
-      ) => <GrandAgeIsolement inconfortThermique={inconfortThermique} data={data} />,
+    titre: 'Grand âge',
+    Component: ({
+      inconfortThermique,
+      data
+    }: Props & { activeDataTab: string }) => (
+      <GrandAgeIsolement inconfortThermique={inconfortThermique} data={data} />
+    )
   },
   {
-    titre: "Fragilité économique",
-    Component: (
-      {carteCommunes}: Props & { activeDataTab: string }
-      ) => <FragiliteEconomique carteCommunes={carteCommunes} />,
+    titre: 'Fragilité économique',
+    Component: ({ carteCommunes }: Props & { activeDataTab: string }) => (
+      <FragiliteEconomique carteCommunes={carteCommunes} />
+    )
   },
   {
-    titre: "Travail en extérieur",
-    Component: (
-      {inconfortThermique}: Props & { activeDataTab: string }
-      ) => <TravailExterieur inconfortThermique={inconfortThermique} />,
+    titre: 'Travail en extérieur',
+    Component: ({ inconfortThermique }: Props & { activeDataTab: string }) => (
+      <TravailExterieur inconfortThermique={inconfortThermique} />
+    )
   },
   {
-    titre: "Age du bâtiment",
-    Component: (
-      {inconfortThermique}: Props & { activeDataTab: string }
-      ) => <AgeBati inconfortThermique={inconfortThermique} />,
+    titre: 'Age du bâtiment',
+    Component: ({ inconfortThermique }: Props & { activeDataTab: string }) => (
+      <AgeBati inconfortThermique={inconfortThermique} />
+    )
   },
   {
-    titre: "Densité du bâti",
-    Component: (
-      {carteCommunes}: Props & { activeDataTab: string }
-      ) => <DensiteBati carteCommunes={carteCommunes} />,
+    titre: 'Densité du bâti',
+    Component: ({ carteCommunes }: Props & { activeDataTab: string }) => (
+      <DensiteBati carteCommunes={carteCommunes} />
+    )
   },
-  // {
-  //   titre: "LCZ",
-  //   Component: (
-  //     {carteCommunes, collectivite}: Props & { activeDataTab: string }
-  //     ) => <LCZ carteCommunes={carteCommunes} collectivite={collectivite} />,
-  // },
   {
-    titre: "Végétalisation",
-    Component: (
-      {clc, inconfortThermique}: Props & VegetalisationProps & { activeDataTab: string }
-      ) => <DynamicVegetalisation inconfortThermique={inconfortThermique} clc={clc} />,
+    titre: 'LCZ',
+    Component: ({
+      carteCommunes,
+      collectivite
+    }: Props & { activeDataTab: string }) => (
+      <LCZ carteCommunes={carteCommunes} collectivite={collectivite} />
+    )
   },
+  {
+    titre: 'Végétalisation',
+    Component: ({
+      clc,
+      inconfortThermique
+    }: Props & VegetalisationProps & { activeDataTab: string }) => (
+      <DynamicVegetalisation
+        inconfortThermique={inconfortThermique}
+        clc={clc}
+      />
+    )
+  }
 ];
 
-const PageComp = ({ data, carteCommunes, inconfortThermique, collectivite, departement }: Props) => {
+const PageComp = ({
+  data,
+  carteCommunes,
+  inconfortThermique,
+  collectivite,
+  departement
+}: Props) => {
   const [clc, setClc] = useState<CLC[]>();
-  const [selectedTabId, setSelectedTabId] = useState("Population");
-  const [selectedSubTab, setSelectedSubTab] = useState("Grand âge");
+  const [selectedTabId, setSelectedTabId] = useState('Population');
+  const [selectedSubTab, setSelectedSubTab] = useState('Grand âge');
   const searchParams = useSearchParams();
-  const codepci = searchParams.get("codepci")!;
+  const codepci = searchParams.get('codepci')!;
   const { isDark } = useIsDark();
   const darkClass = {
-    backgroundColor: fr.colors.getHex({ isDark }).decisions.background.default.grey.active,
-    "&:hover": {
-      backgroundColor: fr.colors.getHex({ isDark }).decisions.background.alt.grey.hover,
-    },
+    backgroundColor: fr.colors.getHex({ isDark }).decisions.background.default
+      .grey.active,
+    '&:hover': {
+      backgroundColor: fr.colors.getHex({ isDark }).decisions.background.alt
+        .grey.hover
+    }
   };
   const { css } = useStyles();
 
   useEffect(() => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth",
-    })
+      behavior: 'smooth'
+    });
   }, []);
-  
+
   useEffect(() => {
-    setSelectedSubTab(data.filter(el => el.facteur_sensibilite === selectedTabId)[0].titre);
+    setSelectedSubTab(
+      data.filter((el) => el.facteur_sensibilite === selectedTabId)[0].titre
+    );
     void (async () => {
-      const temp = await GetClcEpci(codepci); 
+      const temp = await GetClcEpci(codepci);
       temp && codepci ? setClc(temp) : void 0;
     })();
   }, [selectedTabId, codepci]);
 
   return (
     <div className={styles.container}>
-      <Tabs 
-        selectedTabId={selectedTabId} 
+      <Tabs
+        selectedTabId={selectedTabId}
         tabs={[
           {
-            tabId: "Population",
-            label: <TabTooltip 
-              selectedTab={selectedTabId} 
-              tooltip="Santé et emploi des populations sont des indicateurs de sensibilité aux fortes chaleurs." 
-              titre="Population"
-            /> 
+            tabId: 'Population',
+            label: (
+              <TabTooltip
+                selectedTab={selectedTabId}
+                tooltip="Santé et emploi des populations sont des indicateurs de sensibilité aux fortes chaleurs."
+                titre="Population"
+              />
+            )
           },
-          { 
-            tabId: "Bâtiment", 
-            label: "Bâtiment"
+          {
+            tabId: 'Bâtiment',
+            label: 'Bâtiment'
           },
-          { 
-            tabId: "Urbanisme",
-            label: "Urbanisme"
-          },
-        ]} 
-        onTabChange={setSelectedTabId} 
-        className={css({
-          boxShadow: "none",
-          "::before": {
-            boxShadow: "inset 0 1px 0 0 var(--border-default-grey), inset 1px 0 0 0 transparent, inset -1px 0 0 0 transparent",
-          },
-          ".fr-tabs__tab[aria-selected=true]:not(:disabled)": {
-            color: "#0063CB",
-            backgroundImage: "linear-gradient(0deg, #0063CB, #0063CB), linear-gradient(0deg, var(--border-default-grey), var(--border-default-grey)), linear-gradient(0deg, var(--border-default-grey), var(--border-default-grey)), linear-gradient(0deg, var(--border-default-grey), var(--border-default-grey))",
-          },
-          ".fr-tabs__tab": {
-            margin: "0 0.25em 0 0",
-          },
-          ".fr-tabs__tab:not([aria-selected=true])": {
-            color: "#3A3A3A",
-            backgroundColor: "#F9F9FF",
-            "&:hover": {
-              backgroundColor: "#e9e9ff",
-            },
-          },
-          ".fr-tabs__panel": {
-            padding: "1rem 0"
-          },
-          ".fr-tabs__list": {
-            padding: "0",
-            margin: "0",
+          {
+            tabId: 'Urbanisme',
+            label: 'Urbanisme'
           }
-        })}>
+        ]}
+        onTabChange={setSelectedTabId}
+        className={css({
+          boxShadow: 'none',
+          '::before': {
+            boxShadow:
+              'inset 0 1px 0 0 var(--border-default-grey), inset 1px 0 0 0 transparent, inset -1px 0 0 0 transparent'
+          },
+          '.fr-tabs__tab[aria-selected=true]:not(:disabled)': {
+            color: '#0063CB',
+            backgroundImage:
+              'linear-gradient(0deg, #0063CB, #0063CB), linear-gradient(0deg, var(--border-default-grey), var(--border-default-grey)), linear-gradient(0deg, var(--border-default-grey), var(--border-default-grey)), linear-gradient(0deg, var(--border-default-grey), var(--border-default-grey))'
+          },
+          '.fr-tabs__tab': {
+            margin: '0 0.25em 0 0'
+          },
+          '.fr-tabs__tab:not([aria-selected=true])': {
+            color: '#3A3A3A',
+            backgroundColor: '#F9F9FF',
+            '&:hover': {
+              backgroundColor: '#e9e9ff'
+            }
+          },
+          '.fr-tabs__panel': {
+            padding: '1rem 0'
+          },
+          '.fr-tabs__list': {
+            padding: '0',
+            margin: '0'
+          }
+        })}
+      >
         <div className={styles.formContainer}>
           <div className={styles.titles}>
             {data
-              .filter(el => el.facteur_sensibilite === selectedTabId)
+              .filter((el) => el.facteur_sensibilite === selectedTabId)
               .map((element, i) => (
                 <button
                   key={i}
-                  className={selectedSubTab === element.titre ? styles.selectedButton : styles.button}
+                  className={
+                    selectedSubTab === element.titre
+                      ? styles.selectedButton
+                      : styles.button
+                  }
                   onClick={() => {
                     setSelectedSubTab(element.titre);
                   }}
@@ -187,7 +226,9 @@ const PageComp = ({ data, carteCommunes, inconfortThermique, collectivite, depar
           <div className={styles.bubble}>
             <div className={styles.bubbleContent} style={darkClass}>
               {(() => {
-                const Component = allComps.find(el => el.titre === selectedSubTab)?.Component;
+                const Component = allComps.find(
+                  (el) => el.titre === selectedSubTab
+                )?.Component;
                 if (!Component) return null;
                 return (
                   <Component
