b17a02fcbf4fe10788418a83a4dba2916bf742e1
AntoineConegero <antoine.conegero@gmail.com>
2024-12-18 13:12:59 +0100

    indicator: surfaces protegees fillColor territoire

--- src/components/maps/mapContourTerritoire.tsx
+++ src/components/maps/mapContourTerritoire.tsx
@@ -3,11 +3,11 @@
 import { CommunesContoursDto } from '@/lib/dto';
 import { MapContainer } from '@/lib/react-leaflet';
 import * as turf from '@turf/turf';
-import { BBox, GeoJsonObject, Position } from 'geojson';
-import L, { LatLngBoundsExpression } from 'leaflet';
+import { GeoJsonObject } from 'geojson';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useEffect, useRef, useState } from 'react';
-import { GeoJSON, Pane, Rectangle, TileLayer, useMap } from 'react-leaflet';
+import { GeoJSON, TileLayer, useMap } from 'react-leaflet';
 
 export const MapContourTerritoire = (props: {
   territoireContours: CommunesContoursDto[];
@@ -16,63 +16,65 @@ export const MapContourTerritoire = (props: {
   const { territoireContours, pourcentage } = props;
   const mapRef = useRef(null);
   const [data, setData] = useState<CommunesContoursDto[]>([]);
-  // const [bounds, setBounds] = useState<number[]>();
 
-  const rectBorders = [45.433554862, -1.243793323, 45.802437758, -0.707834253];
-  const outer = [
-    [45.433554862, -1.243793323],
-    [45.802437758, -0.707834253]
-  ];
+  const south = 45.433554862;
+  const north = 45.802437758;
 
-  const littleRect = [
-    [45.53, -1.0],
-    [45.78, -0.6]
-  ];
+  var hauteurTerritoire = north - south;
+  var percent = 90;
+  var newValue = south + (hauteurTerritoire * percent) / 100;
 
-  var points = turf.featureCollection([
-    turf.point([45.433554862, -1.243793323]),
-    turf.point([45.433554862, -0.707834253]),
-    turf.point([45.802437758, -0.707834253]),
-    turf.point([45.802437758, -1.243793323])
+  var polygon = turf.polygon([
+    [
+      [-1.243793323, 45.433554862],
+      [-0.707834253, 45.433554862],
+      [-0.707834253, newValue],
+      [-1.243793323, newValue],
+      [-1.243793323, 45.433554862]
+    ]
   ]);
 
-  var hull = turf.concave(points);
+  var union = turf.union(turf.featureCollection(territoireContours as any));
 
-  // console.log("polygonClipping", polygonClipping.intersection([outer], territoireContours[0]?.geometry.coordinates[0]));
+  var intersect = turf.intersect(turf.featureCollection([polygon, union]));
+  // console.log("intersect", intersect);
 
-  // console.log("hull", hull);
-
-  var rect = turf.square(rectBorders as BBox);
-
-  const test_rect = turf.lineString(rectBorders as unknown as Position[]);
-  const test_data = turf.polygon(
-    territoireContours[0]?.geometry.coordinates[0]
-  );
-
-  // console.log("data", test_data);
-
-  // console.log("intersect", turf.intersect(turf.featureCollection([hull, test_data])));
-
-  // var intersection = turf.intersect(turf.featureCollection([data[0].geometry.coordinates[0], rect]));
-  // console.log("intersection", intersection);
+  // console.log("union", union);
 
   const ContourTerritoire = () => {
     const map = useMap();
-
     useEffect(() => {
       setData(territoireContours);
     }, [territoireContours]);
 
     if (data.length > 0) {
       const geojsonObject = L.geoJSON(data as unknown as GeoJsonObject);
+      // const polygonLayer = L.geoJSON(polygon);
+
       map.fitBounds(geojsonObject.getBounds());
       // setBounds(geojsonObject.getBounds().toBBoxString().split(",").map(Number));
       return (
         <>
           <GeoJSON
             ref={mapRef}
-            data={data as unknown as GeoJsonObject}
-            style={style}
+            data={intersect as unknown as GeoJsonObject}
+            style={{
+              color: '#00C190',
+              weight: 1,
+              fillColor: '#00C190',
+              opacity: 1,
+              fillOpacity: 1
+            }}
+          />
+          <GeoJSON
+            ref={mapRef}
+            data={union as unknown as GeoJsonObject}
+            style={{
+              color: '#161616',
+              weight: 1,
+              fillColor: 'transparent',
+              opacity: 1
+            }}
           />
         </>
       );
@@ -107,21 +109,31 @@ export const MapContourTerritoire = (props: {
       dragging={false}
       boundsOptions={{ padding: [0, 0] }}
     >
+      <div
+        style={{
+          display: 'flex',
+          alignSelf: 'center',
+          zIndex: '501',
+          margin: 'auto',
+          backgroundColor: 'white',
+          padding: '1rem'
+        }}
+      >
+        <p
+          style={{
+            fontSize: '24px',
+            fontFamily: 'Marianne',
+            fontWeight: '700',
+            margin: '0'
+          }}
+        >
+          {percent} %
+        </p>
+      </div>
       <TileLayer
-        attribution='<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://tile.jawg.io/jawg-sunny/{z}/{x}/{y}{r}.png?access-token=MBbcKi3EyFqyyHvvHVbfnE4iOJ34FiUs1yWbVID476VAReeeO0NdrKWg6FljGBIC"
+        attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
       />
-      <Pane name="cyan-rectangle" style={{ zIndex: 500 }}>
-        <Rectangle
-          bounds={outer as LatLngBoundsExpression}
-          pathOptions={{ color: 'orange' }}
-        />
-        <Rectangle
-          bounds={littleRect as LatLngBoundsExpression}
-          pathOptions={{ color: 'cyan' }}
-        />
-      </Pane>
-      {/* <div style={{backgroundColor: "lightblue", opacity: "0.5", height: `${pourcentage}%`, width: "100%", alignSelf: "end"}} /> */}
       <ContourTerritoire />
     </MapContainer>
   );
