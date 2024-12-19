86ab878593b0a2100e45335c245d0972e70ea8c9
AntoineConegero <antoine.conegero@gmail.com>
2024-12-19 11:18:11 +0100

    indicator: surfaces protegees treemap colors + legend

--- src/components/themes/biodiversite/surfacesProtegeesDataviz.tsx
+++ src/components/themes/biodiversite/surfacesProtegeesDataviz.tsx
@@ -1,14 +1,12 @@
 'use client';
 
+import { NivoTreeMap } from '@/components/charts/NivoTreeMap';
 import { MapContourTerritoire } from '@/components/maps/mapContourTerritoire';
 import SubTabs from '@/components/SubTabs';
-import { SurfacesProtegeesDto } from '@/lib/dto';
 import { SurfacesProtegeesGraphMapper } from '@/lib/mapper/biodiversite';
 import { CommunesContourMapper } from '@/lib/mapper/communes';
 import { CarteCommunes, SurfacesProtegeesByCol } from '@/lib/postgres/models';
 import { Round } from '@/lib/utils/reusableFunctions/round';
-import { Any } from '@/lib/utils/types';
-import { ResponsiveTreeMap } from '@nivo/treemap';
 import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import styles from './biodiversite.module.scss';
@@ -32,25 +30,6 @@ const Filter = (
     .map((sp) => Number(sp[filter]))
     .reduce((a, b) => a + b, 0);
   return filtered;
-};
-
-const filterNullValues = (data: SurfacesProtegeesDto) => {
-  const tempEntries = Object.entries(data)[2][1] as GraphData[];
-  const tempFiltered = tempEntries.map((e) => {
-    if (e.children.filter((el) => el.loc !== 0).length != 0) {
-      return {
-        name: e.name,
-        color: e.color,
-        children: e.children.filter((el) => el.loc !== 0)
-      };
-    } else return {};
-  });
-  const newEntries = [
-    ['name', 'Surfaces protégées'],
-    ['color', 'hsl(271, 70%, 50%)'],
-    ['children', tempFiltered]
-  ];
-  return Object.fromEntries(newEntries);
 };
 
 const SurfacesProtegeesDataviz = (props: {
@@ -103,6 +82,13 @@ const SurfacesProtegeesDataviz = (props: {
         1
       );
 
+  const legends = data.children.map((e) => {
+    return {
+      name: e.name,
+      color: e.color
+    };
+  });
+
   return (
     <div className={styles.graphWrapper}>
       <div className={styles.dataVizGraphTitleWrapper}>
@@ -114,104 +100,39 @@ const SurfacesProtegeesDataviz = (props: {
         />
       </div>
       {datavizTab === 'Répartition' ? (
-        <div
-          style={{
-            height: '500px',
-            minWidth: '450px',
-            backgroundColor: 'white'
-          }}
-        >
-          <ResponsiveTreeMap
-            data={filterNullValues(data)}
-            nodeComponent={(e: Any) => {
-              console.log(e);
-              return (
-                <g transform={`translate(${e.node.x},${e.node.y})`}>
-                  <rect
-                    rx={8}
-                    width={e.node.width}
-                    height={e.node.height}
-                    fill={e.node.color}
-                    fillOpacity={1}
-                  />
-                  {e.node.height > 20 && e.node.width > 48 ? (
-                    <>
-                      <text
-                        x={e.node.width / 2}
-                        y={e.node.height / 2 - 12}
-                        textAnchor="middle"
-                        dominantBaseline="central"
-                        style={{
-                          fontSize: 18,
-                          fontWeight: 700,
-                          fill: '#FFFFFF',
-                          fontFamily: 'Marianne'
-                        }}
-                      >
-                        {e.node.formattedValue?.length > e.node.width / 6
-                          ? e.node.formattedValue?.slice(
-                              0,
-                              e.node.width / 6 - 1
-                            ) + '...'
-                          : e.node.formattedValue + ' ha'}
-                      </text>
-                      <text
-                        x={e.node.width / 2}
-                        y={e.node.height / 2 + 12}
-                        textAnchor="middle"
-                        dominantBaseline="central"
-                        style={{
-                          fontSize: 14,
-                          fontWeight: 500,
-                          fill: '#FFFFFF',
-                          fontFamily: 'Marianne'
-                        }}
-                      >
-                        {e.node.id?.length > e.node.width / 8
-                          ? e.node.id?.slice(0, e.node.width / 8 - 1) + '...'
-                          : e.node.id}
-                      </text>
-                    </>
-                  ) : (
-                    ''
-                  )}
-                </g>
-              );
+        <div>
+          <NivoTreeMap data={data} />
+          <div
+            style={{
+              backgroundColor: 'white',
+              padding: '0 2em 1em',
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              flexWrap: 'wrap'
             }}
-            identity="name"
-            value="loc"
-            valueFormat=">-.0f"
-            tile="squarify"
-            leavesOnly={true}
-            margin={{ top: 30, right: 50, bottom: 30, left: 30 }}
-            labelSkipSize={12}
-            label={(e) => {
-              const maxWidth = e.width / 6;
-              return e.id?.length > maxWidth
-                ? e.id?.slice(0, maxWidth - 1) + '...'
-                : e.id + ' : ' + e.formattedValue + ' ha';
-            }}
-            orientLabel={false}
-            innerPadding={4}
-            nodeOpacity={1}
-            // parentLabelTextColor={'#000000'}
-            parentLabelSize={25}
-            labelTextColor={'#FFFFFF'}
-            borderColor={{
-              from: 'color'
-            }}
-            tooltip={({ node }) => (
-              <div className={styles.tooltipSurfacesProtegeesWrapper}>
+          >
+            {legends.map((e) => (
+              <div
+                key={e.name}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  flexDirection: 'row'
+                }}
+              >
                 <div
-                  className={styles.color}
-                  style={{ backgroundColor: node.color }}
+                  style={{
+                    backgroundColor: e.color,
+                    width: '20px',
+                    height: '20px',
+                    marginRight: '10px'
+                  }}
                 ></div>
-                <p>
-                  <b>{node.id}</b> : {node.formattedValue} ha
-                </p>
+                <p className="m-0 p-2">{e.name}</p>
               </div>
-            )}
-          />
+            ))}
+          </div>
         </div>
       ) : datavizTab === 'Cartographie' ? (
         <>
