e38dc03eaf55203b2df0142185db6c8cd37df09e
AntoineConegero <antoine.conegero@gmail.com>
2025-01-02 16:35:55 +0100

    feat: fix page stat

--- src/app/(main)/stats/metrics/epciCount.tsx
+++ src/app/(main)/stats/metrics/epciCount.tsx
@@ -5,12 +5,16 @@ const EpciCount = async () => {
   const totalEpci = await GetInsights('9LrQuRLc');
   return (
     <Container m="4w">
-      <div style={{ margin: '0 0 2rem' }}>
-        <h2>Nombre total de collectivités recherchées</h2>
-        <p>
-          {totalEpci[0].count} depuis le {totalEpci[0].labels[0]}
-        </p>
-      </div>
+      {totalEpci ? (
+        <div style={{ margin: '0 0 2rem' }}>
+          <h2>Nombre total de collectivités recherchées</h2>
+          <p>
+            {totalEpci[0].count} depuis le {totalEpci[0].labels[0]}
+          </p>
+        </div>
+      ) : (
+        ''
+      )}
     </Container>
   );
 };
