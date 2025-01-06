2f13b4a371732b54b24f5ba5af36f1587e686b83
AntoineConegero <antoine.conegero@gmail.com>
2025-01-06 13:51:11 +0100

    indicator: finition espaces NAF barchart

--- src/app/(main)/donnees-territoriales/thematiques/biodiversite.tsx
+++ src/app/(main)/donnees-territoriales/thematiques/biodiversite.tsx
@@ -4,7 +4,6 @@ import {
   GetSurfacesProtegees
 } from '@/lib/queries/databases/biodiversite';
 import { GetCommunes, GetEpci } from '@/lib/queries/postgis/cartographie';
-import { GetEtatCoursDeau } from '@/lib/queries/postgis/etatCoursDeau';
 import { GetAgricultureBio, GetBiodiversite } from '@/lib/queries/thematiques';
 import { themes } from '@/lib/utils/themes';
 import dynamic from 'next/dynamic';
@@ -25,7 +24,6 @@ const Biodiversite = async (searchParams: SearchParams) => {
   const dbSurfacesProtegees = await GetSurfacesProtegees(codepci);
   const dbConsommationNAF = await GetConsommationNAF(codepci);
   const epciContours = await GetEpci(codepci);
-  const dbEtatCoursDeau = await GetEtatCoursDeau(codepci, codgeo);
 
   return (
     <div>
@@ -38,7 +36,6 @@ const Biodiversite = async (searchParams: SearchParams) => {
           surfacesProtegees={dbSurfacesProtegees}
           consommationNAF={dbConsommationNAF}
           epciContours={epciContours}
-          etatCoursDeau={dbEtatCoursDeau}
         />
       </div>
     </div>
