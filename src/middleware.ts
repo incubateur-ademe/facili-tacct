d9bc3ffb5c7d88452b0e52b287c3f5e4df2b31d4
AntoineConegero <antoine.conegero@gmail.com>
2025-02-04 17:57:09 +0100

    feat: new db with api

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,3 @@
-// import { setCookie } from "cookies-next";
-// import { type NextRequest, NextResponse } from "next/server";
-
-// export function middleware(req: NextRequest) {
-//   const res = NextResponse.next();
-//   const rand = Math.random().toString(36).substring(2);
-//   const rand2 = Math.random().toString(36).substring(2);
-
-//   setCookie("token", rand + rand2, { res, req });
-
-//   return res;
-// }
-
-// export const config = {
-//   matcher: "/",
-// };
-
 export { default } from 'next-auth/middleware';
 
 export const config = {
