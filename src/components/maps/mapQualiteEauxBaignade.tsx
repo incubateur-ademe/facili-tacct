3ee073f5d483efdbf7ebda43b92588b86bc80da9
AntoineConegero <antoine.conegero@gmail.com>
2025-02-06 13:02:03 +0100

    indicator: init qualite sites baignade

---
+++ src/components/maps/mapQualiteEauxBaignade.tsx
@@ -0,0 +1,237 @@
+'use client';
+
+import { CommunesIndicateursDto, EpciContoursDto } from '@/lib/dto';
+import { QualiteSitesBaignade } from '@/lib/postgres/models';
+import {
+  GeoJSON,
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer
+} from '@/lib/react-leaflet';
+import { Any } from '@/lib/utils/types';
+import * as turf from '@turf/turf';
+import { Position } from 'geojson';
+import L, {
+  LatLngBoundsExpression,
+  LatLngExpression,
+  LeafletMouseEvent,
+  StyleFunction
+} from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { useSearchParams } from 'next/navigation';
+import { useRef } from 'react';
+import MarkerClusterGroup from 'react-leaflet-cluster';
+import './maps.css';
+// documentation : https://akursat.gitbook.io/marker-cluster/api
+
+const color = (valeur: number) => {
+  return valeur > 36000
+    ? '#7A49BE'
+    : valeur > 27000
+      ? '#A67FE1'
+      : valeur > 18000
+        ? '#DB7BDD'
+        : valeur > 12000
+          ? '#FF9699'
+          : valeur > 6000
+            ? '#00C2CC'
+            : '#5EEDF3';
+};
+
+const getCentroid = (arr: number[][]) => {
+  const centroid = arr?.reduce(
+    (x: number[], y: number[]) => {
+      return [x[0] + y[0] / arr.length, x[1] + y[1] / arr.length];
+    },
+    [0, 0]
+  );
+  return [centroid[1], centroid[0]];
+};
+
+export const MapQualiteEauxBaignade = (props: {
+  qualiteEauxBaignade: QualiteSitesBaignade[];
+  epciContours: EpciContoursDto[];
+  carteCommunes: CommunesIndicateursDto[];
+}) => {
+  const { qualiteEauxBaignade, epciContours, carteCommunes } = props;
+  const searchParams = useSearchParams();
+  const codgeo = searchParams.get('codgeo')!;
+  const mapRef = useRef(null);
+  const markerRef = useRef(null);
+
+  const commune = carteCommunes.find(
+    (commune) => commune.properties.code_commune === codgeo
+  );
+  const centerCoord: number[] = commune
+    ? commune.properties.coordinates.split(',').map(Number)
+    : getCentroid(epciContours[0]?.geometry?.coordinates[0][0]);
+
+  const qualiteEauxmap = qualiteEauxBaignade.map((qualite) => {
+    return {
+      coordinates: [qualite.LAT, qualite.LONG],
+      nomSite: qualite.POINT,
+      qualite2013: qualite.QEB_2013,
+      qualite2014: qualite.QEB_2014,
+      qualite2015: qualite.QEB_2015,
+      qualite2016: qualite.QEB_2016,
+      qualite2017: qualite.QEB_2017,
+      qualite2018: qualite.QEB_2018,
+      qualite2019: qualite.QEB_2019,
+      qualite2020: qualite.QEB_2020,
+      icon: L.divIcon({
+        html: `
+          <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="9" cy="9" r="9" fill="red" />
+          </svg>`,
+        className: 'svg-marker',
+        iconSize: [24, 24],
+        iconAnchor: [0, 0]
+        // shadowUrl: "marker_icon_blue.svg",
+        // popupAnchor: [-3, -76],
+        // shadowSize: [0, 0],
+        // shadowAnchor: [22, 94],
+      })
+    };
+  });
+
+  const polygonTerritoire = commune
+    ? turf.multiPolygon(commune?.geometry.coordinates as Position[][][])
+    : turf.multiPolygon(
+        epciContours[0]?.geometry.coordinates as Position[][][]
+      );
+
+  const enveloppe = turf
+    .bboxPolygon(turf.bbox(turf.envelope(polygonTerritoire)))
+    .geometry.coordinates[0].map((coord) => [coord[1], coord[0]]);
+  const pointCollection = qualiteEauxmap.map((e) => {
+    return turf.point(e.coordinates as number[]);
+  });
+  const featureCollection = turf.featureCollection(pointCollection);
+  const nearestPoint = turf.nearestPoint(
+    turf.point([centerCoord[0], centerCoord[1]]),
+    featureCollection
+  );
+  const bbox = turf.bbox(
+    turf.featureCollection([nearestPoint, turf.point(centerCoord)])
+  );
+  const boundsIfNoPoint = [
+    [bbox[0], bbox[1]],
+    [bbox[2], bbox[3]]
+  ];
+
+  const territoireStyle: StyleFunction<Any> = () => {
+    return {
+      weight: 1,
+      opacity: 1,
+      color: '#161616',
+      fillOpacity: 0
+    };
+  };
+
+  const createClusterCustomIcon = function (cluster: any) {
+    const number = cluster.getChildCount();
+    const size = number < 4 ? 40 : 40 + number > 65 ? 65 : 40 + number;
+    return L.divIcon({
+      html: `<span>${number}</span>`,
+      className: 'custom-marker-cluster',
+      iconSize: L.point(size, size, true)
+      // iconSize: [45, 45],
+      // iconAnchor: [15, 15]
+    });
+  };
+
+  const CustomTooltip = () => {
+    return `<div style="padding: 0.25rem">
+        <div style="font-size: 0.75rem; font-family: Marianne; font-weight: 700;"> Contenu cluster hover</div>
+      </div>`;
+  };
+
+  return (
+    <MapContainer
+      zoom={commune ? 11 : 9}
+      ref={mapRef}
+      style={{ height: '500px', width: '100%', cursor: 'pointer' }}
+      attributionControl={false}
+      zoomControl={false}
+      bounds={
+        turf.booleanPointInPolygon(nearestPoint, turf.polygon([enveloppe]))
+          ? (enveloppe as LatLngBoundsExpression)
+          : (boundsIfNoPoint as LatLngBoundsExpression)
+      }
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+      />
+      <GeoJSON
+        data={commune ?? (epciContours as Any)}
+        style={territoireStyle}
+      />
+      <MarkerClusterGroup
+        chunkedLoading
+        removeOutsideVisibleBounds={true}
+        maxClusterRadius={155}
+        iconCreateFunction={createClusterCustomIcon}
+        polygonOptions={{
+          color: 'transparent',
+          fillOpacity: 0
+        }}
+        onMouseOver={(e: LeafletMouseEvent) => {
+          e.layer.bindTooltip(CustomTooltip(), {
+            opacity: 0.97,
+            direction: 'top',
+            offset: [0, -20]
+          });
+          e.layer.openTooltip();
+        }}
+      >
+        {qualiteEauxmap.map((el, i) => {
+          return (
+            <Marker
+              key={i}
+              icon={el.icon}
+              position={el.coordinates as LatLngExpression}
+              ref={markerRef}
+              eventHandlers={{
+                mouseover: (event) => {
+                  event.target.openPopup();
+                },
+                mouseout: (event) => {
+                  event.target.closePopup();
+                }
+              }}
+            >
+              <style>
+                {`
+                .leaflet-popup-content {
+                  margin: 0 !important;
+                  width: fit-content !important;
+                }
+                .leaflet-popup-content p {
+                  margin: 0 !important;
+                }
+                .leaflet-popup-close-button {
+                  display: none !important;
+                }
+                .leaflet-interactive {
+                  cursor: pointer;
+                }
+                `}
+                <Popup offset={[6, 8]}>
+                  <div className="p-[0.75rem]">
+                    <div className="flex flex-row justify-between p-0 gap-2 items-center w-max">
+                      <p className="text-[0.75rem] font-marianne font-[400]">
+                        {el.nomSite} :{' '}
+                      </p>
+                    </div>
+                  </div>
+                </Popup>
+              </style>
+            </Marker>
+          );
+        })}
+      </MarkerClusterGroup>
+    </MapContainer>
+  );
+};
