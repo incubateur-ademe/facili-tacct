0f64a49918410bfa096817272335fc3172f595f7
AntoineConegero <antoine.conegero@gmail.com>
2025-02-17 13:23:10 +0100

    feat: page stat

--- src/app/(main)/stats/page.tsx
+++ src/app/(main)/stats/page.tsx
@@ -1,16 +1,30 @@
-import EpciCount from './metrics/epciCount';
+import { Container } from '@/dsfr/server';
+import Breadcrumb from '@codegouvfr/react-dsfr/Breadcrumb';
+import { Metadata } from 'next';
 import RessourcesClicked from './metrics/ressourcesClicked';
 import ThematiquesTypes from './metrics/thematiquesTypes';
 import UniqueUsers from './metrics/uniqueUsers';
 
+export const metadata: Metadata = {
+  title: 'Stats',
+  description: 'Stats'
+};
+
 const Page = async () => {
   return (
-    <div>
+    <Container size="xl" className="mb-24">
+      <Breadcrumb
+        currentPageLabel="Stats"
+        homeLinkProps={{
+          href: '/'
+        }}
+        segments={[]}
+      />
       <UniqueUsers />
-      <EpciCount />
+      {/* <EpciCount /> */}
       <ThematiquesTypes />
       <RessourcesClicked />
-    </div>
+    </Container>
   );
 };
 
