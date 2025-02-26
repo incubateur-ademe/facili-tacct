5d6f4b98f88bb40d9599cd35d92c5e7ad9963471
AntoineConegero <antoine.conegero@gmail.com>
2025-02-26 10:58:08 +0100

    refactor: all legends

--- src/components/themes/biodiversite/consommationEspacesNAFDataviz.tsx
+++ src/components/themes/biodiversite/consommationEspacesNAFDataviz.tsx
@@ -1,37 +1,11 @@
 'use client';
 
+import { espacesNAFDatavizLegend } from '@/components/maps/legends/datavizLegends';
 import { LegendCompColor } from '@/components/maps/legends/legendComp';
 import { MapEspacesNaf } from '@/components/maps/mapEspacesNAF';
 import { CommunesIndicateursDto } from '@/lib/dto';
 import { ConsommationNAF } from '@/lib/postgres/models';
 import styles from './biodiversite.module.scss';
-
-const legends = [
-  {
-    value: '0-1',
-    color: '#D8EFFA'
-  },
-  {
-    value: '1-2',
-    color: '#FFECEE'
-  },
-  {
-    value: '2-5',
-    color: '#FF9699'
-  },
-  {
-    value: '5-10',
-    color: '#E8323B'
-  },
-  {
-    value: '10-20',
-    color: '#B5000E'
-  },
-  {
-    value: '> 20',
-    color: '#680000'
-  }
-];
 
 export const ConsommationEspacesNAFDataviz = (props: {
   consommationNAF: ConsommationNAF[];
@@ -52,7 +26,7 @@ export const ConsommationEspacesNAFDataviz = (props: {
           className={styles.legend}
           style={{ width: 'auto', justifyContent: 'center' }}
         >
-          <LegendCompColor legends={legends} />
+          <LegendCompColor legends={espacesNAFDatavizLegend} />
         </div>
       </>
       <p style={{ padding: '1em', margin: '0' }}>Source : CEREMA, avril 2024</p>
