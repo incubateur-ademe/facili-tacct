a481ceb35335448b5808a7548407247c02d94f13
AntoineConegero <antoine.conegero@gmail.com>
2025-03-06 14:36:43 +0100

    indicator: feux foret

--- src/components/charts/gestionRisques/pieChartFeuxForet.tsx
+++ src/components/charts/gestionRisques/pieChartFeuxForet.tsx
@@ -1,6 +1,7 @@
 // @ts-nocheck
 'use client';
 
+import { IncendiesForet } from '@/lib/postgres/models';
 import { CountOcc } from '@/lib/utils/reusableFunctions/occurencesCount';
 import { Sum } from '@/lib/utils/reusableFunctions/sum';
 import { Any } from '@/lib/utils/types';
@@ -8,44 +9,33 @@ import { DefaultRawDatum, PieCustomLayerProps, ResponsivePie } from '@nivo/pie';
 import { animated } from '@react-spring/web';
 
 const colors: { [key: string]: string } = {
-  Inondations: '#009ADC',
-  Sécheresse: '#FFCF5E',
-  'Mouvements de terrain': '#F66E19',
-  'Retrait-gonflement des argiles': '#BB43BD',
-  'Cyclones / Tempêtes': '#00C2CC',
-  'Grêle / neige': '#00C190',
-  Avalanche: '#7A49BE'
+  Malveillance: '#91D1CC',
+  Accidentelle: '#038278',
+  'Involontaire (particulier)': '#095D55',
+  'Involontaire (travaux)': '#05413B',
+  Naturelle: '#D3EDEB'
 };
 
-const PieChartCatnat = (props: { gestionRisques: ArreteCatNat[] }) => {
-  const { gestionRisques } = props;
-  const countTypes = CountOcc(gestionRisques, 'lib_risque_jo');
-  const mapGraphData = gestionRisques?.map((el) => {
-    return {
-      id: el.lib_risque_jo ?? '',
-      label: el.lib_risque_jo ?? '',
-      value: countTypes[el.lib_risque_jo!]
-    };
-  });
-  const graphData = mapGraphData.filter(
-    (value, index, self) =>
-      index === self.findIndex((t) => t.label === value.label)
-  );
+const PieChartFeuxForet = (props: { incendiesForet: IncendiesForet[] }) => {
+  const { incendiesForet } = props;
+  const countTypes = CountOcc(incendiesForet, 'nature');
+  const causesInconnues = countTypes['null'];
+  delete countTypes['null'];
+  const graphData = Object.entries(countTypes).map(([id, value]) => ({
+    id,
+    value
+  }));
 
   const CenteredMetric = ({
     dataWithArc,
     centerX,
     centerY
   }: PieCustomLayerProps<DefaultRawDatum>) => {
-    let total = 0;
-    dataWithArc.forEach((datum: { value: number }) => {
-      total += datum.value;
-    });
     return (
       <>
         <text
           x={centerX}
-          y={centerY}
+          y={centerY - 10}
           textAnchor="middle"
           dominantBaseline="central"
           style={{
@@ -53,11 +43,11 @@ const PieChartCatnat = (props: { gestionRisques: ArreteCatNat[] }) => {
             fontWeight: 700
           }}
         >
-          {total}
+          {causesInconnues ?? '0'}
         </text>
         <text
           x={centerX}
-          y={centerY + 30}
+          y={centerY + 20}
           textAnchor="middle"
           dominantBaseline="central"
           style={{
@@ -65,7 +55,7 @@ const PieChartCatnat = (props: { gestionRisques: ArreteCatNat[] }) => {
             fontWeight: 400
           }}
         >
-          arrêté(s) CatNat
+          {causesInconnues ? 'Causes inconnues' : 'Cause inconnue'}
         </text>
       </>
     );
@@ -88,13 +78,13 @@ const PieChartCatnat = (props: { gestionRisques: ArreteCatNat[] }) => {
             fontWeight: 400
           }}
         >
-          <animated.tspan>{label} : </animated.tspan>
+          <animated.tspan>{label} </animated.tspan>
           <animated.tspan style={{ fontWeight: 600 }}>
             {datum.value}{' '}
           </animated.tspan>
           <animated.tspan>
             ({((100 * datum.value) / Sum(Object.values(countTypes))).toFixed(1)}
-             %)
+            %)
           </animated.tspan>
         </animated.text>
       </animated.g>
@@ -109,23 +99,17 @@ const PieChartCatnat = (props: { gestionRisques: ArreteCatNat[] }) => {
         data={graphData}
         margin={{ top: 60, right: 80, bottom: 60, left: 80 }}
         colors={(graphData) => colors[graphData.id]}
-        isInteractive={true}
+        isInteractive={false}
         innerRadius={0.5}
         padAngle={1}
         cornerRadius={3}
         activeOuterRadiusOffset={8}
         borderWidth={1}
         arcLinkLabelComponent={arcLabelsComponent}
-        arcLinkLabel={({ id }) => `${id}`}
+        // arcLinkLabel={({ id }) => `${id}`}
         arcLinkLabelsSkipAngle={7}
         sortByValue={false}
-        layers={[
-          'arcs',
-          'arcLabels',
-          'arcLinkLabels',
-          'legends',
-          CenteredMetric
-        ]}
+        layers={['arcs', 'arcLinkLabels', 'legends', CenteredMetric]}
         borderColor={{
           from: 'color',
           modifiers: [['darker', 0.2]]
@@ -141,4 +125,4 @@ const PieChartCatnat = (props: { gestionRisques: ArreteCatNat[] }) => {
   );
 };
 
-export default PieChartCatnat;
+export default PieChartFeuxForet;
