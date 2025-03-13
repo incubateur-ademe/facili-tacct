ad0d5db10df318532f33c0cb1d14318990cd4daf
AntoineConegero <antoine.conegero@gmail.com>
2025-03-13 15:58:33 +0100

    feat: design search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,20 +5,24 @@ import { SearchBar } from '@codegouvfr/react-dsfr/SearchBar';
 import { Select } from '@codegouvfr/react-dsfr/Select';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-
-import './globalMui.css';
+import { useStyles } from 'tss-react/dsfr';
 import { MySearchInput } from './SearchInput';
+import './globalMui.css';
 
 export const SearchBarComp = () => {
   const router = useRouter();
+  const { css } = useStyles();
   const [epciCode, setEpciCode] = useState<string>('');
   const [searchCode, setSearchCode] = useState<string>('');
+  const [searchLibelle, setSearchLibelle] = useState<string>('');
   const [typeTerritoire, setTypeTerritoire] = useState<string>('');
-  console.log('typeTerritoire', typeTerritoire);
+
   const getCodeFromSearchBar = (code: string) => {
     setSearchCode(code);
   };
-
+  const getLibelleFromSearchBar = (libelle: string) => {
+    setSearchLibelle(libelle);
+  };
   const getEpciCodeFromSearchBar = (code: string) => {
     setEpciCode(code);
   };
@@ -33,11 +37,29 @@ export const SearchBarComp = () => {
 
   return (
     <div
-      style={{ display: 'flex', flexDirection: 'row', width: '100%', gap: 5 }}
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
+        gap: 8,
+        height: 42
+      }}
     >
       <Select
         label=""
-        style={{ width: 200 }}
+        className={css({
+          '.fr-select': {
+            boxShadow: 'none',
+            border: '1px solid #0063CB',
+            borderRadius: '4px 0 0 4px',
+            backgroundColor: 'white',
+            color: '#0063CB',
+            '&:focus': {
+              outline: 'none'
+            }
+          }
+        })}
+        style={{ width: 370 }}
         nativeSelectProps={{
           onChange: (e) => {
             setTypeTerritoire(e.target.value);
@@ -45,17 +67,66 @@ export const SearchBarComp = () => {
         }}
       >
         <>
-          <option disabled hidden selected value="">
+          <option
+            disabled
+            hidden
+            value=""
+            selected
+            style={{ color: '#161616' }}
+          >
             Type de territoire
           </option>
-          <option value="commune">Communes</option>
-          <option value="epci">EPCI/EPT</option>
-          <option value="petr">PETR</option>
-          <option value="pnr">PNR</option>
+          <option value="commune" style={{ color: '#161616' }}>
+            Communes
+          </option>
+          <option value="epci" style={{ color: '#161616' }}>
+            EPCI/EPT
+          </option>
+          <option value="petr" style={{ color: '#161616' }}>
+            PETR
+          </option>
+          <option value="pnr" style={{ color: '#161616' }}>
+            PNR
+          </option>
         </>
       </Select>
       <SearchBar
-        style={{ width: 300 }}
+        className={
+          typeTerritoire.length
+            ? css({
+                border: '1px solid #0063CB',
+                height: 'inherit',
+                '.fr-input': {
+                  color: '#0063CB',
+                  backgroundColor: 'white',
+                  boxShadow: 'none',
+                  borderRadius: '0',
+                  '&:focus': {
+                    outline: 'none'
+                  },
+                  '&::placeholder': {
+                    color: '#7B7B7B'
+                  }
+                }
+              })
+            : css({
+                border: '1px solid #EEEEEE',
+                height: 'inherit',
+                '.fr-input': {
+                  color: '#0063CB',
+                  backgroundColor: '#EEEEEE',
+                  boxShadow: 'none',
+                  borderRadius: '0',
+                  '&:focus': {
+                    outline: 'none'
+                  },
+                  '&::placeholder': {
+                    color: '#7B7B7B'
+                  }
+                }
+              })
+        }
+        style={{ minWidth: 300, width: '100%' }}
         renderInput={({ className, id, placeholder, type }) => (
           <MySearchInput
             className={className}
@@ -64,16 +135,31 @@ export const SearchBarComp = () => {
             type={type}
             typeTerritoire={typeTerritoire}
             searchCodeFromSearchBar={getCodeFromSearchBar}
+            searchLibelleFromSearchBar={getLibelleFromSearchBar}
             searchEpciCodeFromSearchBar={getEpciCodeFromSearchBar}
           />
         )}
       />
-      {searchCode.length === 0 ? (
-        <Button disabled style={{ display: 'block' }}>
+      {searchLibelle.length === 0 ? (
+        <Button
+          disabled
+          style={{
+            display: 'block',
+            borderRadius: '0 4px 4px 0'
+          }}
+        >
           Continuer
         </Button>
       ) : (
-        <Button onClick={handleClick} style={{ display: 'block' }}>
+        <Button
+          onClick={handleClick}
+          style={{
+            display: 'block',
+            backgroundColor: '#0063CB',
+            color: 'white',
+            borderRadius: '0 4px 4px 0'
+          }}
+        >
           Continuer
         </Button>
       )}
