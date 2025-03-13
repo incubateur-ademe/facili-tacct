4a702a273924d5a12428ef159c2202b88984252d
AntoineConegero <antoine.conegero@gmail.com>
2025-03-13 12:27:46 +0100

    feat: queries searchbar pour tous les territoires

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,6 +2,7 @@
 
 import Button from '@codegouvfr/react-dsfr/Button';
 import { SearchBar } from '@codegouvfr/react-dsfr/SearchBar';
+import { Select } from '@codegouvfr/react-dsfr/Select';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
@@ -12,7 +13,8 @@ export const SearchBarComp = () => {
   const router = useRouter();
   const [epciCode, setEpciCode] = useState<string>('');
   const [searchCode, setSearchCode] = useState<string>('');
-
+  const [typeTerritoire, setTypeTerritoire] = useState<string>('');
+  console.log('typeTerritoire', typeTerritoire);
   const getCodeFromSearchBar = (code: string) => {
     setSearchCode(code);
   };
@@ -30,15 +32,37 @@ export const SearchBarComp = () => {
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
+    <div
+      style={{ display: 'flex', flexDirection: 'row', width: '100%', gap: 5 }}
+    >
+      <Select
+        label=""
+        style={{ width: 200 }}
+        nativeSelectProps={{
+          onChange: (e) => {
+            setTypeTerritoire(e.target.value);
+          }
+        }}
+      >
+        <>
+          <option disabled hidden selected value="">
+            Type de territoire
+          </option>
+          <option value="commune">Communes</option>
+          <option value="epci">EPCI/EPT</option>
+          <option value="petr">PETR</option>
+          <option value="pnr">PNR</option>
+        </>
+      </Select>
       <SearchBar
-        style={{ width: 'inherit' }}
+        style={{ width: 300 }}
         renderInput={({ className, id, placeholder, type }) => (
           <MySearchInput
             className={className}
             id={id}
             placeholder={placeholder}
             type={type}
+            typeTerritoire={typeTerritoire}
             searchCodeFromSearchBar={getCodeFromSearchBar}
             searchEpciCodeFromSearchBar={getEpciCodeFromSearchBar}
           />
