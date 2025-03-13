0b5882f7818c9b8f569afc50bb093c331d68188f
AntoineConegero <antoine.conegero@gmail.com>
2025-03-13 15:58:33 +0100

    feat: design search bar

--- src/app/(main)/CollectiviteSearch.tsx
+++ src/app/(main)/CollectiviteSearch.tsx
@@ -6,7 +6,7 @@ import styles from '../root.module.scss';
 export const CollectiviteSearch = () => {
   return (
     <div className={styles.collectiviteWrapper}>
-      <h5>Quelle collectivité représentez-vous ?</h5>
+      <h5>Quel territoire représentez-vous ?</h5>
       <p>
         Cette information nous aidera à vous apporter les informations
         pertinentes pour votre territoire
