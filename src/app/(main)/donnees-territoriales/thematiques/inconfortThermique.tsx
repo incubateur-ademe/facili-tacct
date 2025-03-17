2e506b313495a7c6b7ecfbae62d992b0f0a58f39
AntoineConegero <antoine.conegero@gmail.com>
2025-03-17 13:49:24 +0100

    feat: new requests for new territories

--- src/app/(main)/donnees-territoriales/thematiques/inconfortThermique.tsx
+++ src/app/(main)/donnees-territoriales/thematiques/inconfortThermique.tsx
@@ -14,7 +14,6 @@ const InconfortThermique = async (props: { searchParams: SearchParams }) => {
     libelle,
     type
   );
-  console.log('dbInconfortThermique', dbInconfortThermique.length);
   const carteCommunes = await GetCommunes(code, libelle, type);
 
   return (
