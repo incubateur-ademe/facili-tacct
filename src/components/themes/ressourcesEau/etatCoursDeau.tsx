fc40c621d436a507655c8244dd2a2859550be1bc
AntoineConegero <antoine.conegero@gmail.com>
2025-03-20 18:24:45 +0100

    fix: recettage design patch 4

--- src/components/themes/ressourcesEau/etatCoursDeau.tsx
+++ src/components/themes/ressourcesEau/etatCoursDeau.tsx
@@ -1,6 +1,7 @@
 import fortesChaleursIcon from '@/assets/icons/chaleur_icon_black.svg';
 import precipitationIcon from '@/assets/icons/precipitation_icon_black.svg';
 import { GraphDataNotFound } from '@/components/graph-data-not-found';
+import { Loader } from '@/components/loader';
 import { etatCoursDeauLegends } from '@/components/maps/legends/datavizLegends';
 import { LegendCompColor } from '@/components/maps/legends/legendComp';
 import { MapEtatCoursDeau } from '@/components/maps/mapEtatCoursDeau';
@@ -76,97 +77,105 @@ const EtatQualiteCoursDeau = (props: {
   );
   return (
     <>
-      {etatCoursDeau.length ? (
-        <div className={styles.container}>
-          <div className="w-5/12">
-            <div className={styles.explicationWrapper}>
-              <p>
-                La carte ci-contre reflète l’état écologique des cours d’eau
-                présents sur votre territoire. Le bon fonctionnement des milieux
-                aquatiques est évalué à partir d’éléments physico-chimiques
-                (composition de l’eau, polluants…) mais aussi de la présence de
-                la faune et de la flore (poissons, invertébrés, plantes
-                aquatiques), ainsi que des propriétés hydromorphologiques (état
-                des berges, continuité de la rivière, etc.).
-              </p>
-              <div className={styles.patch4Wrapper}>
-                {fortesChaleurs === 'Intensité très forte' ||
-                fortesChaleurs === 'Intensité forte' ? (
-                  <TagItem
-                    icon={fortesChaleursIcon}
-                    indice="Fortes chaleurs"
-                    tag={fortesChaleurs}
+      {precipitation && fortesChaleurs ? (
+        <>
+          {etatCoursDeau.length ? (
+            <div className={styles.container}>
+              <div className="w-5/12">
+                <div className={styles.explicationWrapper}>
+                  <p>
+                    La carte ci-contre reflète l’état écologique des cours d’eau
+                    présents sur votre territoire. Le bon fonctionnement des
+                    milieux aquatiques est évalué à partir d’éléments
+                    physico-chimiques (composition de l’eau, polluants…) mais
+                    aussi de la présence de la faune et de la flore (poissons,
+                    invertébrés, plantes aquatiques), ainsi que des propriétés
+                    hydromorphologiques (état des berges, continuité de la
+                    rivière, etc.).
+                  </p>
+                  <div className={styles.patch4Wrapper}>
+                    {fortesChaleurs === 'Intensité très forte' ||
+                    fortesChaleurs === 'Intensité forte' ? (
+                      <TagItem
+                        icon={fortesChaleursIcon}
+                        indice="Fortes chaleurs"
+                        tag={fortesChaleurs}
+                      />
+                    ) : null}
+                    {precipitation === 'Intensité très forte' ||
+                    precipitation === 'Intensité forte' ? (
+                      <TagItem
+                        icon={precipitationIcon}
+                        indice="Fortes précipitations"
+                        tag={precipitation}
+                      />
+                    ) : null}
+                  </div>
+                  <CustomTooltip
+                    title={title}
+                    texte="Sur quoi repose ce classement ?"
                   />
-                ) : null}
-                {precipitation === 'Intensité très forte' ||
-                precipitation === 'Intensité forte' ? (
-                  <TagItem
-                    icon={precipitationIcon}
-                    indice="Fortes précipitations"
-                    tag={precipitation}
-                  />
-                ) : null}
+                </div>
+                <div className="px-4">
+                  <p>
+                    Même pour des cours d’eau en bon état, les événements
+                    extrêmes dus au changement climatique aggravent les
+                    pollutions : en période de sécheresse, les polluants se
+                    concentrent ; lors des crues, ils sont massivement charriés
+                    vers les captages, augmentant la contamination.
+                  </p>
+                  <p>
+                    Certaines activités sont directement affectées par la
+                    qualité chimique des cours d’eau (pisciculture en eau douce,
+                    pêche professionnelle ou de loisir, sports d’eau vive…). Par
+                    ailleurs, 48 % de l’eau prélevée pour les usages
+                    domestiques, agricoles et industriels provient des eaux de
+                    surface. Une eau brute plus polluée nécessite des
+                    traitements plus complexes, ce qui augmente les coûts du
+                    service de l’eau.
+                  </p>
+                  <p>
+                    Concernant spécifiquement l’eau potable, si 2/3 des
+                    prélèvement sont fait sur des ressources souterraines, les
+                    prélèvements en eaux de surface sont majoritaires en région
+                    parisienne, en Bretagne, dans les Pays de la Loire, sur la
+                    Côte d’Azur et dans l’ancienne région Midi-Pyrénées.
+                  </p>
+                </div>
               </div>
-              <CustomTooltip
-                title={title}
-                texte="Sur quoi repose ce classement ?"
-              />
+              <div className="w-7/12">
+                <div className={styles.graphWrapper}>
+                  <div
+                    className={styles.ressourcesEauGraphTitleWrapper}
+                    style={{ padding: '1rem' }}
+                  >
+                    <h2>État écologique des cours d’eau</h2>
+                  </div>
+                  <div>
+                    <MapEtatCoursDeau
+                      etatCoursDeau={etatCoursDeauMap}
+                      epciContours={epciContoursMap}
+                      carteCommunes={carteCommunesMap}
+                    />
+                  </div>
+                  <div
+                    className={styles.legend}
+                    style={{ width: 'auto', justifyContent: 'center' }}
+                  >
+                    <LegendCompColor legends={etatCoursDeauLegends} />
+                  </div>
+                  <p style={{ padding: '1em', margin: '0' }}>
+                    Source : Agences de l'eau
+                  </p>
+                </div>
+              </div>
             </div>
-            <div className="px-4">
-              <p>
-                Même pour des cours d’eau en bon état, les événements extrêmes
-                dus au changement climatique aggravent les pollutions : en
-                période de sécheresse, les polluants se concentrent ; lors des
-                crues, ils sont massivement charriés vers les captages,
-                augmentant la contamination.
-              </p>
-              <p>
-                Certaines activités sont directement affectées par la qualité
-                chimique des cours d’eau (pisciculture en eau douce, pêche
-                professionnelle ou de loisir, sports d’eau vive…). Par ailleurs,
-                48 % de l’eau prélevée pour les usages domestiques, agricoles et
-                industriels provient des eaux de surface. Une eau brute plus
-                polluée nécessite des traitements plus complexes, ce qui
-                augmente les coûts du service de l’eau.
-              </p>
-              <p>
-                Concernant spécifiquement l’eau potable, si 2/3 des prélèvement
-                sont fait sur des ressources souterraines, les prélèvements en
-                eaux de surface sont majoritaires en région parisienne, en
-                Bretagne, dans les Pays de la Loire, sur la Côte d’Azur et dans
-                l’ancienne région Midi-Pyrénées.
-              </p>
-            </div>
-          </div>
-          <div className="w-7/12">
-            <div className={styles.graphWrapper}>
-              <div
-                className={styles.ressourcesEauGraphTitleWrapper}
-                style={{ padding: '1rem' }}
-              >
-                <h2>État écologique des cours d’eau</h2>
-              </div>
-              <div>
-                <MapEtatCoursDeau
-                  etatCoursDeau={etatCoursDeauMap}
-                  epciContours={epciContoursMap}
-                  carteCommunes={carteCommunesMap}
-                />
-              </div>
-              <div
-                className={styles.legend}
-                style={{ width: 'auto', justifyContent: 'center' }}
-              >
-                <LegendCompColor legends={etatCoursDeauLegends} />
-              </div>
-              <p style={{ padding: '1em', margin: '0' }}>
-                Source : Agences de l'eau
-              </p>
-            </div>
-          </div>
-        </div>
+          ) : (
+            <GraphDataNotFound code={codgeo ? codgeo : codepci} />
+          )}
+        </>
       ) : (
-        <GraphDataNotFound code={codgeo ? codgeo : codepci} />
+        <Loader />
       )}
     </>
   );
