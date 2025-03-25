c24a0c1380381bc9a171cc799a4ea192283a6f51
AntoineConegero <antoine.conegero@gmail.com>
2025-03-25 16:56:30 +0100

    style: update patch4

--- src/app/iframe/donnees-territoriales/thematiques/inconfortThermiqueComp.tsx
+++ src/app/iframe/donnees-territoriales/thematiques/inconfortThermiqueComp.tsx
@@ -7,7 +7,7 @@ import { useSearchParams } from 'next/navigation';
 import { Suspense, useEffect, useState } from 'react';
 
 import { Loader } from '@/components/loader';
-import { AgeBati } from '@/components/themes/inconfortThermique/age-bati';
+import { AgeBati } from '@/components/themes/inconfortThermique/ageBati';
 import { DensiteBati } from '@/components/themes/inconfortThermique/densite-bati';
 import { FragiliteEconomique } from '@/components/themes/inconfortThermique/fragilite-economique';
 import { GrandAgeIsolement } from '@/components/themes/inconfortThermique/grand-age-isolement';
