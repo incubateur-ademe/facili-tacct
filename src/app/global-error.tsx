20f97afd76b2d4d535d3fa26f272e95c6a969f76
AntoineConegero <antoine.conegero@gmail.com>
2025-03-26 14:32:16 +0100

    feat: nouveaux territoires requetes + travail ext

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -14,12 +14,8 @@ export default function GlobalError({
   }, [error]);
 
   return (
-    <html>
+    <html lang="fr" title="Erreur site Facili-TACCT">
       <body>
-        {/* `NextError` is the default Next.js error page component. Its type
-        definition requires a `statusCode` prop. However, since the App Router
-        does not expose status codes for errors, we simply pass 0 to render a
-        generic error message. */}
         <NextError statusCode={0} />
       </body>
     </html>
