1eeca102e8fea9418dfd6547f38e0d50962d60b6
Antoine <antoine.conegero@predict-a.com>
2025-03-31 00:07:26 +0200

    feat: clean modale home page

--- src/app/(main)/CollectiviteSearch.tsx
+++ src/app/(main)/CollectiviteSearch.tsx
@@ -5,23 +5,21 @@ import { ToggleTheme } from '@/components/ToggleTheme';
 import { useEffect } from 'react';
 import styles from '../root.module.scss';
 
-export const CollectiviteSearch = ({noticeClosed}: {noticeClosed: boolean}) => {
-  const style = document.documentElement.style;
-  const computedStyle = getComputedStyle(document.documentElement);
+export const CollectiviteSearch = ({ noticeClosed }: { noticeClosed: boolean }) => {
 
   useEffect(() => {
-    ToggleTheme(computedStyle, style)
-  }
-  , [noticeClosed]);
+    if (typeof document !== 'undefined') {
+      const style = document.documentElement.style;
+      const computedStyle = getComputedStyle(document.documentElement);
+      ToggleTheme(computedStyle, style);
+    }
+  }, [noticeClosed]);
+
   return (
     <div className={styles.collectiviteWrapper}  >
       <h2 className="text-center text-[1.5rem] font-bold">
         Quel territoire représentez-vous ?
       </h2>
-      {/* <p>
-        Cette information nous aidera à vous apporter les informations
-        pertinentes pour votre territoire
-      </p> */}
       <div>
         <SearchBarComp />
       </div>
