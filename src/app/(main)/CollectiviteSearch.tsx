d1329ca060acae50971a3458ff576f8c689c6c12
Antoine <antoine.conegero@predict-a.com>
2025-03-30 21:11:20 +0200

    feat: add notice comp home page

--- src/app/(main)/CollectiviteSearch.tsx
+++ src/app/(main)/CollectiviteSearch.tsx
@@ -1,11 +1,20 @@
 'use client';
 
 import { SearchBarComp } from '@/components/SearchBar';
+import { ToggleTheme } from '@/components/ToggleTheme';
+import { useEffect } from 'react';
 import styles from '../root.module.scss';
 
-export const CollectiviteSearch = () => {
+export const CollectiviteSearch = ({noticeClosed}: {noticeClosed: boolean}) => {
+  const style = document.documentElement.style;
+  const computedStyle = getComputedStyle(document.documentElement);
+
+  useEffect(() => {
+    ToggleTheme(computedStyle, style)
+  }
+  , [noticeClosed]);
   return (
-    <div className={styles.collectiviteWrapper}>
+    <div className={styles.collectiviteWrapper}  >
       <h2 className="text-center text-[1.5rem] font-bold">
         Quel territoire représentez-vous ?
       </h2>
