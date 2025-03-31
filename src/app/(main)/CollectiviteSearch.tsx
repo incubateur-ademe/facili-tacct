ba27065313de50c20e68871abe5dba2a137f8192
AntoineConegero <antoine.conegero@gmail.com>
2025-03-31 18:12:30 +0200

    fix: modale state

--- src/app/(main)/CollectiviteSearch.tsx
+++ src/app/(main)/CollectiviteSearch.tsx
@@ -1,20 +1,9 @@
 'use client';
 
 import { SearchBarComp } from '@/components/SearchBar';
-import { ToggleTheme } from '@/components/ToggleTheme';
-import { useEffect } from 'react';
 import styles from '../root.module.scss';
 
-export const CollectiviteSearch = ({ noticeClosed }: { noticeClosed: boolean }) => {
-
-  useEffect(() => {
-    if (typeof document !== 'undefined') {
-      const style = document.documentElement.style;
-      const computedStyle = getComputedStyle(document.documentElement);
-      ToggleTheme(computedStyle, style);
-    }
-  }, [noticeClosed]);
-
+export const CollectiviteSearch = () => {
   return (
     <div className={styles.collectiviteWrapper}  >
       <h2 className="text-center text-[1.5rem] font-bold">
