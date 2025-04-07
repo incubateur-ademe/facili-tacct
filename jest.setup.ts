717b3a891c3f938a341b3d0d6bcb4b8ebc22c761
AntoineConegero <antoine.conegero@gmail.com>
2025-04-07 17:24:10 +0200

    test: unit test for queries

---
+++ jest.setup.ts
@@ -0,0 +1,5 @@
+// Jest setup file
+// Add any global setup for Jest here, such as extending matchers or mocking global objects.
+
+// Example: Extend Jest with additional matchers
+import '@testing-library/jest-dom';
