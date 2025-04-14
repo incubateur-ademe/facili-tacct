82bbd2b3380d6170974a6bfc963506c85dc55caf
Antoine <antoine.conegero@gmail.com>
2025-04-14 17:51:04 +0200

    config: migrate next 15.3 + dsfr 1.22.4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,9 @@
 import './global.css';
 
-import { DsfrHead } from '@codegouvfr/react-dsfr/next-appdir/DsfrHead';
-import { DsfrProvider } from '@codegouvfr/react-dsfr/next-appdir/DsfrProvider';
-import { getHtmlAttributes } from '@codegouvfr/react-dsfr/next-appdir/getHtmlAttributes';
+import { DsfrProvider, StartDsfrOnHydration } from '@/dsfr';
+import { DsfrHead, getHtmlAttributes } from '@/dsfr/server-only-index';
 import { cx } from '@codegouvfr/react-dsfr/tools/cx';
-import Link from 'next/link';
 import { type PropsWithChildren } from 'react';
-import { defaultColorScheme } from '../defaultColorScheme';
-import { StartDsfr } from '../StartDsfr';
 import { PHProvider } from './providers';
 import styles from './root.module.scss';
 
@@ -15,14 +11,12 @@ const RootLayout = ({ children }: PropsWithChildren) => {
   return (
     <html
       lang="fr"
-      {...getHtmlAttributes({ defaultColorScheme, lang: 'fr' })}
+      {...getHtmlAttributes({ lang: 'fr' })}
       className={cx(styles.app)}
       title=""
     >
       <head>
-        <StartDsfr />
         <DsfrHead
-          Link={Link}
           preloadFonts={[
             'Marianne-Regular',
             'Spectral-Regular',
@@ -32,7 +26,10 @@ const RootLayout = ({ children }: PropsWithChildren) => {
       </head>
       <PHProvider>
         <body>
-          <DsfrProvider lang="fr">{children}</DsfrProvider>
+          <DsfrProvider lang="fr">
+            <StartDsfrOnHydration />
+            {children}
+          </DsfrProvider>
         </body>
       </PHProvider>
     </html>
