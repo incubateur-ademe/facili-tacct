432c7731178ed1b573336d76df9f23316d781a3e
Antoine <antoine.conegero@gmail.com>
2025-04-15 16:23:20 +0200

    deploy: fix sentry

--- src/app/iframe/layout.tsx
+++ src/app/iframe/layout.tsx
@@ -1,9 +1,9 @@
+import { ClientOnly } from '@/components/utils/ClientOnly';
 import '@iframe-resizer/child';
-import '../global.css';
-
 import dynamic from 'next/dynamic';
 import { Suspense, type PropsWithChildren } from 'react';
 import { NextAppDirEmotionCacheProvider } from 'tss-react/next/appDir';
+import '../global.css';
 
 const PostHogPageView = dynamic(() => import('../PostHogPageView'));
 const RootLayoutIframe = ({ children }: PropsWithChildren) => {
@@ -12,7 +12,9 @@ const RootLayoutIframe = ({ children }: PropsWithChildren) => {
       <Suspense>
         <PostHogPageView />
       </Suspense>
+      <ClientOnly>
       <main>{children}</main>
+      </ClientOnly>
     </NextAppDirEmotionCacheProvider>
   );
 };
