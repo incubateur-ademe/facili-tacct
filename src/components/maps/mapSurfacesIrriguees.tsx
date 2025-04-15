ca3ec35248609950bdde49c42cc2d616b879bf71
Antoine <antoine.conegero@gmail.com>
2025-04-15 09:34:17 +0200

    fix: recette mobile FTT-461

--- src/components/maps/mapSurfacesIrriguees.tsx
+++ src/components/maps/mapSurfacesIrriguees.tsx
@@ -44,7 +44,7 @@ const getColor = (d: number) => {
           ? '#03508B'
           : d > 60 && d <= 100
             ? '#093454'
-            : 'transparent';
+            : 'white';
 };
 
 export const MapSurfacesIrriguees = (props: {
