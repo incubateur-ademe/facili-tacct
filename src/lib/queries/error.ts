f0169e73de0f630edaa363015e35b0b5528560ff
AntoineConegero <antoine.conegero@gmail.com>
2025-04-16 15:28:57 +0200

    build: env check to pass build

--- src/lib/queries/error.ts
+++ src/lib/queries/error.ts
@@ -5,11 +5,15 @@ import { PrismaClient as PostgresClient } from '../../generated/client';
 const PrismaPostgres = new PostgresClient();
 
 export const GetError = async () => {
-  try {
-    await PrismaPostgres.$queryRaw`SELECT * FROM non_existent_table`;
-  } catch (error) {
-    console.error('Forced error for testing:', error);
-    await PrismaPostgres.$disconnect();
-    process.exit(1);
+  if (process.env.NODE_ENV !== 'production') {
+    try {
+      await PrismaPostgres.$queryRaw`SELECT * FROM non_existent_table`;
+    } catch (error) {
+      console.error('Forced error for testing:', error);
+      await PrismaPostgres.$disconnect();
+      process.exit(1);
+    }
+  } else {
+    console.warn('Skipping error test query in production environment.');
   }
 };
