d5410b1b5529f995c826aa60816ce6264e7a16e0
AntoineConegero <antoine.conegero@gmail.com>
2025-04-16 12:27:35 +0200

    fix: recette FTT-459 7

--- src/lib/utils/reusableFunctions/focus.ts
+++ src/lib/utils/reusableFunctions/focus.ts
@@ -1,8 +1,9 @@
 export const FocusOnElement = (id: string) => {
+  console.log('typeof document', typeof document);
   if (typeof document !== 'undefined') {
     const inputElement = document.getElementById(id);
     if (inputElement) {
       inputElement.focus();
     }
   }
-}
+};
