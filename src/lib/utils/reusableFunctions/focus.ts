4fefcf56e9b3a9da2035e6e481c807c2ad5d9efa
Antoine <antoine.conegero@gmail.com>
2025-04-17 08:57:35 +0200

    config: new env

--- src/lib/utils/reusableFunctions/focus.ts
+++ src/lib/utils/reusableFunctions/focus.ts
@@ -1,5 +1,4 @@
 export const FocusOnElement = (id: string) => {
-  console.log('typeof document', typeof document);
   if (typeof document !== 'undefined') {
     const inputElement = document.getElementById(id);
     if (inputElement) {
