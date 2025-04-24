f38575f6f799ecaacbdf842bc15529fb0ecd8b8a
Antoine <antoine.conegero@gmail.com>
2025-04-24 17:40:43 +0200

    feat: rm notice + new loading home page

--- src/components/utils/ClientOnly.tsx
+++ src/components/utils/ClientOnly.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import HeaderImage from "@/assets/images/header.svg";
+import Image from 'next/image';
 import { type SuspenseProps, useEffect, useState } from 'react';
 
 export const ClientOnly = ({ children, fallback }: SuspenseProps) => {
@@ -20,3 +22,23 @@ export function useHasMounted() {
   }, []);
   return hasMounted;
 }
+
+export const HeaderClientOnly = ({ children, fallback }: SuspenseProps) => {
+  const [hasMounted, setHasMounted] = useState(false);
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+  if (!hasMounted) {
+    return (
+      <div className="bg-white w-full flex justify-center">
+        <Image
+          src={HeaderImage}
+          alt="header"
+          width={1232}
+          height={116.5}
+        />
+      </div>
+    );
+  }
+  return <>{children}</>;
+};
