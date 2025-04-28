f9f3dabe4acd46e4bb36722223735b4aec33cec8
Antoine <antoine.conegero@gmail.com>
2025-04-28 18:21:49 +0200

    deploy: secnum server + new dbs

--- src/app/(main)/donnees-territoriales/thematiques/ressourcesEau.tsx
+++ src/app/(main)/donnees-territoriales/thematiques/ressourcesEau.tsx
@@ -1,4 +1,4 @@
-import { GetRessourceEauNew } from '@/lib/queries/databases/ressourcesEau';
+import { GetRessourceEau } from '@/lib/queries/databases/ressourcesEau';
 import { GetCommunes } from '@/lib/queries/postgis/cartographie';
 import { themes } from '@/lib/themes';
 import { Suspense } from 'react';
@@ -8,7 +8,7 @@ import RessourcesEauComp from './ressourcesEauComp';
 const RessourcesEau = async (props: { searchParams: SearchParams }) => {
   const theme = themes.ressourcesEau;
   const { code, libelle, type } = await props.searchParams;
-  const dbRessourcesEau = await GetRessourceEauNew(code, libelle, type);
+  const dbRessourcesEau = await GetRessourceEau(code, libelle, type);
   const carteCommunes = await GetCommunes(code, libelle, type);
 
   return (
--- src/app/(main)/shared-metadata.ts
+++ src/app/(main)/shared-metadata.ts
@@ -1,6 +1,6 @@
 import { type Metadata } from 'next';
 
-const description = 'Facili-TACCT';
+const description = 'Facili-TACCT est un outil d’aide à la décision pour les collectivités territoriales. Il permet de mieux comprendre les enjeux liés au changement climatique et d’identifier des solutions adaptées.';
 
 export const sharedMetadata: Metadata = {
   description,
