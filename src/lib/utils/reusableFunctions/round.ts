76d0eae2ade5f929d94c71c6d730382e9b93164f
Antoine <antoine.conegero@gmail.com>
2025-04-29 13:38:58 +0200

    feat: point to comma for numbers

--- src/lib/utils/reusableFunctions/round.ts
+++ src/lib/utils/reusableFunctions/round.ts
@@ -1,4 +1,7 @@
+import { numberWithSpacesRegex } from "../regex";
+
 export const Round = (value: number, precision: number) => {
   var multiplier = Math.pow(10, precision || 0);
-  return Math.round(value * multiplier) / multiplier;
+  var result = numberWithSpacesRegex(Math.round(value * multiplier) / multiplier);
+  return result
 };
