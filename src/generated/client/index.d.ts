041a642048f7624c2788be5456b32f9bee0b634c
AntoineConegero <antoine.conegero@gmail.com>
2025-04-30 17:06:46 +0200

    feat: fix unique fields prisma

--- src/generated/client/index.d.ts
+++ src/generated/client/index.d.ts
@@ -27150,16 +27150,16 @@ export namespace Prisma {
 
   export type usersWhereUniqueInput = Prisma.AtLeast<{
     pk?: number
+    email?: string
+    username?: string
     AND?: usersWhereInput | usersWhereInput[]
     OR?: usersWhereInput[]
     NOT?: usersWhereInput | usersWhereInput[]
-    email?: StringFilter<"users"> | string
-    username?: StringFilter<"users"> | string
     password?: StringFilter<"users"> | string
     created_at?: DateTimeFilter<"users"> | Date | string
     last_connection?: DateTimeNullableFilter<"users"> | Date | string | null
     role?: StringFilter<"users"> | string
-  }, "pk">
+  }, "pk" | "email" | "username">
 
   export type usersOrderByWithAggregationInput = {
     pk?: SortOrder
@@ -30006,7 +30006,6 @@ export namespace Prisma {
   }
 
   export type usersCreateInput = {
-    pk: number
     email: string
     username: string
     password: string
@@ -30016,7 +30015,7 @@ export namespace Prisma {
   }
 
   export type usersUncheckedCreateInput = {
-    pk: number
+    pk?: number
     email: string
     username: string
     password: string
@@ -30026,7 +30025,6 @@ export namespace Prisma {
   }
 
   export type usersUpdateInput = {
-    pk?: IntFieldUpdateOperationsInput | number
     email?: StringFieldUpdateOperationsInput | string
     username?: StringFieldUpdateOperationsInput | string
     password?: StringFieldUpdateOperationsInput | string
@@ -30046,7 +30044,7 @@ export namespace Prisma {
   }
 
   export type usersCreateManyInput = {
-    pk: number
+    pk?: number
     email: string
     username: string
     password: string
@@ -30056,7 +30054,6 @@ export namespace Prisma {
   }
 
   export type usersUpdateManyMutationInput = {
-    pk?: IntFieldUpdateOperationsInput | number
     email?: StringFieldUpdateOperationsInput | string
     username?: StringFieldUpdateOperationsInput | string
     password?: StringFieldUpdateOperationsInput | string
