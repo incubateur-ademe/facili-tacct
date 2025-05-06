dce4a3dee56f1e831953e9fe4950cc1cd8c9f925
Antoine <antoine.conegero@gmail.com>
2025-05-06 11:48:29 +0200

    feat: race all queries + image metadata

--- src/lib/queries/searchBar.ts
+++ src/lib/queries/searchBar.ts
@@ -8,56 +8,77 @@ export const GetCollectivite = async (
   typeTerritoire: string | undefined,
   collectivite: string
 ): Promise<CollectivitesSearchbar[]> => {
-  try {
-    console.time(`Query Execution Time ${typeTerritoire} ${collectivite}`);
-    if (typeTerritoire === 'pnr') {
-      const variableCollectivite = '%' + collectivite + '%';
-      const value = await PNR(variableCollectivite);
-      console.timeEnd(`Query Execution Time ${typeTerritoire} ${collectivite}`);
-      return value;
-    } else if (typeTerritoire === 'petr') {
-      const variableCollectivite = '%' + collectivite + '%';
-      const value = await PETR(variableCollectivite);
-      console.timeEnd(`Query Execution Time ${typeTerritoire} ${collectivite}`);
-      return value;
-    } else if (typeTerritoire === 'epci') {
-      const variableCollectivite = '%' + collectivite + '%';
-      const value = await EPCI(variableCollectivite);
-      console.timeEnd(`Query Execution Time ${typeTerritoire} ${collectivite}`);
-      return value;
-    } else if (typeTerritoire === 'commune') {
-      const variableCollectivite = collectivite + '%';
-      const value = await Commune(variableCollectivite);
-      console.timeEnd(`Query Execution Time ${typeTerritoire} ${collectivite}`);
-      return value;
-    } else if (typeTerritoire === 'departement') {
-      const variableCollectivite = collectivite + '%';
-      const value = await Departement(variableCollectivite);
-      console.timeEnd(`Query Execution Time ${typeTerritoire} ${collectivite}`);
-      return value;
-    } else {
-      return [
-        {
-          code_geographique: '',
-          search_code: '',
-          search_libelle: '',
-          epci: '',
-          libelle_geographique: '',
-          libelle_epci: '',
-          departement: '',
-          region: '',
-          libelle_departement: '',
-          ept: '',
-          code_pnr: '',
-          libelle_pnr: '',
-          libelle_petr: '',
-          coordinates: '',
-        }
-      ];
+  const timeoutPromise = new Promise<[]>((resolve) =>
+    setTimeout(() => {
+      console.log(
+        'GetSearchBar: Timeout reached (3 seconds), returning empty array.'
+      );
+      resolve([]);
+    }, 3000)
+  );
+  const dbQuery = (async () => {
+    try {
+      console.time(`Query Execution Time ${typeTerritoire} ${collectivite}`);
+      if (typeTerritoire === 'pnr') {
+        const variableCollectivite = '%' + collectivite + '%';
+        const value = await PNR(variableCollectivite);
+        console.timeEnd(
+          `Query Execution Time ${typeTerritoire} ${collectivite}`
+        );
+        return value;
+      } else if (typeTerritoire === 'petr') {
+        const variableCollectivite = '%' + collectivite + '%';
+        const value = await PETR(variableCollectivite);
+        console.timeEnd(
+          `Query Execution Time ${typeTerritoire} ${collectivite}`
+        );
+        return value;
+      } else if (typeTerritoire === 'epci') {
+        const variableCollectivite = '%' + collectivite + '%';
+        const value = await EPCI(variableCollectivite);
+        console.timeEnd(
+          `Query Execution Time ${typeTerritoire} ${collectivite}`
+        );
+        return value;
+      } else if (typeTerritoire === 'commune') {
+        const variableCollectivite = collectivite + '%';
+        const value = await Commune(variableCollectivite);
+        console.timeEnd(
+          `Query Execution Time ${typeTerritoire} ${collectivite}`
+        );
+        return value;
+      } else if (typeTerritoire === 'departement') {
+        const variableCollectivite = collectivite + '%';
+        const value = await Departement(variableCollectivite);
+        console.timeEnd(
+          `Query Execution Time ${typeTerritoire} ${collectivite}`
+        );
+        return value;
+      } else {
+        return [
+          {
+            code_geographique: '',
+            search_code: '',
+            search_libelle: '',
+            epci: '',
+            libelle_geographique: '',
+            libelle_epci: '',
+            departement: '',
+            region: '',
+            libelle_departement: '',
+            ept: '',
+            code_pnr: '',
+            libelle_pnr: '',
+            libelle_petr: '',
+            coordinates: ''
+          }
+        ];
+      }
+    } catch (error) {
+      console.error(error);
+      Sentry.captureException(error);
+      return [];
     }
-  } catch (error) {
-    console.error(error);
-    Sentry.captureException(error);
-    return [];
-  }
+  })();
+  return Promise.race([dbQuery, timeoutPromise]);
 };
