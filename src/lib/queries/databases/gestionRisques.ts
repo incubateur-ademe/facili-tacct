34d2b1969c6f45623e345923dacd47186a10b4ba
Antoine <antoine.conegero@gmail.com>
2025-05-13 17:01:23 +0200

    feat: db rga

--- src/lib/queries/databases/gestionRisques.ts
+++ src/lib/queries/databases/gestionRisques.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { ArreteCatNat, IncendiesForet } from '@/lib/postgres/models';
+import { ArreteCatNat, IncendiesForet, RGA } from '@/lib/postgres/models';
 import { eptRegex } from '@/lib/utils/regex';
 import * as Sentry from '@sentry/nextjs';
 import { PrismaPostgres } from '../db';
@@ -94,6 +94,59 @@ export const GetIncendiesForet = async (
     } catch (error) {
       console.error(error);
       // PrismaPostgres.$disconnect();
+      Sentry.captureException(error);
+      return [];
+    }
+  })();
+  return Promise.race([dbQuery, timeoutPromise]);
+};
+
+export const GetRGA = async (
+  code: string,
+  libelle: string,
+  type: string
+): Promise<RGA[]> => {
+  const column =
+    type === 'pnr'
+      ? 'code_pnr'
+      : type === 'petr'
+        ? 'libelle_petr'
+        : type === 'ept' && eptRegex.test(libelle)
+          ? 'ept'
+          : type === 'epci' && !eptRegex.test(libelle)
+            ? 'epci'
+            : type === 'departement'
+              ? 'departement'
+              : type === 'commune'
+                ? 'code_geographique'
+                : '';
+  const timeoutPromise = new Promise<[]>((resolve) =>
+    setTimeout(() => {
+      resolve([]);
+    }, 3000)
+  );
+  const dbQuery = (async () => {
+    try {
+      console.time('Query Execution Time RGA');
+      if (type === 'petr' || type === 'ept') {
+        const value = await PrismaPostgres.rga.findMany({
+          where: {
+            [column]: libelle
+          }
+        });
+        console.timeEnd('Query Execution Time RGA');
+        return value;
+      } else {
+        const value = await PrismaPostgres.rga.findMany({
+          where: {
+            [column]: code
+          }
+        });
+        console.timeEnd('Query Execution Time RGA');
+        return value;
+      }
+    } catch (error) {
+      console.error(error);
       Sentry.captureException(error);
       return [];
     }
