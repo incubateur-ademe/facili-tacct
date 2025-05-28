589d8b1a29eaeb973dd4d77f6c94d7e12844f7cb
Antoine <antoine.conegero@gmail.com>
2025-05-28 16:43:38 +0200

    feat: rga carte facile

--- src/lib/queries/postgis/rga.ts
+++ src/lib/queries/postgis/rga.ts
@@ -22,28 +22,99 @@ export const GetRGACarte = async (
       if (type === 'commune') {
         const commune = await prisma.$queryRaw<RGACarte[]>`
           SELECT
-          code_geographique,
-          alea,
-          ST_AsGeoJSON(geometry) geometry
-          FROM postgis."rga" 
-          WHERE code_geographique=${code} LIMIT 1;`;
+            code_geographique,
+            alea,
+            ST_AsGeoJSON(ST_Union(geometry)) as geometry
+          FROM postgis."rga"
+          WHERE code_geographique = ANY (
+            SELECT code_geographique
+            FROM collectivites_searchbar
+            WHERE epci = (
+              SELECT epci
+              FROM collectivites_searchbar
+              WHERE code_geographique = ${code}
+              LIMIT 1
+            )
+          )
+          GROUP BY code_geographique, alea;
+        `;
         return commune;
+
       } else if (type === 'epci' && !eptRegex.test(libelle)) {
-        const communes = await prisma.collectivites_searchbar.findMany({
-          where: {
-            epci: code
-          }
-        });
         const epci = await prisma.$queryRaw<RGACarte[]>`
           SELECT
-          code_geographique,
-          alea,
-          ST_AsGeoJSON(ST_Union(geometry)) as geometry
-          FROM postgis."rga" 
-          WHERE code_geographique = ANY(${communes.map(c => c.code_geographique)})
-          GROUP BY code_geographique, alea;`;
+            code_geographique,
+            alea,
+            ST_AsGeoJSON(ST_Union(geometry)) as geometry
+          FROM postgis."rga"
+          WHERE code_geographique = ANY (
+            SELECT code_geographique
+            FROM collectivites_searchbar
+            WHERE epci = ${code}
+          )
+          GROUP BY code_geographique, alea;
+        `;
         return epci;
-
+      } else if (type === 'pnr') {
+        const pnr = await prisma.$queryRaw<RGACarte[]>`
+          SELECT
+            code_geographique,
+            alea,
+            ST_AsGeoJSON(ST_Union(geometry)) as geometry
+          FROM postgis."rga"
+          WHERE code_geographique = ANY (
+            SELECT code_geographique
+            FROM collectivites_searchbar
+            WHERE code_pnr = ${code}
+          )
+          GROUP BY code_geographique, alea;
+        `;
+        return pnr;
+      } else if (type === "petr") {
+        const petr = await prisma.$queryRaw<RGACarte[]>`
+          SELECT
+            code_geographique,
+            alea,
+            ST_AsGeoJSON(ST_Union(geometry)) as geometry
+          FROM postgis."rga"
+          WHERE code_geographique = ANY (
+            SELECT code_geographique
+            FROM collectivites_searchbar
+            WHERE libelle_petr = ${libelle}
+          )
+          GROUP BY code_geographique, alea;
+        `;
+        return petr;
+      } else if (type === "departement") {
+        const departement = await prisma.$queryRaw<RGACarte[]>`
+          SELECT
+            code_geographique,
+            alea,
+            ST_AsGeoJSON(ST_Union(geometry)) as geometry
+          FROM postgis."rga"
+          WHERE code_geographique = ANY (
+            SELECT code_geographique
+            FROM collectivites_searchbar
+            WHERE departement = ${code}
+          )
+          GROUP BY code_geographique, alea;
+        `;
+        return departement;
+      } else if (type === "ept") {
+        const ept = await prisma.$queryRaw<RGACarte[]>`
+          SELECT
+            code_geographique,
+            alea,
+            ST_AsGeoJSON(ST_Union(geometry)) as geometry
+          FROM postgis."rga"
+          WHERE code_geographique = ANY (
+            SELECT code_geographique
+            FROM collectivites_searchbar
+            WHERE ept = ${code}
+          )
+          GROUP BY code_geographique, alea;
+        `;
+        return ept;
       } else return [];
     } catch (error) {
       console.error(error);
