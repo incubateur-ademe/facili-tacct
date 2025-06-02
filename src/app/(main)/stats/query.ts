da188d8e9d3f0ee4b13bc79016d35d764d123eb0
AntoineConegero <antoine.conegero@gmail.com>
2025-06-02 11:23:49 +0200

    fix: query stats

--- src/app/(main)/stats/query.ts
+++ src/app/(main)/stats/query.ts
@@ -19,26 +19,32 @@ interface Response {
 }
 
 const GetInsights = async (shortId?: string) => {
-  const url = `https://eu.posthog.com/api/projects/39308/insights?refresh=lazy_async`;
-  const request = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.POSTHOG_API_KEY}`
-    },
-    cache: 'no-store'
-  });
+  try {
+    const url = `https://eu.posthog.com/api/projects/39308/insights?refresh=lazy_async`;
+    const request = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.POSTHOG_API_KEY}`
+      },
+      cache: 'no-store'
+    });
 
-  if (!request.ok) {
-    throw new Error('Failed to fetch data');
+    if (!request.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const response: Response = await request.json();
+    const filteredResponse = response.results.filter(
+      (e) => e.short_id === shortId
+    )[0]?.result;
+
+    return filteredResponse;
+  } catch (error) {
+    // Optionally log the error for debugging
+    console.error('GetInsights error:', error);
+    return null;
   }
-
-  const response: Response = await request.json();
-  const filteredResponse = response.results.filter(
-    (e) => e.short_id === shortId
-  )[0].result;
-
-  return filteredResponse;
 };
 
 export default GetInsights;
