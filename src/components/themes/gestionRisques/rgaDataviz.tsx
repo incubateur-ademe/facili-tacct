af7af48170087421320101dd0c6ffb84e85a660a
Antoine <antoine.conegero@gmail.com>
2025-06-05 12:32:02 +0200

    feat: repartition rga

--- src/components/themes/gestionRisques/rgaDataviz.tsx
+++ src/components/themes/gestionRisques/rgaDataviz.tsx
@@ -1,9 +1,15 @@
 import { NewNivoBarChart } from '@/components/charts/NivoBarChart';
+import { RgaEvolutionLegend, RgaMapLegend, RgaRepartitionLegend } from '@/components/maps/legends/datavizLegends';
+import { LegendCompColor } from '@/components/maps/legends/legendComp';
 import SubTabs from '@/components/SubTabs';
 import { CommunesIndicateursDto, RGADto } from '@/lib/dto';
 import { RGAdb } from '@/lib/postgres/models';
+import { Average } from '@/lib/utils/reusableFunctions/average';
+import { CapitalizeFirstLetter } from '@/lib/utils/reusableFunctions/capitalize';
+import { Round } from '@/lib/utils/reusableFunctions/round';
+import { BarDatum, BarTooltipProps } from '@nivo/bar';
 import { useSearchParams } from 'next/navigation';
-import CarteFacileTemplate from './CarteFacile';
+import RGAMap from '../../maps/mapRGA';
 import styles from './gestionRisques.module.scss';
 
 type Props = {
@@ -17,7 +23,7 @@ type Props = {
   setDatavizTab: (value: string) => void;
 };
 
-const getBarChartData = (rga: RGAdb[]) => {
+const barChartEvolution = (rga: RGAdb[]) => {
   const avant1975 = {
     nb_logement_alea_faible:
       rga.reduce(
@@ -54,19 +60,30 @@ const getBarChartData = (rga: RGAdb[]) => {
 
   return [avant1975, apres1975];
 }
-
-const legends = [
-  {
-    variable: "nb_logement_alea_faible",
-    texte_raccourci: "Aléas faibles",
-    couleur: "#FFCF5E"
-  },
-  {
-    variable: "nb_logement_alea_moyen_fort",
-    texte_raccourci: "Aléas moyens / forts",
-    couleur: "#E8323B"
-  },
-];
+const barChartRepartition = (rga: RGAdb[], code: string) => {
+  const communeAlea = rga.find(item => item.code_geographique === code);
+  const sansAlea = {
+    commune:
+      100 -
+      (communeAlea?.part_alea_moyen_fort_commune ?? 0) -
+      (communeAlea?.part_alea_faible_commune ?? 0),
+    epci: 100 -
+      Average(rga.map(el => el.part_alea_moyen_fort_commune)) -
+      Average(rga.map(el => el.part_alea_faible_commune)),
+    alea: "Sans aléa",
+  };
+  const aleaFaible = {
+    commune: communeAlea?.part_alea_faible_commune,
+    epci: Average(rga.map(el => el.part_alea_faible_commune)),
+    alea: "Aléas faibles",
+  };
+  const aleaMoyenFort = {
+    commune: communeAlea?.part_alea_moyen_fort_commune,
+    epci: Average(rga.map(el => el.part_alea_moyen_fort_commune)),
+    alea: "Aléas moyens / forts",
+  };
+  return [sansAlea, aleaFaible, aleaMoyenFort];
+}
 
 const RgaDataViz = (props: Props) => {
   const {
@@ -78,46 +95,94 @@ const RgaDataViz = (props: Props) => {
   } = props;
   const searchParams = useSearchParams();
   const type = searchParams.get('type')!;
+  const code = searchParams.get('code')!;
+  const evolutionRga = barChartEvolution(rga);
+  const repartitionRga = barChartRepartition(rga, code);
 
-  console.log("rga", rga)
-
-  const evolutionRga = getBarChartData(rga);
-  console.log("evolutionRga", evolutionRga);
+  const CustomTooltip = ({ data }: BarTooltipProps<BarDatum>) => {
+    const dataArray = Object.entries(data).map(el => {
+      return {
+        titre: CapitalizeFirstLetter(el[0]),
+        value: el[1],
+        color: RgaRepartitionLegend.find(e => e.variable === el[0])?.couleur
+      }
+    });
+    return (
+      <div className={styles.tooltipEvolutionWrapper}>
+        <h3>{dataArray.at(-1)?.value}</h3>
+        {
+          dataArray.slice(0, -1).map((el, i) => {
+            return (
+              <div className={styles.itemWrapper} key={i}>
+                <div className={styles.titre}>
+                  <div className={styles.colorSquare} style={{ background: el.color }} />
+                  <p>{el.titre}</p>
+                </div>
+                <div className={styles.value}>
+                  <p>{Round(Number(el.value), 1)} %</p>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    );
+  }
 
   return (
     <div className={styles.graphWrapper}>
       <div className={styles.catnatGraphTitleWrapper}>
         <h2>Retrait-gonflement des argiles</h2>
         <SubTabs
-          data={['Répartition', 'Évolution', 'Cartographie']}
+          data={type === "commune" ? ['Répartition', 'Évolution', 'Cartographie'] : ['Évolution', 'Cartographie']}
           defaultTab={datavizTab}
           setValue={setDatavizTab}
         />
       </div>
       {datavizTab === 'Répartition' ? (
-        <div>Répartition
-          <p>
-            comment faire la comparaison des territoires ? département ? epci ?
-          </p>
+        <div style={{ height: "500px", minWidth: "450px", backgroundColor: "white" }}>
+          <NewNivoBarChart
+            colors={RgaRepartitionLegend.map(e => e.couleur)}
+            graphData={repartitionRga as BarDatum[]}
+            keys={RgaRepartitionLegend.map(e => e.variable)}
+            indexBy="alea"
+            legendData={RgaRepartitionLegend
+              .map((legend, index) => ({
+                id: index,
+                label: legend.texteRaccourci,
+                color: legend.couleur,
+              }))}
+            axisLeftLegend="Part des logements (%)"
+            groupMode="grouped"
+            tooltip={CustomTooltip}
+          />
         </div>
       ) : datavizTab === 'Évolution' ? (
         <div style={{ height: "500px", minWidth: "450px", backgroundColor: "white" }}>
           <NewNivoBarChart
-            colors={legends.map(e => e.couleur)}
+            colors={RgaEvolutionLegend.map(e => e.couleur)}
             graphData={evolutionRga}
-            keys={legends.map(e => e.variable)}
+            keys={RgaEvolutionLegend.map(e => e.variable)}
             indexBy="annee"
-            legendData={legends
+            legendData={RgaEvolutionLegend
               .map((legend, index) => ({
                 id: index,
-                label: legend.texte_raccourci,
+                label: legend.texteRaccourci,
                 color: legend.couleur,
               }))}
             axisLeftLegend="Nombre de logements"
           />
         </div>
       ) : datavizTab === 'Cartographie' ? (
-        <CarteFacileTemplate rgaCarte={rgaCarte} carteCommunes={carteCommunes} />
+        <>
+          <RGAMap rgaCarte={rgaCarte} carteCommunes={carteCommunes} />
+          <div
+            className={styles.legend}
+            style={{ width: 'auto', justifyContent: 'center' }}
+          >
+            <LegendCompColor legends={RgaMapLegend} />
+          </div>
+        </>
       ) : (
         ''
       )}
