2d668f7ee8b709975cfd35a47e6b2b79d336ae55
Antoine <antoine.conegero@gmail.com>
2025-07-09 11:00:24 +0200

    feat: new home page

--- src/app/(main)/CollectiviteSearch.tsx
+++ src/app/(main)/CollectiviteSearch.tsx
@@ -4,8 +4,8 @@ import styles from '../root.module.scss';
 const CollectiviteSearch = () => {
   return (
     <div className={styles.collectiviteWrapper} >
-      <h2 className="text-center text-[1.5rem] font-bold">
-        Sélectionnez votre territoire
+      <h2 className="text-center text-[1rem] font-weight-700">
+        Quel territoire représentez-vous ?
       </h2>
       <SearchBarComp />
     </div>
