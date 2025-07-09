c3400988e52ef9c7c8a5b5b118bf94cfed9dd694
Antoine <antoine.conegero@gmail.com>
2025-07-09 16:27:53 +0200

    feat: responsive home page

---
+++ src/app/(main)/home/homeButton.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@mui/material";
+
+export const HomeButton = ({
+  borderColor,
+  backgroundColor,
+  textColor,
+  link,
+  text,
+}: {
+  borderColor: string;
+  backgroundColor: string;
+  textColor: string;
+  link: string;
+  text: string;
+}) => {
+  return (
+    <Button
+      key="0"
+      variant="outlined"
+      href={link}
+      sx={{
+        textTransform: 'none',
+        color: textColor,
+        backgroundColor: backgroundColor,
+        borderRadius: '60px',
+        border: `1px solid ${borderColor}`,
+        padding: '0.5em 1em',
+        fontWeight: 500,
+        fontFamily: 'inherit',
+        fontSize: '18px',
+        margin: '2rem 0 0',
+        backgroundImage: 'none',
+        '&:hover': {
+          color: `${backgroundColor} !important`,
+          backgroundColor: `${textColor} !important`,
+        }
+      }}
+    >
+      {text}
+    </Button>
+  );
+}
