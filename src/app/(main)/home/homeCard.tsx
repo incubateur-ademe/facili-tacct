37ed627573e5d277e1dc7ea1434cfd4c4e41383d
Antoine <antoine.conegero@gmail.com>
2025-07-09 11:00:24 +0200

    feat: new home page

---
+++ src/app/(main)/home/homeCard.tsx
@@ -0,0 +1,26 @@
+import Image, { StaticImageData } from 'next/image';
+import styles from '../../root.module.scss';
+
+type HomeCardProps = {
+  icone: StaticImageData;
+  titre: string;
+  description: string;
+}
+
+export const HomeCard = ({
+  icone,
+  titre,
+  description
+}: HomeCardProps) => {
+  return (
+    <div className={styles.homeCard}>
+      <Image
+        src={icone}
+        alt="image-cartographie"
+        className={styles.homeCardImage}
+      />
+      <h4>{titre}</h4>
+      <p>{description}</p>
+    </div>
+  )
+}
