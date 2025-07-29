16364f088b6839f0cd89457d894084c2e821e758
AntoineConegero <antoine.conegero@gmail.com>
2025-07-29 15:19:53 +0200

    feat: carte facile erosion cotiere

--- src/components/themes/amenagement/consommationEspacesNAF.tsx
+++ src/components/themes/amenagement/consommationEspacesNAF.tsx
@@ -1,3 +1,4 @@
+"use client";
 import fortesChaleursIcon from '@/assets/icons/chaleur_icon_black.svg';
 import precipitationIcon from '@/assets/icons/precipitation_icon_black.svg';
 import { Loader } from '@/components/loader';
