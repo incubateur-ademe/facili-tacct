6a1e450d714ff0a320831566b55f352718591693
AntoineConegero <antoine.conegero@gmail.com>
2025-07-29 20:15:36 +0200

    feat: catnat bat chart

--- src/components/charts/NivoBarChart.tsx
+++ src/components/charts/NivoBarChart.tsx
@@ -35,7 +35,7 @@ type NivoBarChartProps = {
   graphData: BarDatum[];
   keys: string[];
   indexBy: string;
-  bottomTickValues?: string[];
+  bottomTickValues?: (string | number)[];
   colors?: string[];
   legendData?: Datum[];
   tooltip?: ({ data }: BarTooltipProps<BarDatum>) => JSX.Element;
@@ -238,6 +238,155 @@ export const NivoBarChartRessourcesEau = ({
         legendOffset: -50,
         truncateTickAt: 0,
         tickValues: 5, //number of tickvalues displayed along the ax
+        renderTick: (e) => {
+          return (
+            <g transform={`translate(${e.x},${e.y})`}>
+              <text
+                x={-20}
+                y={5}
+                textAnchor="middle"
+                style={{
+                  fill: 'black',
+                  fontSize: 12,
+                  fontWeight: 400
+                }}
+              >
+                {(e.value / axisLeftTickFactor) % 1 != 0
+                  ? ''
+                  : numberWithSpacesRegex(e.value / axisLeftTickFactor)}
+              </text>
+            </g>
+          );
+        }
+      }}
+      enableLabel={false}
+      legends={
+        showLegend && legendData
+          ? legendData.length >= 4
+            ? [
+              // First row - first half of items
+              {
+                ...legendProps,
+                data: legendData.slice(0, Math.ceil(legendData.length / 2)),
+                direction: "row",
+                anchor: "bottom",
+                translateX: 0,
+                translateY: 70,
+                itemsSpacing: 40,
+                itemWidth: 120,
+                itemHeight: 25,
+                symbolSize: 15,
+              },
+              // Second row - second half of items
+              {
+                ...legendProps,
+                data: legendData.slice(Math.ceil(legendData.length / 2)),
+                direction: "row",
+                anchor: "bottom",
+                translateX: 0,
+                translateY: 100,
+                itemsSpacing: 40,
+                itemWidth: 120,
+                itemHeight: 25,
+                symbolSize: 15,
+              }
+            ]
+            : [
+              {
+                ...legendProps,
+                data: legendData,
+                direction: "row",
+                anchor: "bottom",
+                translateX: 0,
+                translateY: 70,
+                itemsSpacing: 50,
+              }
+            ]
+          : []
+      }
+      tooltip={tooltip}
+      role="application"
+    />
+  );
+};
+
+export const NivoBarChartCatnat = ({
+  graphData,
+  keys,
+  indexBy,
+  bottomTickValues,
+  colors,
+  legendData,
+  tooltip,
+  axisLeftLegend,
+  axisBottomLegend,
+  axisLeftTickFactor = 1,
+  groupMode = 'stacked',
+  showLegend = true
+}: NivoBarChartProps) => {
+  return (
+    <ResponsiveBar
+      data={graphData}
+      keys={keys}
+      isFocusable={true}
+      animate={false}
+      motionConfig={"stiff"}
+      indexBy={indexBy}
+      colors={colors}
+      margin={
+        showLegend
+          ? {
+            top: 40,
+            right: 80,
+            bottom: legendData && legendData.length >= 4 ? 120 : 80,
+            left: 80
+          }
+          : { top: 40, right: 80, bottom: 80, left: 80 }
+      }
+      groupMode={groupMode}
+      padding={0.3}
+      innerPadding={2}
+      borderRadius={1}
+      valueScale={{ type: 'linear' }}
+      indexScale={{ type: 'band', round: true }}
+      borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        tickValues: bottomTickValues,
+        tickSize: 0,
+        tickPadding: 15,
+        legend: axisBottomLegend,
+        renderTick: (e: Any) => {
+          return (
+            <g transform={`translate(${e.x},${e.y})`}>
+              <text
+                x={0}
+                y={10}
+                dy={16}
+                textAnchor="middle"
+                style={{
+                  fill: 'black',
+                  fontSize: 12,
+                  fontWeight: 400
+                }}
+              >
+                {e.value}
+              </text>
+            </g>
+          );
+        }
+      }}
+      gridYValues={5}
+      axisLeft={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: axisLeftLegend,
+        legendPosition: 'middle',
+        legendOffset: -50,
+        truncateTickAt: 0,
+        tickValues: 5,
         renderTick: (e) => {
           return (
             <g transform={`translate(${e.x},${e.y})`}>
