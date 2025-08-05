0064b8862c35e843d8cc4de2c65dcbff54ebb298
AntoineConegero <antoine.conegero@gmail.com>
2025-08-05 13:20:25 +0200

    feat: finition patch4 + sandbox celine

---
+++ src/app/(main)/_(nouveau-parcours)/patch4c/cursorVisualization.tsx
@@ -0,0 +1,48 @@
+"use client";
+import InfoIcon from '@/assets/icons/info_round_icon_black.svg';
+import { HtmlTooltip } from '@/components/utils/HtmlTooltip';
+import { Body } from "@/design-system/base/Textes";
+import Image from "next/image";
+import { agravationItems } from '../../../../lib/patch4/constantes';
+import styles from './patch4c.module.scss';
+
+const CursorVisualization = () => {
+  return (
+    <div className="pt-20">
+      <div className={styles.CursorVisualizationBarColor}>
+        {
+          agravationItems.map((item, index) => (
+            <div key={index}>
+              <div className={styles.barColorText} style={{ left: `calc(${index * 25}% + ${item.offset}px)` }}>
+                <div className='flex flex-row items-center'>
+                  <Body size="sm">
+                    {item.label}
+                  </Body>
+                  <HtmlTooltip
+                    title={
+                      <Body>
+                        {item.hover}
+                      </Body>
+                    }
+                    placement="top"
+                  >
+                    <Image
+                      src={InfoIcon}
+                      alt="hover d'information"
+                      width={20}
+                      height={20}
+                      className={styles.infoIcon}
+                    />
+                  </HtmlTooltip>
+                </div>
+              </div>
+              <div className={styles.cursor} style={{ left: item.values }} />
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
+}
+
+export default CursorVisualization;
