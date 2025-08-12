1768089156ff291e6f5209e108ce4e7181cd49d6
AntoineConegero <antoine.conegero@gmail.com>
2025-08-12 11:16:05 +0200

    text: typo documentation

--- src/lib/utils/export/documentations.ts
+++ src/lib/utils/export/documentations.ts
@@ -2,77 +2,77 @@ export const consommationEspacesNafDoc = [
   {
     Variable: 'naf09art10',
     Description:
-      'Flux entre NAF (Natuel, Agricole et Forestier) et artificialisé, sur la période 2009-2010'
+      'Flux entre NAF (Naturel, Agricole et Forestier) et artificialisé, sur la période 2009-2010'
   },
   {
     Variable: 'art09act10',
     Description:
-      "Flux NAF (Natuel, Agricole et Forestier) vers artificialisé destiné à l'activité sur la période 2009-2010"
+      "Flux NAF (Naturel, Agricole et Forestier) vers artificialisé destiné à l'activité sur la période 2009-2010"
   },
   {
     Variable: 'art09hab10',
     Description:
-      "Flux NAF (Natuel, Agricole et Forestier) vers artificialisé destiné à l'habitat sur la période 2009-2010"
+      "Flux NAF (Naturel, Agricole et Forestier) vers artificialisé destiné à l'habitat sur la période 2009-2010"
   },
   {
     Variable: 'art09mix10',
     Description:
-      'Flux NAF (Natuel, Agricole et Forestier) vers artificialisé destiné au mixte sur la période 2009-2010'
+      'Flux NAF (Naturel, Agricole et Forestier) vers artificialisé destiné au mixte sur la période 2009-2010'
   },
   {
     Variable: 'art09rou10',
     Description:
-      'Flux NAF (Natuel, Agricole et Forestier) vers artificialisé destiné aux infrastructures routières sur la période 2009-2010'
+      'Flux NAF (Naturel, Agricole et Forestier) vers artificialisé destiné aux infrastructures routières sur la période 2009-2010'
   },
   {
     Variable: 'art09fer10',
     Description:
-      'Flux NAF (Natuel, Agricole et Forestier) vers artificialisé destiné aux infrastructures ferroviaires sur la période 2009-2010'
+      'Flux NAF (Naturel, Agricole et Forestier) vers artificialisé destiné aux infrastructures ferroviaires sur la période 2009-2010'
   },
   {
     Variable: 'art09inc10',
     Description:
-      'Flux NAF (Natuel, Agricole et Forestier) vers artificialisé dont la destination est inconnue sur la période 2009-2010'
+      'Flux NAF (Naturel, Agricole et Forestier) vers artificialisé dont la destination est inconnue sur la période 2009-2010'
   },
   {
     Variable: 'nafart0923',
     Description:
-      'Total des flux entre NAF (Natuel, Agricole et Forestier) et artificialisé, sur la période 2009-2023'
+      'Total des flux entre NAF (Naturel, Agricole et Forestier) et artificialisé, sur la période 2009-2023'
   },
   {
     Variable: 'nafact0923',
     Description:
-      "Flux NAF (Natuel, Agricole et Forestier) vers artificialisé destiné à l'activité sur la période 2009-2023"
+      "Flux NAF (Naturel, Agricole et Forestier) vers artificialisé destiné à l'activité sur la période 2009-2023"
   },
   {
     Variable: 'artact0923',
     Description:
-      "Flux NAF (Natuel, Agricole et Forestier) vers artificialisé destiné à l'habitat sur la période 2009-2023"
+      "Flux NAF (Naturel, Agricole et Forestier) vers artificialisé destiné à l'habitat sur la période 2009-2023"
   },
   {
     Variable: 'arthab0923',
     Description:
-      'Flux NAF (Natuel, Agricole et Forestier) vers artificialisé destiné au mixte sur la période 2009-2023'
+      'Flux NAF (Naturel, Agricole et Forestier) vers artificialisé destiné au mixte sur la période 2009-2023'
   },
   {
     Variable: 'artmix0923',
     Description:
-      'Flux NAF (Natuel, Agricole et Forestier) vers artificialisé destiné aux infrastructures routières sur la période 2009-2023'
+      'Flux NAF (Naturel, Agricole et Forestier) vers artificialisé destiné aux infrastructures routières sur la période 2009-2023'
   },
   {
     Variable: 'artrou0923',
     Description:
-      'Flux NAF (Natuel, Agricole et Forestier) vers artificialisé destiné aux infrastructures ferroviaires sur la période 2009-2023'
+      'Flux NAF (Naturel, Agricole et Forestier) vers artificialisé destiné aux infrastructures ferroviaires sur la période 2009-2023'
   },
   {
     Variable: 'artfer0923',
     Description:
-      'Flux NAF (Natuel, Agricole et Forestier) vers artificialisé dont la destination est inconnue sur la période 2009-2023'
+      'Flux NAF (Naturel, Agricole et Forestier) vers artificialisé dont la destination est inconnue sur la période 2009-2023'
   },
   {
     Variable: 'artinc0923',
     Description:
-      'Flux NAF (Natuel, Agricole et Forestier) vers artificialisé dont la destination est inconnue sur la période 2009-2023'
+      'Flux NAF (Naturel, Agricole et Forestier) vers artificialisé dont la destination est inconnue sur la période 2009-2023'
   },
   {
     Variable: '',
