5d479329b3aa31d70b3ca771d72594ae1e258b56
Antoine <antoine.conegero@gmail.com>
2025-08-28 13:18:24 +0200

    build: suspense boundary

--- src/app/(main)/(nouveau-parcours)/donnees/layout.tsx
+++ src/app/(main)/(nouveau-parcours)/donnees/layout.tsx
@@ -1,18 +1,20 @@
-import { type PropsWithChildren } from 'react';
+import { Suspense, type PropsWithChildren } from 'react';
 import { MenuLateral } from '../../../../components/MenuLateral';
 
 const ExplorerTerritoireLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="flex min-h-screen">
-      {/* Menu latéral fixe */}
-      <MenuLateral />
-      {/* Contenu principal */}
-      <div className="flex-1 ml-[322px] flex flex-col">
-        <div className="flex-1">
-          {children}
+    <Suspense>
+      <div className="flex min-h-screen">
+        {/* Menu latéral fixe */}
+        <MenuLateral />
+        {/* Contenu principal */}
+        <div className="flex-1 ml-[322px] flex flex-col">
+          <div className="flex-1">
+            {children}
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
