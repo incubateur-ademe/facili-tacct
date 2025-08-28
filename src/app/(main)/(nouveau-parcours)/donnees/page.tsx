862981bf57b0578294bb0543b61a2c86a9aa3769
Antoine <antoine.conegero@gmail.com>
2025-08-28 13:18:24 +0200

    build: suspense boundary

--- src/app/(main)/(nouveau-parcours)/donnees/page.tsx
+++ src/app/(main)/(nouveau-parcours)/donnees/page.tsx
@@ -13,19 +13,19 @@ const ExplorerTerritoirePage = async (props: { searchParams: SearchParams }) => 
       {
         ((code || libelle) && type) ?
           <div>
-            {thematique === 'Confort thermique' ? (
-              <ConfortThermiqueServerPage searchParams={props.searchParams} />
-            ) : thematique === "Biodiversité" ? (
-              <BiodiversiteServerPage searchParams={props.searchParams} />
-            ) : thematique === "Agriculture et pêche" ? (
-              <AgricultureEtPecheServerPage searchParams={props.searchParams} />
-            ) : thematique === "Aménagement" ? (
-              <AmenagementServerPage searchParams={props.searchParams} />
-            ) : thematique === "Eau" ? (
-              <EauServerPage searchParams={props.searchParams} />
-            ) : thematique === "Gestion des risques" ? (
-              <GestionRisquesServerPage searchParams={props.searchParams} />
-            ) : ""}
+              {thematique === 'Confort thermique' ? (
+                <ConfortThermiqueServerPage searchParams={props.searchParams} />
+              ) : thematique === "Biodiversité" ? (
+                <BiodiversiteServerPage searchParams={props.searchParams} />
+              ) : thematique === "Agriculture et pêche" ? (
+                <AgricultureEtPecheServerPage searchParams={props.searchParams} />
+              ) : thematique === "Aménagement" ? (
+                <AmenagementServerPage searchParams={props.searchParams} />
+              ) : thematique === "Eau" ? (
+                <EauServerPage searchParams={props.searchParams} />
+              ) : thematique === "Gestion des risques" ? (
+                <GestionRisquesServerPage searchParams={props.searchParams} />
+              ) : ""}
           </div>
           : <ErrorDisplay code="404" />
       }
