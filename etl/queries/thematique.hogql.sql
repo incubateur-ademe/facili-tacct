6524781559335e89c50423442feee61b4dd4fb29
Antoine <antoine.conegero@gmail.com>
2025-09-22 10:24:35 +0200

    feat: posthog request etl clic thematiques

---
+++ etl/queries/thematique.hogql.sql
@@ -0,0 +1,22 @@
+select properties.$sent_at, properties, distinct_id, `$session_id`, person_id, properties.thematique
+from events
+where
+and(
+    event LIKE '%clic_explore_thematique%',
+    notIn(
+        properties.$host, tuple(
+            'facili-tacct-preprod.osc-fr1.scalingo.io',
+            'facili-tacct.osc-secnum-fr1.scalingo.io',
+            'facili-tacct-nginx.osc-secnum-fr1.scalingo.io',
+            -- 'facili-tacct.incubateur.ademe.dev',
+            'null'
+        )
+    ),
+)
+AND timestamp < now()
+AND properties.$current_url NOT LIKE '%localhost%'
+-- AND properties.$current_url LIKE 'https://facili-tacct.beta.gouv.fr/%'
+AND properties.$current_url NOT LIKE '%scalingo%'
+AND properties.$current_url NOT LIKE '%secnum%'
+order by timestamp desc
+limit 50000
