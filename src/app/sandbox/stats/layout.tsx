fbafa5306e6f83f1803443d81828c4c2089e3bc5
Antoine <antoine.conegero@gmail.com>
2025-09-25 13:51:20 +0200

    build: import fix

--- src/app/sandbox/stats/layout.tsx
+++ src/app/sandbox/stats/layout.tsx
@@ -1,4 +1,4 @@
-import HeaderComp from "@/components/Header";
+import HeaderComp from "@/components/ui/Header";
 import AppFooter from "@/design-system/layout/Footer";
 import { Suspense, type PropsWithChildren } from 'react';
 import { NextAppDirEmotionCacheProvider } from 'tss-react/next/appDir';
