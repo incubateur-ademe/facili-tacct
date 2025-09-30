f7ae1757dc46ce0c0e51c0902b877c7e4eb28d6c
Antoine <antoine.conegero@gmail.com>
2025-09-30 13:40:04 +0200

    feat: route stat api finition

--- src/lib/queries/ecologieGouv/test.ts
+++ src/lib/queries/ecologieGouv/test.ts
@@ -104,94 +104,94 @@ export const GetNAF = async (
 ): Promise<ConsommationNAFEcolabApi[]> => {
   try {
     const url = `https://api.indicateurs.ecologie.gouv.fr/cubejs-api/v1/load`;
-  let listeTerritoires = [];
-  console.time(`Query Execution Time CUBEJS NAF GetCommunes ${type}`);
+    let listeTerritoires = [];
+    console.time(`Query Execution Time CUBEJS NAF GetCommunes ${type}`);
 
-  if (type !== 'epci' && type !== 'departement') {
-    const listeCommunes = await prisma.collectivites_searchbar.findMany({
-      where: {
-        [type === 'petr'
-          ? 'libelle_petr'
-          : type === 'ept'
-            ? 'ept'
-            : type === 'pnr'
-              ? 'code_pnr'
-              : type === 'commune'
-                ? 'code_geographique'
-                : '']: type === 'petr' || type === 'ept' ? libelle : code
-      }
+    if (type !== 'epci' && type !== 'departement') {
+      const listeCommunes = await prisma.collectivites_searchbar.findMany({
+        where: {
+          [type === 'petr'
+            ? 'libelle_petr'
+            : type === 'ept'
+              ? 'ept'
+              : type === 'pnr'
+                ? 'code_pnr'
+                : type === 'commune'
+                  ? 'code_geographique'
+                  : '']: type === 'petr' || type === 'ept' ? libelle : code
+        }
+      });
+      listeTerritoires = listeCommunes
+        .map((el) => el.code_geographique)
+        .filter((code) => code !== null);
+    } else {
+      listeTerritoires = [code];
+    }
+    console.timeEnd(`Query Execution Time CUBEJS NAF GetCommunes ${type}`);
+    console.time('Query Execution Time CUBEJS NAF');
+
+    const request = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${process.env.TEST_ECOLAB_TOKEN}`
+      },
+      body: JSON.stringify({
+        query: {
+          measures: ['conso_enaf_com.id_611'],
+          filters: [
+            {
+              member:
+                type === 'commune' ||
+                type === 'petr' ||
+                type === 'ept' ||
+                type === 'pnr'
+                  ? 'conso_enaf_com.geocode_commune'
+                  : type === 'epci'
+                    ? 'conso_enaf_com.geocode_epci'
+                    : 'conso_enaf_com.geocode_departement',
+              operator: 'equals',
+              values: listeTerritoires
+            }
+            // {
+            //   member: 'conso_enaf_com.secteur',
+            //   operator: 'equals',
+            //   values: ['Route']
+            // }
+          ],
+          timezone: 'UTC',
+          dimensions: [
+            'conso_enaf_com.secteur',
+            type === 'commune' ||
+            type === 'petr' ||
+            type === 'ept' ||
+            type === 'pnr'
+              ? 'conso_enaf_com.geocode_commune'
+              : type === 'epci'
+                ? 'conso_enaf_com.geocode_epci'
+                : 'conso_enaf_com.geocode_departement'
+          ],
+          timeDimensions: [
+            {
+              dimension: 'conso_enaf_com.date_mesure',
+              granularity: 'year'
+              // dateRange: ['2023-01-01', '2023-12-01']
+            }
+          ],
+          limit: 50000
+          // order: { 'conso_enaf_com.geocode_commune': 'asc' }
+        }
+      })
     });
-    listeTerritoires = listeCommunes
-      .map((el) => el.code_geographique)
-      .filter((code) => code !== null);
-  } else {
-    listeTerritoires = [code];
-  }
-  console.timeEnd(`Query Execution Time CUBEJS NAF GetCommunes ${type}`);
-  console.time('Query Execution Time CUBEJS NAF');
 
-  const request = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `${process.env.TEST_ECOLAB_TOKEN}`
-    },
-    body: JSON.stringify({
-      query: {
-        measures: ['conso_enaf_com.id_611'],
-        filters: [
-          {
-            member:
-              type === 'commune' ||
-              type === 'petr' ||
-              type === 'ept' ||
-              type === 'pnr'
-                ? 'conso_enaf_com.geocode_commune'
-                : type === 'epci'
-                  ? 'conso_enaf_com.geocode_epci'
-                  : 'conso_enaf_com.geocode_departement',
-            operator: 'equals',
-            values: listeTerritoires
-          }
-          // {
-          //   member: 'conso_enaf_com.secteur',
-          //   operator: 'equals',
-          //   values: ['Route']
-          // }
-        ],
-        timezone: 'UTC',
-        dimensions: [
-          'conso_enaf_com.secteur',
-          type === 'commune' ||
-          type === 'petr' ||
-          type === 'ept' ||
-          type === 'pnr'
-            ? 'conso_enaf_com.geocode_commune'
-            : type === 'epci'
-              ? 'conso_enaf_com.geocode_epci'
-              : 'conso_enaf_com.geocode_departement'
-        ],
-        timeDimensions: [
-          {
-            dimension: 'conso_enaf_com.date_mesure',
-            granularity: 'year'
-            // dateRange: ['2023-01-01', '2023-12-01']
-          }
-        ],
-        limit: 50000
-        // order: { 'conso_enaf_com.geocode_commune': 'asc' }
-      }
-    })
-  });
+    if (!request.ok) {
+      throw new Error('Failed to fetch data');
+    }
 
-  if (!request.ok) {
-    throw new Error('Failed to fetch data');
-  }
+    const response = await request.json();
+    console.timeEnd('Query Execution Time CUBEJS NAF');
 
-  const response = await request.json();
-  console.timeEnd('Query Execution Time CUBEJS NAF');
-
-  return response.data;
+    return response.data;
   } catch (error) {
     console.error(error);
     return [];
