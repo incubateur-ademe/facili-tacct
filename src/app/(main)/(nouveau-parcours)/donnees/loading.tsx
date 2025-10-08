0919aa62452269e10c8d8a8dd57441313723be5d
AntoineConegero <antoine.conegero@gmail.com>
2025-10-08 11:28:24 +0200

    refactor: cleaning mep

---
+++ src/app/(main)/(nouveau-parcours)/donnees/loading.tsx
@@ -0,0 +1,9 @@
+import { LoaderText } from "@/components/ui/loader";
+
+const Loading = () => {
+  return (
+    <LoaderText text='Nous chargeons vos données' />
+  );
+};
+
+export default Loading;
