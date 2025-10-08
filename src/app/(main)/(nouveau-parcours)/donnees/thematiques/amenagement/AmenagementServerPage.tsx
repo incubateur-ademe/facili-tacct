ff630c38ee42d0963be91a2fd3c50af95284bf46
AntoineConegero <antoine.conegero@gmail.com>
2025-10-08 11:28:24 +0200

    refactor: cleaning mep

--- src/app/(main)/(nouveau-parcours)/donnees/thematiques/amenagement/AmenagementServerPage.tsx
+++ src/app/(main)/(nouveau-parcours)/donnees/thematiques/amenagement/AmenagementServerPage.tsx
@@ -1,6 +1,5 @@
 import { SearchParams } from "@/app/(main)/types";
 import { GetConsommationNAF } from "@/lib/queries/databases/biodiversite";
-import { GetNAF } from "@/lib/queries/ecologieGouv/test";
 import { GetCommunes } from "@/lib/queries/postgis/cartographie";
 import { DonneesAmenagement } from "./DonneesAmenagement";
 
@@ -8,18 +7,11 @@ const AmenagementServerPage = async (props: { searchParams: SearchParams }) => {
   const { code, libelle, type } = await props.searchParams;
   const dbConsommationNAF = await GetConsommationNAF(code, libelle, type);
   const carteCommunes = await GetCommunes(code, libelle, type);
-  const dbConsommationNAFEcolab = await GetNAF(code, libelle, type);
-
-  // Si les données ne sont pas disponibles, on peut soit retourner notFound() soit un message d'erreur
-  // if (!carteCommunes.length || !dbConsommationNAF) {
-  //   notFound();
-  // }
 
   return (
     <DonneesAmenagement
       carteCommunes={carteCommunes}
       consommationNAF={dbConsommationNAF}
-      consommationNAFEcolab={dbConsommationNAFEcolab}
     />
   );
 };
--- src/components/ToggleTheme.ts
+++
@@ -1,9 +0,0 @@
-// export const ToggleTheme = (
-//   computedStyle: CSSStyleDeclaration,
-//   style: CSSStyleDeclaration
-// ) => {
-//   const noticeStyle = computedStyle.getPropertyValue('--height-notice');
-//   noticeStyle === "543px" ?
-//   style.setProperty('--height-notice', "415px")
-//   : style.setProperty('--height-notice', '543px');
-// }
