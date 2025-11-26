0d433a34d5bfa5f6b5bbb7e244e4ff59a56df9b6
AntoineConegero <antoine.conegero@gmail.com>
2025-11-26 18:48:27 +0100

    style: ressources bloc autres outils

--- src/app/(main)/ressources/CarteCollection.tsx
+++ src/app/(main)/ressources/CarteCollection.tsx
@@ -1,5 +1,5 @@
 import Image, { StaticImageData } from "next/image";
-import styles from "./Cartes.module.scss";
+import styles from "./ressources.module.scss";
 
 export const CarteCollection = ({
   texte,
--- src/app/(main)/ressources/blocs/blocAutresOutils.tsx
+++ src/app/(main)/ressources/blocs/blocAutresOutils.tsx
@@ -1,5 +1,7 @@
+import { CarteHoverLink } from "@/components/Cartes";
 import { Body, H2 } from "@/design-system/base/Textes";
 import { NewContainer } from "@/design-system/layout";
+import { autresOutilsCartes } from "@/lib/ressources/cartes";
 import styles from '../ressources.module.scss';
 
 export const BlocAutresOutils = () => {
@@ -15,6 +17,18 @@ export const BlocAutresOutils = () => {
               Des réponses à vos questions, un appui méthodologique et une communauté d’entraide,
               pour accompagner votre démarche d’adaptation.
             </Body>
+          </div>
+          <div className={styles.cartesWrapper}>
+            {
+              autresOutilsCartes.map((carte, index) => (
+                <CarteHoverLink
+                  key={index}
+                  titre={carte.titre}
+                  description={carte.description}
+                  icone={carte.icone}
+                />
+              ))
+            }
           </div>
         </div>
       </NewContainer>
