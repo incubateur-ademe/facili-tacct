9ec8cfc12910c6ca9150eca576928921e4a07ebf
AntoineConegero <antoine.conegero@gmail.com>
2025-11-27 18:40:01 +0100

    feat: tuiles horizontales et verticales

--- src/components/Tuile.tsx
+++ src/components/Tuile.tsx
@@ -1,29 +1,32 @@
 "use client";
 
 import ClockIcon from "@/assets/icons/clock_icon_black.png";
-import TestImageTuile from "@/assets/images/test_tuile.png";
+import LienExterneIcon from "@/assets/icons/fr-icon-external-link-line.png";
 import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
 import styles from "./Tuile.module.scss";
 
 interface Props {
   titre: string;
-  description: string;
-  image?: StaticImageData;
+  description?: string;
+  image: StaticImageData;
   lien?: string;
+  lienExterne?: boolean;
   tags?: React.ReactNode[];
   tempsLecture?: number;
 }
 
-export const Tuile = ({
+export const TuileVerticale = ({
   titre,
   description,
-  image = TestImageTuile,
+  image,
   lien,
+  lienExterne,
   tags = [],
   tempsLecture
 }: Props) => {
-  return (
-    <div className={styles.tuile} tabIndex={0} role="article">
+  const content = (
+    <div className={styles.tuile} tabIndex={lien ? -1 : 0} role="article">
       <div className={styles.imageContainer}>
         <Image src={image} alt={titre} fill />
       </div>
@@ -49,7 +52,75 @@ export const Tuile = ({
             <span>{tempsLecture} min</span>
           </div>
         )}
-        <span className={`fr-icon-arrow-right-line ${styles.arrow}`} aria-hidden="true"></span>
+        {
+          lien && (
+            <div className={styles.lien}>
+              {
+                lienExterne ? <Image src={LienExterneIcon} alt="" width={24} height={24} /> :
+                  <span className={`fr-icon-arrow-right-line ${styles.arrow}`} aria-hidden="true"></span>
+              }
+            </div>
+          )
+        }
+      </div>
+    </div>
+  );
+
+  if (lien) {
+    return (
+      <Link
+        href={lien}
+        target={lienExterne ? "_blank" : "_self"}
+        rel={lienExterne ? "noopener noreferrer" : undefined}
+        className={styles.tuileLink}
+      >
+        {content}
+      </Link>
+    );
+  }
+
+  return content;
+}
+
+export const TuileHorizontale = ({
+  titre,
+  image,
+  lien,
+  tags = [],
+  tempsLecture
+}: Props) => {
+  return (
+    <div className={styles.tuileHorizontale} tabIndex={0} role="article">
+      <div className={styles.imageContainer}>
+        <Image src={image} alt={titre} fill />
+      </div>
+
+      <div className={styles.contenu}>
+        <div className={styles.titre}>
+          {titre}
+        </div>
+
+        <div className={styles.footer}>
+          <div className={styles.leftSection}>
+            {tags.length > 0 && (
+              <div className={styles.tags}>
+                {tags.map((tag, index) => (
+                  <span key={index}>
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            )}
+            {tempsLecture && (
+              <div className={styles.tempsLecture}>
+                <Image src={ClockIcon} alt="Temps de lecture" width={16} height={16} />
+                <span>{tempsLecture} min</span>
+              </div>
+            )}
+          </div>
+
+          <span className={`fr-icon-arrow-right-line ${styles.arrow}`} aria-hidden="true"></span>
+        </div>
       </div>
     </div>
   );
--- src/lib/ressources/toutesRessources.ts
+++ src/lib/ressources/toutesRessources.ts
@@ -1,3 +1,19 @@
+import TestImageTuile from '@/assets/images/test_tuile.png';
+import Adacc from '@/lib/ressources/articles/adacc';
+import BriserSilosApprocheSystemique from '@/lib/ressources/articles/briserSilosApprocheSystemique';
+import CDC from '@/lib/ressources/articles/CDC';
+import ClimaStory from '@/lib/ressources/articles/climaStory';
+import EviterPrecherDesert from '@/lib/ressources/articles/eviterPrecherDesert';
+import FacilitationDateliers from '@/lib/ressources/articles/facilitationDateliers';
+import IlotChaleurUrbain from '@/lib/ressources/articles/ilotChaleurUrbain';
+import IndicateursDeResilience from '@/lib/ressources/articles/indicateursDeResilience';
+import LireUnDiagnostic from '@/lib/ressources/articles/lireUnDiagnostic';
+import MiseEnRecit from '@/lib/ressources/articles/miseEnRecit';
+import PnaccTracc from '@/lib/ressources/articles/pnaccTracc';
+import PourquoiMobiliser from '@/lib/ressources/articles/pourquoiMobiliser';
+import RecruterStagiaire from '@/lib/ressources/articles/recruterStagiaire';
+import RisqueALadaptation from '@/lib/ressources/articles/risqueALadaptation';
+
 export const FiltresOptions = [
   {
     titre: 'Je cherche à',
@@ -33,39 +49,36 @@ export const FiltresOptions = [
   }
 ];
 
-import Adacc from '@/lib/ressources/articles/adacc';
-import BriserSilosApprocheSystemique from '@/lib/ressources/articles/briserSilosApprocheSystemique';
-import CDC from '@/lib/ressources/articles/CDC';
-import ClimaStory from '@/lib/ressources/articles/climaStory';
-import EviterPrecherDesert from '@/lib/ressources/articles/eviterPrecherDesert';
-import FacilitationDateliers from '@/lib/ressources/articles/facilitationDateliers';
-import IlotChaleurUrbain from '@/lib/ressources/articles/ilotChaleurUrbain';
-import IndicateursDeResilience from '@/lib/ressources/articles/indicateursDeResilience';
-import LireUnDiagnostic from '@/lib/ressources/articles/lireUnDiagnostic';
-import MiseEnRecit from '@/lib/ressources/articles/miseEnRecit';
-import PnaccTracc from '@/lib/ressources/articles/pnaccTracc';
-import PourquoiMobiliser from '@/lib/ressources/articles/pourquoiMobiliser';
-import RecruterStagiaire from '@/lib/ressources/articles/recruterStagiaire';
-import RisqueALadaptation from '@/lib/ressources/articles/risqueALadaptation';
-
 export const TousLesArticles = [
   {
     slug: 'analyser-diagnostic-vulnerabilite',
     Component: LireUnDiagnostic,
-    metadata: {
-      title: 'Analyser un diagnostic de vulnérabilité',
-      description:
-        'Relisez votre diagnostic de vulnérabilité aux effets du changement climatique en 10 minutes : repérez les données utiles et les enjeux clés.'
-    },
-    filtres: ["M'inspirer", 'Article', 'National'],
+    titre: 'Analyser un diagnostic de vulnérabilité',
+    description:
+      'Relisez votre diagnostic de vulnérabilité aux effets du changement climatique en 10 minutes : repérez les données utiles et les enjeux clés.',
+    lien: '/ressources/articles/analyser-diagnostic-vulnerabilite',
+    filtres: ['Article', "M'inspirer", 'National'],
     collection: "Bâtir ma stratégie d'adaptation",
-    tempsLecture: 15
+    tempsLecture: 15,
+    image: TestImageTuile
+  },
+  {
+    tab: 'Vous découvrez le diagnostic pour la 1ère fois',
+    titre:
+      'Comment restituer son diagnostic des impacts du changement climatique en Haute-Loire (43)',
+    description:
+      'Comment embarquer dans la construction de votre stratégie d’adaptation? Un partage engageant du diagnostic est important, pour cela, la CA du Puy-en-Velay a articulé TACCT et l’atelier ClimaSTORY.',
+    lien: 'https://librairie.ademe.fr/7180-comment-restituer-son-diagnostic-des-impacts-du-changement-climatique-en-haute-loire-43.html',
+    filtres: ["Retour d'expérience", 'Agir', 'Centre-Val de Loire'],
+    collection: "Bâtir ma stratégie d'adaptation",
+    tempsLecture: 15,
+    image: TestImageTuile
   },
   {
     slug: 'mise-en-recit-territoire-adaptation-climat',
     Component: MiseEnRecit,
     metadata: {
-      title: 'Mettre en récit mon territoire pour engager',
+      titre: 'Mettre en récit mon territoire pour engager',
       description:
         'Découvrez comment mettre en récit votre territoire pour engager les acteurs locaux dans l’adaptation au changement climatique.'
     }
@@ -74,7 +87,7 @@ export const TousLesArticles = [
     slug: 'mobilisation-diagnostic-vulnerabilite',
     Component: PourquoiMobiliser,
     metadata: {
-      title:
+      titre:
         'Comment mobiliser élus, services et partenaires autour d’un diagnostic de vulnérabilité ?',
       description:
         'Découvrez le retour d’expérience de Jolet Van Kipshagen (CC Vallée de Villé) et les clés d’une démarche concertée et ancrée dans le territoire.'
@@ -84,7 +97,7 @@ export const TousLesArticles = [
     slug: 'facilitation-ateliers-mobilisation',
     Component: FacilitationDateliers,
     metadata: {
-      title: 'La facilitation d’ateliers : une démarche éprouvée d’engagement',
+      titre: 'La facilitation d’ateliers : une démarche éprouvée d’engagement',
       description:
         'Découvrez comment la facilitation d’ateliers peut renforcer l’engagement des parties prenantes dans l’adaptation.'
     }
@@ -93,7 +106,7 @@ export const TousLesArticles = [
     slug: 'facilitation-cahier-charges',
     Component: CDC,
     metadata: {
-      title: 'Le cahier des charges, levier pour intégrer la facilitation',
+      titre: 'Le cahier des charges, levier pour intégrer la facilitation',
       description:
         'Le rôle du cahier des charges dans l’intégration de la facilitation pour l’adaptation au changement climatique.'
     }
@@ -102,7 +115,7 @@ export const TousLesArticles = [
     slug: 'reussir-mobilisation-acteurs-adaptation',
     Component: EviterPrecherDesert,
     metadata: {
-      title: 'Comment éviter de prêcher dans le désert ?',
+      titre: 'Comment éviter de prêcher dans le désert ?',
       description:
         'Conseils pour éviter de prêcher dans le désert et maximiser l’impact de vos actions d’adaptation.'
     }
@@ -111,7 +124,7 @@ export const TousLesArticles = [
     slug: 'ateliers-adacc-adaptation',
     Component: Adacc,
     metadata: {
-      title: 'Sensibiliser à l’adaptation : les AdACC',
+      titre: 'Sensibiliser à l’adaptation : les AdACC',
       description:
         'Présentation des Ateliers de l’Adaptation au Changement Climatique (AdACC) pour sensibiliser les acteurs.'
     }
@@ -120,7 +133,7 @@ export const TousLesArticles = [
     slug: 'atelier-climastory-sensibilisation-adaptation',
     Component: ClimaStory,
     metadata: {
-      title: 'ClimaSTORY, une cartographie pour sensibiliser',
+      titre: 'ClimaSTORY, une cartographie pour sensibiliser',
       description:
         'Découvrez ClimaSTORY, un outil cartographique pour sensibiliser à l’adaptation au changement climatique.'
     }
@@ -129,7 +142,7 @@ export const TousLesArticles = [
     slug: 'stagiaire-diagnostic-vulnerabilite-climat',
     Component: RecruterStagiaire,
     metadata: {
-      title:
+      titre:
         'Recruter un stagiaire sur le diagnostic de vulnérabilité : bonne ou mauvaise idée ?',
       description:
         'Vous envisagez de confier le diagnostic de vulnérabilité à votre stagiaire ? Découvrez les erreurs à éviter, les missions possibles, et les bonnes pratiques pour un appui vraiment utile.'
@@ -139,7 +152,7 @@ export const TousLesArticles = [
     slug: 'ilot-chaleur-urbain-erreurs-a-eviter',
     Component: IlotChaleurUrbain,
     metadata: {
-      title:
+      titre:
         'Îlot de chaleur urbain : 4 idées reçues à déjouer pour mieux agir sur le terrain',
       description:
         'Îlot de chaleur urbain : 4 idées reçues à déjouer pour mieux comprendre le phénomène, cibler les bons diagnostics et adapter l’action aux enjeux'
@@ -149,7 +162,7 @@ export const TousLesArticles = [
     slug: 'pnacc-tracc-comment-suis-je-concerne',
     Component: PnaccTracc,
     metadata: {
-      title: 'PNACC, TRACC, Comment suis-je concerné ?',
+      titre: 'PNACC, TRACC, Comment suis-je concerné ?',
       description:
         'PNACC, TRACC autant de documents stratégiques qui orientent l’adaptation au changement climatique en France. Comprendre leur contenu est nécessaire pour anticiper et mettre en oeuvre des stratégies cohérentes.'
     }
@@ -158,7 +171,7 @@ export const TousLesArticles = [
     slug: 'briser-silos-approche-systemique',
     Component: BriserSilosApprocheSystemique,
     metadata: {
-      title: 'Brisez les silos : introduction à l’approche systémique',
+      titre: 'Brisez les silos : introduction à l’approche systémique',
       description:
         'Comprendre les interactions au sein de votre système est essentiel pour éviter les maladaptations. L’approche systémique vous accompagne de l’identification jusqu’à la mobilisation.'
     }
@@ -167,7 +180,7 @@ export const TousLesArticles = [
     slug: 'resilience-agricole-indicateurs-territoire',
     Component: IndicateursDeResilience,
     metadata: {
-      title:
+      titre:
         'Comment mesurer la résilience d’un territoire agricole face au changement climatique ?',
       description:
         '22 indicateurs pour aider les chargés de mission climat à évaluer la résilience agricole et repérer les vulnérabilités au changement climatique.'
@@ -177,7 +190,7 @@ export const TousLesArticles = [
     slug: 'strategie-adaptation-gestion-risque-relocalisation',
     Component: RisqueALadaptation,
     metadata: {
-      title:
+      titre:
         'De la gestion du risque à l’adaptation : le cas de la relocalisation de Miquelon',
       description:
         'De la gestion du risque à l’adaptation ou quand reculer devient avancer : témoignages d’une projection sur le temps long avec l’exemple de Miquelon.'
