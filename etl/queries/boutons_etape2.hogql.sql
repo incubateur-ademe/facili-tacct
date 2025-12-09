c46043c1565b6b42865994657fe8c1587485b0eb
Antoine <antoine.conegero@gmail.com>
2025-12-09 16:00:42 +0100

    etl: exclusion preprod

---
+++ etl/queries/boutons_etape2.hogql.sql
@@ -0,0 +1,28 @@
+select event,
+    properties.$sent_at,
+    properties,
+    distinct_id,
+    `$session_id`,
+    person_id
+from events
+where
+    and(
+        event LIKE '%clic_diagnostic_impact%',
+        notIn(
+            properties.$host,
+            tuple(
+                'facili-tacct-preprod.osc-fr1.scalingo.io',
+                'facili-tacct.osc-secnum-fr1.scalingo.io',
+                'facili-tacct-nginx.osc-secnum-fr1.scalingo.io',
+                -- 'facili-tacct.incubateur.ademe.dev',
+                'null'
+            )
+        ),
+    )
+    AND timestamp < now()
+    -- AND properties.$current_url LIKE 'https://facili-tacct.beta.gouv.fr/%'
+    AND properties.$current_url NOT LIKE '%localhost%'
+    AND properties.$current_url NOT LIKE '%scalingo%'
+    AND properties.$current_url NOT LIKE '%secnum%'
+order by timestamp desc
+limit 50000
