1b71e36e00a85cfd6825b0185309a6fda7d3a8f9
Antoine <antoine.conegero@gmail.com>
2025-12-11 14:18:34 +0100

    feat: filtres des ressources pour mobile

--- src/components/MultiSelect.tsx
+++ src/components/MultiSelect.tsx
@@ -8,7 +8,7 @@ import MenuItem from "@mui/material/MenuItem";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { useState } from 'react';
 
-const MultiSelect = ({
+export const MultiSelect = ({
   options,
   handleSelectObjectifOptions,
   selectedValues = []
@@ -116,4 +116,106 @@ const MultiSelect = ({
   );
 };
 
-export default MultiSelect;
+export const MultiSelectResponsive = ({
+  handleSelectObjectifOptions,
+  selectedValues = [],
+  filtre
+}: {
+  handleSelectObjectifOptions: (event: SelectChangeEvent<string[]>) => void;
+  selectedValues?: string[];
+  filtre: { titre: string; options: string[]; };
+}) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <FormControl fullWidth>
+      <Select
+        multiple
+        value={selectedValues}
+        onChange={handleSelectObjectifOptions}
+        open={open}
+        onOpen={() => setOpen(true)}
+        onClose={() => setOpen(false)}
+        displayEmpty
+        IconComponent={ExpandMoreOutlinedIcon}
+        renderValue={() => selectedValues.length !== 0 ? `${filtre.titre} (${selectedValues.length})` : filtre.titre}
+        MenuProps={{
+          sx: {
+            '& .MuiPaper-root': {
+              maxHeight: '500px',
+              backgroundColor: '#FAFAFA',
+              borderRadius: '0px',
+              boxShadow: 'none',
+              '& .Mui-selected': {
+                fontWeight: 700,
+                backgroundColor: '#FFFFFF',
+              },
+            },
+            '& .MuiTypography-root': {
+              fontFamily: 'Marianne !important',
+            },
+            '& .MuiList-root': {
+              padding: '1rem 0.5rem',
+            },
+            '& .MuiMenuItem-root:not(:last-child)': {
+              // borderBottom: '1px solid var(--gris-medium)',
+            },
+            '& .MuiMenuItem-root': {
+              fontSize: '14px',
+              lineHeight: '19px',
+              padding: 0,
+
+              '&:hover': { fontWeight: '700 !important', backgroundColor: 'transparent !important' },
+              '&:focus': { fontWeight: '700 !important', backgroundColor: 'transparent !important', outline: 'none' },
+            },
+            '& .MuiCheckbox-root': {
+              padding: '6px',
+            },
+          },
+        }}
+        sx={{
+          '&': {
+            backgroundColor: 'white',
+            borderRadius: '0px',
+            height: "48px",
+            fontWeight: 400,
+            borderBottom: '1px solid var(--gris-medium)',
+          },
+          '& .MuiSelect-select': {
+            color: '#161616',
+            fontSize: '18px',
+            fontFamily: 'Marianne',
+            fontWeight: selectedValues.length === 0 ? 'normal' : 'bold',
+            border: 'none',
+          },
+          '& .MuiOutlinedInput-notchedOutline': {
+            border: 'none',
+          },
+          '& .MuiSelect-icon': {
+            fill: '#161616',
+          },
+        }}
+      >
+        {filtre.options.map((option) => (
+          <MenuItem key={option} value={option}>
+            <Checkbox
+              checked={selectedValues.includes(option)}
+              sx={{
+                color: '#038278',
+                '&.Mui-checked': {
+                  color: '#038278',
+                },
+                '& .MuiSvgIcon-root': {
+                  borderRadius: '4px',
+                },
+                '&:hover': {
+                  backgroundColor: 'transparent',
+                },
+              }}
+            />
+            <ListItemText primary={option} />
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
