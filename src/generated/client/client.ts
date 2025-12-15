792adde203cfadd988090a026bb69f2500a828d8
Antoine <antoine.conegero@gmail.com>
2025-12-15 13:32:41 +0100

    test: jest testing sans prisma

--- src/generated/client/client.ts
+++ src/generated/client/client.ts
@@ -10,18 +10,19 @@
  * 🟢 You can import this file directly.
  */
 
-import * as process from 'node:process'
-import * as path from 'node:path'
-import { fileURLToPath } from 'node:url'
+import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
+// import __dirname from "./dirname.cjs";
+// console.log("__dirname:", __dirname);
 globalThis['__dirname'] = path.dirname(fileURLToPath(import.meta.url))
 
-import * as runtime from "@prisma/client/runtime/client"
-import * as $Enums from "./enums"
-import * as $Class from "./internal/class"
-import * as Prisma from "./internal/prismaNamespace"
+import * as runtime from "@prisma/client/runtime/client";
+import * as $Class from "./internal/class";
+import * as Prisma from "./internal/prismaNamespace";
 
-export * as $Enums from './enums'
-export * from "./enums"
+export * from "./enums";
+export * as $Enums from './enums';
+export { Prisma };
 /**
  * ## Prisma Client
  * 
@@ -37,7 +38,6 @@ export * from "./enums"
  */
 export const PrismaClient = $Class.getPrismaClientClass()
 export type PrismaClient<LogOpts extends Prisma.LogLevel = never, OmitOpts extends Prisma.PrismaClientOptions["omit"] = Prisma.PrismaClientOptions["omit"], ExtArgs extends runtime.Types.Extensions.InternalArgs = runtime.Types.Extensions.DefaultArgs> = $Class.PrismaClient<LogOpts, OmitOpts, ExtArgs>
-export { Prisma }
 
 /**
  * Model north_star_metric
