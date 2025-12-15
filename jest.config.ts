19a5826495b36283fa47d0d46dcfd6e33cc2d2c5
Antoine <antoine.conegero@gmail.com>
2025-12-15 13:32:41 +0100

    test: jest testing sans prisma

--- jest.config.ts
+++ jest.config.ts
@@ -3,16 +3,9 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  testMatch: [
-    '**/__tests__/**/*.+(ts|tsx|js)',
-    '**/?(*.)+(spec|test).+(ts|tsx|js)'
-  ],
   moduleNameMapper: {
-    '\\.(css|scss|sass)$': 'identity-obj-proxy',
     '^@/(.*)$': '<rootDir>/src/$1'
-  },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+  }
 };
 
 export default config;
