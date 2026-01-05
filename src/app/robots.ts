6673cf3edb0144203ec6cd919d1d780c4e19e3b5
AntoineConegero <antoine.conegero@gmail.com>
2026-01-05 15:57:11 +0100

    feat: referencement

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,17 @@
 import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
+  if (
+    process.env.NEXT_PUBLIC_ENV === 'preprod' ||
+    process.env.NEXT_PUBLIC_ENV === 'development'
+  ) {
+    return {
+      rules: {
+        userAgent: '*',
+        disallow: '/'
+      }
+    };
+  }
   return {
     rules: {
       userAgent: '*',
