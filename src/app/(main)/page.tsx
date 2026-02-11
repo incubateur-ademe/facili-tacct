76dc397e56e929cb7423d3e52ba9b6711af6264c
salamèche <antoine.conegero@gmail.com>
2026-02-11 09:29:21 +0100

    chore: notice home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect } from 'react';
-// import { useStyles } from 'tss-react/dsfr';
+import { useStyles } from 'tss-react/dsfr';
 import { CommunauteBloc } from './(home)/CommunauteBloc';
 import { DiagnosticBloc } from './(home)/DiagnosticBloc';
 import { Patch4Bloc } from './(home)/Patch4Bloc';
@@ -9,11 +9,11 @@ import { PremierBloc } from './(home)/PremierBloc';
 import { RessourcesBloc } from './(home)/RessourcesBloc';
 import { TacctBloc } from './(home)/TacctBloc';
 import { VerbatimBloc } from './(home)/VerbatimBloc';
-// import Link from 'next/link';
-// import Notice from '@codegouvfr/react-dsfr/Notice';
+import Link from 'next/link';
+import Notice from '@codegouvfr/react-dsfr/Notice';
 
 const Home = () => {
-  // const { css } = useStyles();
+  const { css } = useStyles();
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -23,43 +23,26 @@ const Home = () => {
 
   return (
     <div>
-      {/* <Notice
+      <Notice
         className={css({
           backgroundColor: 'var(--gris-medium)',
           color: "#201F1E"
         })}
         isClosable={true}
-        title={"Nouveau :"}
+        title={"Nouveautés sur le site :"}
         description={
           <>
-            une sélection de ressources regroupées par "collection" pour vous accompagner
-            dans chaque situation : découvrez votre nouvel espace "
+            votre territoire est-il concerné par une obligation légale de débroussaillement ? Découvrez-le 
+            dans la thématique Gestion des risques. Retrouvez également un tout nouveau retour 
+            d’expérience sur la réalisation du diagnostic de vulnérabilité, {" "}
             <Link
-              href="/ressources"
+              href="/ressources/demarrer-diagnostic-vulnerabilite/realiser-diagnostic-vulnerabilite"
             >
-              Boîte à outils
-            </Link>
-            " ! Vous y trouverez également les formations ADEME de la méthode TACCT ainsi
-            qu’un{" "}
-            <Link
-              href="https://tally.so/r/w4l8pk"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              quiz pour tester vos connaissances sur l’adaptation
-            </Link>
-            . Une remarque, une suggestion ?{' '}
-            <a
-              href="https://tally.so/r/mJGELz"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Contactez-nous
-            </a>
-            , on a hâte d’avoir vos retours !
+              celui de Rennes Métropole
+            </Link> !
           </>
         }
-      /> */}
+      />
       <PremierBloc />
       <Patch4Bloc />
       <TacctBloc />
