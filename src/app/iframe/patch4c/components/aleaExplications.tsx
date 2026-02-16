6969fbcc1f5063c4c0bf4b81c47bf525d9d3e6e6
AntoineConegero <antoine.conegero@gmail.com>
2026-02-16 12:28:32 +0100

    fix: aléas iframe

--- src/app/iframe/patch4c/components/aleaExplications.tsx
+++ src/app/iframe/patch4c/components/aleaExplications.tsx
@@ -6,7 +6,6 @@ import { Body } from "@/design-system/base/Textes";
 import Image, { StaticImageData } from "next/image";
 import { useState } from "react";
 import styles from '../patch4c.module.scss';
-import { agravationItems } from './constantes';
 import { getBackgroundColor } from "./fonctions";
 
 type Item = {
@@ -97,12 +96,18 @@ export const AleaExplications = ({
           <Body>
             {item.definition}
           </Body>
-          <Body weight="bold" style={{ margin: '2rem 0 0.5rem' }}>
-            Niveau d’aggravation
-          </Body>
-          <Body>
-            {agravationItems.find(agr => agr.label === item.value)?.hover}
-          </Body>
+          {/* {
+            !isMap && (
+              <>
+                <Body weight="bold" style={{ margin: '2rem 0 0.5rem' }}>
+                  Niveau d’aggravation
+                </Body>
+                <Body>
+                  {agravationItems.find(agr => agr.label === item.value)?.hover}
+                </Body>
+              </>
+            )
+          } */}
           <Body weight="bold" style={{ margin: '2rem 0 0.5rem' }}>
             D'où vient cette donnée ?
           </Body>
